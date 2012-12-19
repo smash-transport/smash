2dd3fb7d82475620385b843fb3d4e9b308c4c370
maximilian attems <attems@fias.uni-frankfurt.de>
2012-12-19 09:15:15 +0100

    ParticleData: add access to position fields of the particles
    First useage in random IC.

--- src/include/ParticleData.h
+++ src/include/ParticleData.h
@@ -17,6 +17,10 @@ class ParticleData {
   double inline momenta_t(void);
   void inline set_momenta_t(double momenta_t);
   void inline set_momenta(double momenta_l, double momenta_t);
+  float inline x(void);
+  float inline y(void);
+  float inline z(void);
+  void inline set_position(float pos_x, float pos_y, float pos_z);
 
   private:
     /* Each particle has a unique identifier */
@@ -58,6 +62,24 @@ void inline ParticleData::set_momenta_t(double m_t) {
 void inline ParticleData::set_momenta(double m_l, double m_t) {
   momenta_l_ = m_l;
   momenta_t_ = m_t;
+}
+
+float inline ParticleData::x(void) {
+  return x_;
+}
+
+float inline ParticleData::y(void) {
+  return y_;
+}
+
+float inline ParticleData::z(void) {
+  return z_;
+}
+
+void inline ParticleData::set_position(float pos_x, float pos_y, float pos_z) {
+  x_ = pos_x;
+  y_ = pos_y;
+  z_ = pos_z;
 }
 
 #endif  // SRC_INCLUDE_PARTICLEDATA_H_
--- src/initial-conditions.cc
+++ src/initial-conditions.cc
@@ -15,6 +15,7 @@
 #include "include/ParticleType.h"
 #include "include/random.h"
 
+/* initial_conditions - sets partilce data for @particles */
 void initial_conditions(ParticleData *particles) {
   int num = 5;
   ParticleType pi("pi", 0.13957);
@@ -23,11 +24,15 @@ void initial_conditions(ParticleData *particles) {
   printd("Pi^± mass: %g [GeV]\n", pi.mass());
   printd("Pi^0 mass: %g [GeV]\n", pi0.mass());
 
+  /* Set random IC */
   particles = new ParticleData[num];
   for (int i = 0; i < num; i++) {
     particles[i].set_id(i);
     particles[i].set_momenta(randGauss(1.0), randGauss(1.0));
+    particles[i].set_position(randGauss(0.2), randGauss(0.2), randGauss(0.2));
     printd("Particle %d momenta: %g %g [GeV]\n", particles[i].id(),
       particles[i].momenta_l(), particles[i].momenta_t());
+    printd("Particle %d position: %g %g %g\n", particles[i].id(),
+      particles[i].x(), particles[i].y(), particles[i].z());
   }
 }
