2b58db212701a80567f9a99fb0dab8c0a37ac932
maximilian attems <attems@fias.uni-frankfurt.de>
2012-12-19 09:50:39 +0100

    IC: Set random particle positions inside the box
    For the spacial distribution of the particles gaussian doesn't
    make sense, just use random distribution.

--- src/initial-conditions.cc
+++ src/initial-conditions.cc
@@ -18,18 +18,24 @@
 /* initial_conditions - sets partilce data for @particles */
 void initial_conditions(ParticleData *particles) {
   int num = 5;
+  float x_pos, y_pos, z_pos;
   ParticleType pi("pi", 0.13957);
   ParticleType pi0("pi0", 0.134977);
 
   printd("Pi^± mass: %g [GeV]\n", pi.mass());
   printd("Pi^0 mass: %g [GeV]\n", pi0.mass());
 
-  /* Set random IC */
+  /* Set random IC:
+   * particles at random position in the box with random momenta
+   */
   particles = new ParticleData[num];
   for (int i = 0; i < num; i++) {
     particles[i].set_id(i);
     particles[i].set_momenta(randGauss(1.0), randGauss(1.0));
-    particles[i].set_position(randGauss(0.2), randGauss(0.2), randGauss(0.2));
+    x_pos = 1.0 * rand_r(&seedp) / RAND_MAX * A;
+    y_pos = 1.0 * rand_r(&seedp) / RAND_MAX * A;
+    z_pos = 1.0 * rand_r(&seedp) / RAND_MAX * A;
+    particles[i].set_position(x_pos, y_pos, z_pos);
     printd("Particle %d momenta: %g %g [GeV]\n", particles[i].id(),
       particles[i].momenta_l(), particles[i].momenta_t());
     printd("Particle %d position: %g %g %g\n", particles[i].id(),
