cc79054f0e0f5e32116ddc079b5df092fee3ba8d
maximilian attems <attems@fias.uni-frankfurt.de>
2012-12-20 20:42:15 +0100

    outputroutines: Add more info about the box startup
    Directly taken from Jussi's box.

--- src/outputroutines.cc
+++ src/outputroutines.cc
@@ -2,6 +2,7 @@
  *
  *    Copyright (c) 2012
  *      maximilian attems <attems@fias.uni-frankfurt.de>
+ *      Jussi Auvinen <auvinen@fias.uni-frankfurt.de>
  *
  *    GNU General Public License (GPLv3)
  *
@@ -12,7 +13,9 @@
 #include <stdio.h>
 
 void print_startup(void) {
-  printf("Size of the box: %g [fm]\n", A);
+  printf("Size of the box: %g x %g x %g [fm]\n", A, A, A);
   printf("Initial temperature: %g [GeV]\n", temperature);
   printf("Using temporal stepsize: %g [GeV]\n", EPS);
+  printf("Maximum number of steps: %i \n", STEPS);
+  printf("Random number seed: %i \n", seedp);
 }
