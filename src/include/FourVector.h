2807c121e5b632d9f498b43c9b8916220703d5bd
maximilian attems <attems@fias.uni-frankfurt.de>
2013-01-18 17:28:26 +0100

    add rudimentary FourVectorclass
    This will need more work to overload additions for example,
    for now it allows to set and access members.

---
+++ src/include/FourVector.h
@@ -0,0 +1,54 @@
+/*
+ *    Copyright (c) 2012-2013
+ *      maximilian attems <attems@fias.uni-frankfurt.de>
+ *      Jussi Auvinen <auvinen@fias.uni-frankfurt.de>
+ *    GNU General Public License (GPLv3)
+ */
+#ifndef SRC_INCLUDE_FOURVECTOR_H_
+#define SRC_INCLUDE_FOURVECTOR_H_
+
+class FourVector {
+  public:
+    /* default constructor */
+    FourVector(): x0_(0), x1_(0), x2_(0), x3_(0) {}
+    /* t, z, x_\perp */
+    double inline x0();
+    double inline x1();
+    double inline x2();
+    double inline x3();
+    void inline set_FourVector(double t, double z, double x, double y);
+    double Dot(FourVector);
+
+    /* overloaded operators */
+// XX:    FourVector operator+(const FourVector &a) const;
+
+  private:
+    double x0_, x1_, x2_, x3_;
+};
+
+double inline FourVector::x0(void) {
+	return x0_;
+}
+
+double inline FourVector::x1(void) {
+	return x1_;
+}
+
+double inline FourVector::x2(void) {
+	return x2_;
+}
+
+double inline FourVector::x3(void) {
+	return x3_;
+}
+
+void inline FourVector::set_FourVector(double t, double z, double x,
+		double y) {
+  x0_ = t;
+  x1_ = z;
+  x2_ = x;
+  x3_ = y;
+}
+
+
+#endif  // SRC_INCLUDE_FOURVECTOR_H_
