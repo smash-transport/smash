aeabe785cc93e222a72b486c134ff299549698f9
maximilian attems <attems@fias.uni-frankfurt.de>
2013-03-17 17:24:21 +0100

    FourVector: wire up Dot operation
    Needed for distance operation and what not.. (;
    
    While at it add better comments to FourVector.h.
    
    Signed-off-by: maximilian attems <attems@fias.uni-frankfurt.de>

--- src/include/FourVector.h
+++ src/include/FourVector.h
@@ -11,6 +11,7 @@ class FourVector {
   public:
     /* default constructor */
     FourVector(): x0_(0), x1_(0), x2_(0), x3_(0) {}
+    /* useful constructor */
     FourVector(double y0, double y1, double y2, double y3): x0_(y0),
       x1_(y1), x2_(y2), x3_(y3) {}
     /* t, z, x_\perp */
@@ -22,9 +23,11 @@ class FourVector {
     void inline set_x2(double x);
     double inline x3(void);
     void inline set_x3(double y);
+    /* set all four values */
     void inline set_FourVector(const double t, const double z, const double x,
       const double y);
-    double Dot(FourVector);
+    /* operations */
+    double Dot(const FourVector &a);
 
     /* overloaded operators */
     FourVector inline operator+=(const FourVector &a);
@@ -107,6 +110,10 @@ FourVector inline FourVector::operator*=(const double &a) {
   this->x2_ *= a;
   this->x3_ *= a;
   return *this;
+}
+
+double inline FourVector::Dot(const FourVector &a) {
+  return x0_ * a.x0_ - x1_ * a.x1_ - x2_ * a.x2_ - x3_ * a.x3_;
 }
 
 #endif  // SRC_INCLUDE_FOURVECTOR_H_
