c0d68a1522b2370f7a449adba2e9e3fbf27fe2e3
maximilian attems <attems@fias.uni-frankfurt.de>
2013-03-18 08:54:46 +0100

    FourVector: add three vector difference
    Again useful for q&d calculation and later on. (;
    
    Signed-off-by: maximilian attems <attems@fias.uni-frankfurt.de>

--- src/include/FourVector.h
+++ src/include/FourVector.h
@@ -28,6 +28,7 @@ class FourVector {
       const double y);
     /* operations */
     double Dot(const FourVector &a);
+    double DiffThree(const FourVector &a);
 
     /* overloaded operators */
     FourVector inline operator+=(const FourVector &a);
@@ -119,6 +120,10 @@ inline FourVector operator*(FourVector a, const double &b) {
 
 double inline FourVector::Dot(const FourVector &a) {
   return x0_ * a.x0_ - x1_ * a.x1_ - x2_ * a.x2_ - x3_ * a.x3_;
+}
+
+double inline FourVector::DiffThree(const FourVector &a) {
+  return x1_ - a.x1_ + x2_ - a.x2_ + x3_ - a.x3_;
 }
 
 #endif  // SRC_INCLUDE_FOURVECTOR_H_
