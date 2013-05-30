049bd27656861140ed9b8e516abf79535b2fad61
maximilian attems <attems@fias.uni-frankfurt.de>
2013-05-30 21:03:46 +0200

    input-particles, param-reader: Add full debug output
    This might be useful when tracking what was read.
    Also as a side bonus it squashes cppcheck style warning
    of unused variable.
    
    Signed-off-by: maximilian attems <attems@fias.uni-frankfurt.de>

--- src/input-particles.cc
+++ src/input-particles.cc
@@ -42,6 +42,8 @@ void input_particles(std::vector<ParticleType> *type, char *path) {
   printf("Processing %s/particles.txt.\n", path);
 
   while ((read = getline(&line, &len, fp)) != -1) {
+   dprintf("Retrieved particles.txt line of length %zu :\n", read);
+   dprintf("%s", line);
     /* Skip comments and blank lines */
     if (line[0] == '#' || line[0] == '\n' || line[0] == '\t' || line[0] == '/')
       continue;
--- src/param-reader.cc
+++ src/param-reader.cc
@@ -42,6 +42,8 @@ void process_params(Box *box, Parameters *parameters, char *path) {
   printf("Processing %s/params.txt.\n", path);
 
   while ((read = getline(&line, &len, fp)) != -1) {
+    dprintf("Retrieved params.txt line of length %zu :\n", read);
+    dprintf("%s", line);
     /* Skip comments and blank lines */
     if (line[0] == '#' || line[0] == '\n' || line[0] == '\t' || line[0] == '/')
       continue;
