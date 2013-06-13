27779686bfc15be137b29bda3a489e28481f67e6
Jussi Auvinen <auvinen@fias.uni-frankfurt.de>
2013-06-13 19:56:09 +0200

    cpplint code cleaning
    cpplint should not have any complains now.
    
    Signed-off-by: Jussi Auvinen <auvinen@fias.uni-frankfurt.de>

--- src/box.cc
+++ src/box.cc
@@ -114,8 +114,8 @@ static void check_collision_geometry(std::map<int, ParticleData> *particle,
 
       for (std::map<int, ParticleData>::iterator j; j != particle->end();
          ++j) {
-	/* exclude check on same particle */
-	if (i->first != j->first)
+        /* exclude check on same particle */
+        if (i->first != j->first)
           continue;
         /* The other particle has formed a resonance or has decayed
          * and is not active anymore
