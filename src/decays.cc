90cf7ba03bea425674b9f72e01de2f4ac195a25c
Jussi Auvinen <auvinen@fias.uni-frankfurt.de>
2013-06-13 16:55:24 +0200

    Modification of does_decay input parameters
    No point to give this function the whole particle and particletype lists, just
    give the data and type of the particle we're interested of.
    
    Signed-off-by: Jussi Auvinen <auvinen@fias.uni-frankfurt.de>

--- src/decays.cc
+++ src/decays.cc
@@ -16,18 +16,17 @@
 #include "include/constants.h"
 
 /* does_decay - does a resonance decay on this timestep? */
-bool does_decay(std::vector<ParticleData> *particle,
-  std::vector<ParticleType> *particle_type, std::map<int, int> *map_type,
-  std::list<int> *collision_list, const Parameters &parameters, int id_res) {
+bool does_decay(ParticleData *particle, ParticleType *particle_type,
+  std::list<int> *collision_list, const Parameters &parameters) {
   /* local rest frame velocity */
   FourVector velocity_lrf;
   velocity_lrf.set_x0(1.0);
-  velocity_lrf.set_x1((*particle)[id_res].momentum().x1()
-                      / (*particle)[id_res].momentum().x0());
-  velocity_lrf.set_x2((*particle)[id_res].momentum().x2()
-                      / (*particle)[id_res].momentum().x0());
-  velocity_lrf.set_x3((*particle)[id_res].momentum().x3()
-                      / (*particle)[id_res].momentum().x0());
+  velocity_lrf.set_x1(particle->momentum().x1()
+                      / particle->momentum().x0());
+  velocity_lrf.set_x2(particle->momentum().x2()
+                      / particle->momentum().x0());
+  velocity_lrf.set_x3(particle->momentum().x3()
+                      / particle->momentum().x0());
 
   /* The clock goes slower in the rest frame of the resonance */
   double inverse_gamma = velocity_lrf.Dot(velocity_lrf);
@@ -40,12 +39,11 @@ bool does_decay(std::vector<ParticleData> *particle,
    * = (1 - width * Delta_t)^(t / Delta_t)
    * -> exp(-width * t) when Delta_t -> 0
    */
-  if (drand48() < resonance_frame_timestep
-      * (*particle_type)[(*map_type)[id_res]].width() / hbarc) {
+  if (drand48() < resonance_frame_timestep * particle_type->width() / hbarc) {
     /* Time is up! Set the particle to decay at this timestep */
-    (*particle)[id_res].set_collision(2, 0.0, -1);
-    collision_list->push_back(id_res);
+    particle->set_collision(2, 0.0, -1);
+    collision_list->push_back(particle->id());
     return true;
-  } 
+  }
   return false;
 }
--- src/include/decays.h
+++ src/include/decays.h
@@ -10,16 +10,13 @@
 
 #include <cstdlib>
 #include <list>
-#include <map>
-#include <vector>
 
 class Parameters;
 class ParticleData;
 class ParticleType;
 
 /* does_decay - does a resonance decay on this timestep? */
-bool does_decay(std::vector<ParticleData> *particle,
-  std::vector<ParticleType> *particle_type, std::map<int, int> *map_type,
-  std::list<int> *collision_list, const Parameters &parameters, int id_res);
+bool does_decay(ParticleData *particle, ParticleType *particle_type,
+                std::list<int> *collision_list, const Parameters &parameters);
 
 #endif  // SRC_INCLUDE_DECAYS_H_
