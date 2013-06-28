7c350e0aa7158b76d40dc3e92087f02d8afdbdcd
maximilian attems <attems@fias.uni-frankfurt.de>
2013-06-28 11:59:02 +0200

    input-particles: Enhance debug output of input_particles()
    It has been the second time Jussi and me starred at what was
    going on here, so better be informative at what we get from
    the particles.txt input file in debug mode.
    
    While at it split superlong printf in 2 lines to better
    accomodate for "usual" length terminals. (;
    
    Signed-off-by: Jussi Auvinen <auvinen@fias.uni-frankfurt.de>
    Signed-off-by: maximilian attems <attems@fias.uni-frankfurt.de>

--- src/input-particles.cc
+++ src/input-particles.cc
@@ -42,45 +42,56 @@ void input_particles(std::vector<ParticleType> *type, char *path) {
   printf("Processing %s/particles.txt.\n", path);
 
   while ((read = getline(&line, &len, fp)) != -1) {
-    printd("Retrieved particles.txt line of length %zu :\n", read);
-    printd("%s", line);
+    printd("Retrieved particles.txt line of length %zu:\n", read);
     /* Skip comments and blank lines */
-    if (line[0] == '#' || line[0] == '\n' || line[0] == '\t' || line[0] == '/')
+    if (line[0] == '#' || line[0] == '\n' || line[0] == '\t'
+        || line[0] == '/') {
+      printd("Skipping line: %s", line);
       continue;
+    }
+    printd("line: %s", line);
 
     char *particle_name = strtok_r(line, sep, &saveptr);
     if (particle_name == NULL)
       continue;
+    printd("particle name: %s\n", particle_name);
     characters = strtok_r(NULL, sep, &saveptr);
     if (characters == NULL)
       continue;
     float mass = atof(characters);
+    printd("mass: %f\n", mass);
     characters = strtok_r(NULL, sep, &saveptr);
     if (characters == NULL)
       continue;
     float width = atof(characters);
+    printd("width: %f\n", width);
     characters = strtok_r(NULL, sep, &saveptr);
     if (characters == NULL)
       continue;
     int pdgcode = atoi(characters);
+    printd("pdgcode: %d\n", pdgcode);
     characters = strtok_r(NULL, sep, &saveptr);
     if (characters == NULL)
       continue;
     int isospin = atoi(characters);
+    printd("isospin: %d\n", isospin);
     characters = strtok_r(NULL, sep, &saveptr);
     if (characters == NULL)
       continue;
     int charge = atoi(characters);
+    printd("charge: %d\n", charge);
     characters = strtok_r(NULL, sep, &saveptr);
     if (characters == NULL)
       continue;
     int spin = atoi(characters);
+    printd("spin: %d\n", spin);
 
     /* Have a new particle type */
     (*type).resize(type_number + 1);
-    printf("Setting particle type %s mass %g width %g"
-           " pdgcode %i isospin %i charge %i spin %i\n", particle_name,
-           mass, width, pdgcode, isospin, charge, spin);
+    printf("Setting particle type %s mass %g width %g pdgcode %i\n",
+           particle_name, mass, width, pdgcode);
+    printf("Setting particle type %s isospin %i charge %i spin %i\n",
+           particle_name, isospin, charge, spin);
     std::string name(particle_name);
     (*type)[type_number].set(name, mass, width, pdgcode, isospin, charge, spin);
     type_number++;
