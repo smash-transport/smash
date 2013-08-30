75912fe74dd4175e3b5d2ccfacd3b680fee29173
maximilian attems <attems@fias.uni-frankfurt.de>
2013-08-30 16:27:57 +0200

    include: [ParticleData] minor beautification
    Be consistent with keywords.
    
    Signed-off-by: maximilian attems <attems@fias.uni-frankfurt.de>

--- src/include/ParticleData.h
+++ src/include/ParticleData.h
@@ -19,38 +19,38 @@ class ParticleData {
     collision_time_(0.0), process_type_(-1) {}
   explicit ParticleData(int i) :id_(i), pdgcode_(-1), id_partner_(-1),
     id_process_(-1), collision_time_(0.0), process_type_(-1) {}
-  void inline set(int id, const double &momenta_l, const double &momenta_t);
-  int id(void) const;
-  void inline set_id(int id);
-  int pdgcode(void) const;
-  void inline set_pdgcode(int pdgcode);
-  int id_partner(void) const;
-  void inline set_id_partner(int id_b);
-  int id_process(void) const;
-  void inline set_id_process(int id);
-  double collision_time(void) const;
-  int process_type(void) const;
-  void inline set_collision_time(const double &collision_time);
-  void inline set_collision(int process_type, const double &collision_time,
+  inline void set(int id, const double &momenta_l, const double &momenta_t);
+  inline int id(void) const;
+  inline void set_id(int id);
+  inline int pdgcode(void) const;
+  inline void set_pdgcode(int pdgcode);
+  inline int id_partner(void) const;
+  inline void set_id_partner(int id_b);
+  inline int id_process(void) const;
+  inline void set_id_process(int id);
+  inline double collision_time(void) const;
+  inline int process_type(void) const;
+  inline void set_collision_time(const double &collision_time);
+  inline void set_collision(int process_type, const double &collision_time,
                             int collision_id);
-  void inline set_collision_past(int process_id);
-  FourVector inline momentum(void) const;
-  void inline set_momentum(const FourVector &momentum_vector);
-  void inline set_momentum(const double &mass, const double &px,
+  inline void set_collision_past(int process_id);
+  inline FourVector momentum(void) const;
+  inline void set_momentum(const FourVector &momentum_vector);
+  inline void set_momentum(const double &mass, const double &px,
                            const double &py, const double &pz);
-  FourVector inline position(void) const;
-  void inline set_position(const FourVector &position);
-  void inline set_position(const double &x0, const double &x1,
+  inline FourVector position(void) const;
+  inline void set_position(const FourVector &position);
+  inline void set_position(const double &x0, const double &x1,
                            const double &x2, const double &x3);
   /* get velocities */
-  double inline velocity_x(void) { return momentum().x1() / momentum().x0(); }
-  double inline velocity_y(void) { return momentum().x2() / momentum().x0(); }
-  double inline velocity_z(void) { return momentum().x3() / momentum().x0(); }
+  inline double velocity_x(void) { return momentum().x1() / momentum().x0(); }
+  inline double velocity_y(void) { return momentum().x2() / momentum().x0(); }
+  inline double velocity_z(void) { return momentum().x3() / momentum().x0(); }
   /* overloaded operators */
-  bool inline operator==(const ParticleData &a) const;
-  bool inline operator<(const ParticleData &a) const;
-  bool inline operator==(int id_a) const;
-  bool inline operator<(int id_a) const;
+  inline bool operator==(const ParticleData &a) const;
+  inline bool operator<(const ParticleData &a) const;
+  inline bool operator==(int id_a) const;
+  inline bool operator<(int id_a) const;
 
   private:
     /* Each particle has a unique identifier */
@@ -72,59 +72,59 @@ class ParticleData {
 };
 
 /* look up the id of the particle */
-int inline ParticleData::id(void) const {
+inline int ParticleData::id(void) const {
   return id_;
 }
 
 /* set id of the particle */
-void inline ParticleData::set_id(int i) {
+inline void ParticleData::set_id(int i) {
   id_ = i;
 }
 
 /* look up the pdgcode of the particle */
-int inline ParticleData::pdgcode(void) const {
+inline int ParticleData::pdgcode(void) const {
   return pdgcode_;
 }
 
 /* set id of the particle */
-void inline ParticleData::set_pdgcode(int i) {
+inline void ParticleData::set_pdgcode(int i) {
   pdgcode_ = i;
 }
 
 /* look up the id of the collision partner */
-int inline ParticleData::id_partner(void) const {
+inline int ParticleData::id_partner(void) const {
   return id_partner_;
 }
 
 /* set the id of the collision partner */
-void inline ParticleData::set_id_partner(int id_b) {
+inline void ParticleData::set_id_partner(int id_b) {
   id_partner_ = id_b;
 }
 
 /* look up the id of the collision process */
-int inline ParticleData::id_process(void) const {
+inline int ParticleData::id_process(void) const {
   return id_process_;
 }
 
 /* set the id of the collision process */
-void inline ParticleData::set_id_process(int process_id) {
+inline void ParticleData::set_id_process(int process_id) {
   id_process_ = process_id;
 }
 
-double inline ParticleData::collision_time(void) const {
+inline double ParticleData::collision_time(void) const {
   return collision_time_;
 }
 
-int inline ParticleData::process_type(void) const {
+inline int ParticleData::process_type(void) const {
   return process_type_;
 }
 
-void inline ParticleData::set_collision_time(const double &collision_t) {
+inline void ParticleData::set_collision_time(const double &collision_t) {
   collision_time_ = collision_t;
 }
 
 /* set possible collision data */
-void inline ParticleData::set_collision(int proc_type,
+inline void ParticleData::set_collision(int proc_type,
   const double &collision_t, int id_b) {
   process_type_ = proc_type;
   collision_time_ = collision_t;
@@ -132,61 +132,61 @@ void inline ParticleData::set_collision(int proc_type,
 }
 
 /* set happened collision data */
-void inline ParticleData::set_collision_past(int id_counter) {
+inline void ParticleData::set_collision_past(int id_counter) {
   collision_time_ = 0.0;
   id_process_ = id_counter;
   id_partner_ = -1;
 }
 
-FourVector inline ParticleData::momentum(void) const {
+inline FourVector ParticleData::momentum(void) const {
   return momentum_;
 }
 
 /* set particle four momentum directly */
-void inline ParticleData::set_momentum(const FourVector &momentum_vector) {
+inline void ParticleData::set_momentum(const FourVector &momentum_vector) {
   momentum_ = momentum_vector;
 }
 
 /* set particle four momentum by components */
-void inline ParticleData::set_momentum(const double &mass, const double &px,
+inline void ParticleData::set_momentum(const double &mass, const double &px,
                           const double &py, const double &pz) {
   momentum_.set_FourVector(sqrt(mass * mass + px * px + py * py + pz * pz),
                            px, py, pz);
 }
 
 /* particle position in Minkowski space */
-FourVector inline ParticleData::position(void) const {
+inline FourVector ParticleData::position(void) const {
   return position_;
 }
 
 /* set the particle position directly */
-void inline ParticleData::set_position(const FourVector &pos) {
+inline void ParticleData::set_position(const FourVector &pos) {
   position_ = pos;
 }
 
 /* set the particle position by components */
-void inline ParticleData::set_position(const double &x0, const double &x3,
+inline void ParticleData::set_position(const double &x0, const double &x3,
                           const double &x1, const double &x2) {
   position_.set_FourVector(x0, x3, x1, x2);
 }
 
 /* check if the particles are identical */
-bool inline ParticleData::operator==(const ParticleData &a) const {
+inline bool ParticleData::operator==(const ParticleData &a) const {
   return this->id_ == a.id_;
 }
 
 /* sort particles along their id */
-bool inline ParticleData::operator<(const ParticleData &a) const {
+inline bool ParticleData::operator<(const ParticleData &a) const {
   return this->id_ < a.id_;
 }
 
 /* check if the particles are identical to a given id */
-bool inline ParticleData::operator==(int id_a) const {
+inline bool ParticleData::operator==(int id_a) const {
   return this->id_ == id_a;
 }
 
 /* sort particles along to given id */
-bool inline ParticleData::operator<(int id_a) const {
+inline bool ParticleData::operator<(int id_a) const {
   return this->id_ < id_a;
 }
 
