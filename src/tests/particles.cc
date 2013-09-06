4f4ed464ff6fefdd99233a675b6b74623fc12430
maximilian attems <attems@fias.uni-frankfurt.de>
2013-09-06 14:03:51 +0200

    tests: Particles add more tests
    While we are it make sure to test also the ParticleData iterator
    and member functions.
    Discussed with Jussi so to be sure the Particles class itself
    is well tested.
    
    This commit refs #30
    
    Signed-off-by: maximilian attems <attems@fias.uni-frankfurt.de>

--- src/tests/particles.cc
+++ src/tests/particles.cc
@@ -30,23 +30,24 @@ int main() {
   /* XXX: does this test NaN?? */
   if (distance_squared > 1000.0)
     return -2;
-  particle_a.set_momentum(0.1, 10.0, 9.0, 8.0);
-  particle_b.set_momentum(0.1, -10.0, -90.0, -80.0);
-  distance_squared = particle_distance(&particle_a, &particle_b);
-  if (distance_squared < 0.0)
-    return -3;
 
   /* check collision_time for parallel momenta => impossible collision */
   particle_a.set_momentum(0.1, 0.3, -0.1, 0.2);
   particle_b.set_momentum(0.1, 0.3, -0.1, 0.2);
   double time = collision_time(particle_a, particle_b);
   if (time >= 0.0)
-    return -4;
+    return -3;
 
+  /* reset momenta for possible collision and compare to Particle class */
   particle_a.set_momentum(0.1, 10.0, 9.0, 8.0);
   particle_b.set_momentum(0.1, -10.0, -90.0, -80.0);
+  distance_squared = particle_distance(&particle_a, &particle_b);
+  if (distance_squared < 0.0)
+    return -4;
+
   /* now check the Particles class itself */
   Particles particles;
+  /* check addition of particle types */
   ParticleType piplus("pi+", 0.13957, -1.0, 211, 1, 1, 0);
   particles.add_type(piplus, 211);
   if (particles.types_size() != 1)
@@ -72,21 +73,38 @@ int main() {
   particles.add_type(piminus, -211);
   if (particles.types_size() != 2)
     return -8;
+
+  /* check addition of particles */
   particles.add_data(particle_a);
   if (particles.size() != 1)
     return -9;
   particles.add_data(particle_b);
   if (particles.size() != 2)
     return -10;
-  printd_position(particle_a);
-  printd_position(particle_b);
+  type_size = 0;
+  for (std::map<int, ParticleData>::const_iterator
+       i = particles.cbegin(); i != particles.cend(); ++i) {
+    printd("id %d: pdg %d\n", i->first, i->second.pdgcode());
+    /* check that id and and own id are the same */
+    if (i->first != i->second.id())
+      return -11;
+    type_size++;
+  }
+  if (type_size != 2)
+    return -12;
+  if (particles.empty())
+    return -13;
+  if (particles.count(1) != 1)
+    return -14;
+
+  /* check useage particle data */
   double distance_squared_2 = particle_distance(particles.data_pointer(0),
     particles.data_pointer(1));
   printd("%g versus %g\n", distance_squared, distance_squared_2);
   if (distance_squared_2 < 0.0)
-    return -11;
+    return -15;
   if (distance_squared_2 - distance_squared > really_small)
-    return -12;
+    return -16;
 
   return 0;
 }
