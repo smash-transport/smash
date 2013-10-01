a7aad5f7c3ebf5cc0ce7e17680fb296a889feb87
Jussi Auvinen <auvinen@fias.uni-frankfurt.de>
2013-10-01 21:05:23 +0200

    CrossSections: Input file reader added
    input_cross_sections reads the cross-sections.txt and saves the parameter
    values to be used in the cross section calculations later in the simulation.
    
    Signed-off-by: Jussi Auvinen <auvinen@fias.uni-frankfurt.de>

---
+++ src/include/input-cross-sections.h
@@ -0,0 +1,18 @@
+/*
+ *    Copyright (c) 2013
+ *      maximilian attems <attems@fias.uni-frankfurt.de>
+ *      Jussi Auvinen <auvinen@fias.uni-frankfurt.de>
+ *
+ *    GNU General Public License (GPLv3)
+ */
+#ifndef SRC_INCLUDE_INPUT_CROSS_SECTIONS_H_
+#define SRC_INCLUDE_INPUT_CROSS_SECTIONS_H_
+
+/* forward declarations */
+class Particles;
+
+/* read cross section parametrizations */
+void input_cross_sections(CrossSections *cross_sections,
+ Particles *particles, char *path);
+
+#endif  // SRC_INCLUDE_INPUT_CROSS_SECTIONS_H_
