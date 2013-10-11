30cb24b7313b1a7caddc06b150cf35bad939b7b6
Jussi Auvinen <auvinen@fias.uni-frankfurt.de>
2013-10-11 12:25:29 +0200

    CrossSections cleanup
    Axed all the parametrization-related stuff from CrossSections class,
    and for now also the annihilation cross section.
    
    Signed-off-by: Jussi Auvinen <auvinen@fias.uni-frankfurt.de>

--- src/cross-sections.cc
+++ src/cross-sections.cc
@@ -10,7 +10,6 @@
 #include <cmath>
 #include <cstdio>
 #include <cstdlib>
-#include <vector>
 
 #include "include/CrossSections.h"
 #include "include/constants.h"
@@ -37,55 +36,6 @@ void CrossSections::compute_kinematics(Particles *particles,
                 / (2 * sqrt(squared_mass_b_)));
 }
 
-float CrossSections::parametrization_(std::vector<float> parameters) const {
-  switch (parameters.size() - 1) {
-  case 1:
-    /* just a constant */
-    return parameters[1];
-    break;
-  case 2:
-    /* A + B*mass_a/(mandelstam s - 2*mass_a^2 - 2*mass_b^2) */
-    return parameters[1] + parameters[2] * sqrt(squared_mass_a_)
-      / (mandelstam_s_ - 2 * squared_mass_a_ - 2 * squared_mass_b_);
-    break;
-  case 3:
-    /* A * p^n * exp[-B*(log(p))^2] */
-    return parameters[1] * pow(p_lab_, parameters[2])
-      * exp(-parameters[3] * log(p_lab_) * log(p_lab_));
-    break;
-  case 4:
-    /* A + B*(p-b)*exp(-C*p) */
-    return parameters[1] + parameters[2] * (p_lab_ - parameters[3])
-      * exp(-parameters[4] * p_lab_);
-    break;
-  case 5:
-    /* A + B*p^n + C*(log(p))^2 + D*log(p) */
-    return parameters[1] + parameters[2] * pow(p_lab_, parameters[3])
-      + parameters[4] * log(p_lab_) * log(p_lab_) + parameters[5] * log(p_lab_);
-    break;
-  case 7:
-    /* A + B*(abs(p+b))^k + C*(p+c)^m */
-    return parameters[1]
-      + parameters[2] * pow(fabs(p_lab_ + parameters[3]), parameters[4])
-      + parameters[5] * pow(p_lab_ + parameters[6], parameters[7]);
-  case 8:
-    /* A + B*{C + c*exp[-D*(p+d)^m]}^n */
-    return parameters[1] + parameters[2]
-      * pow(parameters[3] + parameters[4] * exp(-parameters[5]
-            * pow(p_lab_ + parameters[6], parameters[7])), parameters[8]);
-    break;
-  case 10:
-    /* A + B*(abs(p+b))^k + C*(p+c)^m + D*(p+d)^n */
-    return parameters[1]
-      + parameters[2] * pow(fabs(p_lab_ + parameters[3]), parameters[4])
-      + parameters[5] * pow(p_lab_ + parameters[6], parameters[7])
-      + parameters[8] * pow(p_lab_ + parameters[9], parameters[10]);
-    break;
-  default:
-    return 0.0;
-  }
-}
-
 float CrossSections::elastic(Particles *particles, int id_a, int id_b)
   const {
   const int spin_a = particles->type(id_a).spin(),
@@ -107,27 +57,6 @@ float CrossSections::elastic(Particles *particles, int id_a, int id_b)
   /* np-scattering */
   } else {
     return np_elastic(p_lab_, mandelstam_s_, sqrt(squared_mass_a_));
-  }
-}
-
-float CrossSections::annihilation(Particles *particles,
-                                  int id_a, int id_b) const {
-  const int spin_a = particles->type(id_a).spin(),
-    spin_b = particles->type(id_b).spin();
-  /* For now, the meson-meson and meson-baryon
-   * annihilation cross sections are zero
-   */
-  if (spin_a % 2 == 0 || spin_b % 2 == 0)
-    return 0.0;
-
-  /* For baryon-baryon, we have to check the parametrized cross sections */
-  if (particles->type(id_a).pdgcode() == -(particles->type(id_b).pdgcode())) {
-    size_t i = 0;
-    while (p_lab_ < (ppbar_annihilation_[i])[0])
-      i++;
-    return parametrization_(ppbar_annihilation_[i]);
-  } else {
-    return 0.0;
   }
 }
 
--- src/include/CrossSections.h
+++ src/include/CrossSections.h
@@ -8,8 +8,6 @@
 #ifndef SRC_INCLUDE_CROSSSECTIONS_H_
 #define SRC_INCLUDE_CROSSSECTIONS_H_
 
-#include <vector>
-
 #include "../include/Particles.h"
 
 class CrossSections {
@@ -17,20 +15,12 @@ class CrossSections {
   /* Default constructor */
   CrossSections() : elastic_parameter_(0.0), squared_mass_a_(-1.0),
                     squared_mass_b_(-1.0), mandelstam_s_(-1.0), p_lab_(-1.0) {}
-  /* Add the values to be used in parametrizations */
+  /* Fixed elastic cross section value */
   inline void add_elastic_parameter(const float parameter);
-  inline void add_pp_elastic(std::vector<float> parameters);
-  inline void add_pp_total(std::vector<float> parameters);
-  inline void add_pn_elastic(std::vector<float> parameters);
-  inline void add_pn_total(std::vector<float> parameters);
-  inline void add_ppbar_elastic(std::vector<float> parameters);
-  inline void add_ppbar_annihilation(std::vector<float> parameters);
-  inline void add_ppbar_total(std::vector<float> parameters);
   /* Compute kinematics */
   void compute_kinematics(Particles *particles, int id_a, int id_b);
   /* Return cross sections */
   float elastic(Particles *particles, int id_a, int id_b) const;
-  float annihilation(Particles *particles, int id_a, int id_b) const;
   float total(Particles *particles, int id_a, int id_b) const;
 
  private:
@@ -44,50 +34,12 @@ class CrossSections {
   double mandelstam_s_;
   /* "Beam" momentum */
   double p_lab_;
-  /* Member containing the parametrizations */
-  float parametrization_(std::vector<float> parameters) const;
-  /* Values to be used in parametrizations */
-  std::vector< std::vector<float> > pp_elastic_;
-  std::vector< std::vector<float> > pp_total_;
-  std::vector< std::vector<float> > pn_elastic_;
-  std::vector< std::vector<float> > pn_total_;
-  std::vector< std::vector<float> > ppbar_elastic_;
-  std::vector< std::vector<float> > ppbar_annihilation_;
-  std::vector< std::vector<float> > ppbar_total_;
 };
 
-/* Add the values to be used in parametrizations */
+/* Fixed elastic cross section value */
 inline void CrossSections::add_elastic_parameter(const float parameter) {
   elastic_parameter_ = parameter;
 }
 
-inline void CrossSections::add_pp_elastic(std::vector<float> parameters) {
-  pp_elastic_.push_back(parameters);
-}
-
-inline void CrossSections::add_pp_total(std::vector<float> parameters) {
-  pp_total_.push_back(parameters);
-}
-
-inline void CrossSections::add_pn_elastic(std::vector<float> parameters) {
-  pn_elastic_.push_back(parameters);
-}
-
-inline void CrossSections::add_pn_total(std::vector<float> parameters) {
-  pn_total_.push_back(parameters);
-}
-inline void CrossSections::add_ppbar_elastic(
-            std::vector<float> parameters) {
-  ppbar_elastic_.push_back(parameters);
-}
-
-inline void CrossSections::add_ppbar_annihilation(
-            std::vector<float> parameters) {
-  ppbar_annihilation_.push_back(parameters);
-}
-
-inline void CrossSections::add_ppbar_total(std::vector<float> parameters) {
-  ppbar_total_.push_back(parameters);
-}
 
 #endif  // SRC_INCLUDE_CROSSSECTIONS_H_
