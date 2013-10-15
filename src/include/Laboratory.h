cb6b46b382aeb20c5098c9d67aa69c04aa9f8f2a
maximilian attems <attems@fias.uni-frankfurt.de>
2013-10-15 16:36:58 +0200

    include: [Laboratory.h] add modus field
    We want to be able to distinguish in which modus
    smash should run.
    
    Signed-off-by: maximilian attems <attems@fias.uni-frankfurt.de>

--- src/include/Laboratory.h
+++ src/include/Laboratory.h
@@ -14,26 +14,30 @@ class Laboratory {
     Laboratory(): steps_(10000), output_interval_(100), initial_condition_(1),
       testparticles_(1), eps_(0.001f), cross_section_(10.0f), seed_(1) {}
     /* member funtions */
-    int inline steps() const;
-    void inline set_steps(const int &STEPS);
-    int inline output_interval() const;
-    void inline set_output_interval(const int &UPDATE);
-    int inline initial_condition() const;
-    void inline set_initial_condition(const int &INITIAL_CONDITION);
-    int inline testparticles() const;
-    void inline set_testparticles(const int &TESTPARTICLES);
-    float inline eps() const;
-    void inline set_eps(const float &EPS);
-    float inline cross_section() const;
-    void inline set_cross_section(const float &sigma);
-    int64_t inline seed() const;
-    void inline set_seed(const int64_t &RANDOMSEED);
+    inline int steps() const;
+    inline void set_steps(const int &STEPS);
+    inline int output_interval() const;
+    inline void set_output_interval(const int &UPDATE);
+    inline int modus() const;
+    inline void set_modus(const int &MODUS);
+    inline int initial_condition() const;
+    inline void set_initial_condition(const int &INITIAL_CONDITION);
+    inline int testparticles() const;
+    inline void set_testparticles(const int &TESTPARTICLES);
+    inline float eps() const;
+    inline void set_eps(const float &EPS);
+    inline float cross_section() const;
+    inline void set_cross_section(const float &sigma);
+    inline int64_t seed() const;
+    inline void set_seed(const int64_t &RANDOMSEED);
 
   private:
     /* number of steps */
     int steps_;
     /* number of steps before giving measurables */
     int output_interval_;
+    /* box or sphere or .. */
+    int modus_;
     /* initial condition */
     int initial_condition_;
     /* number of test particle */
@@ -47,32 +51,42 @@ class Laboratory {
 };
 
 /* return the number of steps */
-int inline Laboratory::steps(void) const {
+inline int Laboratory::steps(void) const {
   return steps_;
 }
 
 /* set the number of steps */
-void inline Laboratory::set_steps(const int &STEPS) {
+inline void Laboratory::set_steps(const int &STEPS) {
   steps_ = STEPS;
 }
 
 /* return the number on which interval output will be shown */
-int inline Laboratory::output_interval(void) const {
+inline int Laboratory::output_interval(void) const {
   return output_interval_;
 }
 
 /* set when to output physics */
-void inline Laboratory::set_output_interval(const int &update) {
+inline void Laboratory::set_output_interval(const int &update) {
   output_interval_ = update;
 }
 
+/* return the modus in use */
+inline int Laboratory::modus(void) const {
+  return modus_;
+}
+
+/* set the modus */
+inline void Laboratory::set_modus(const int &MODUS) {
+  modus_ = MODUS;
+}
+
 /* return the used initial condition */
-int inline Laboratory::initial_condition(void) const {
+inline int Laboratory::initial_condition(void) const {
   return initial_condition_;
 }
 
 /* set the initial condition */
-void inline Laboratory::set_initial_condition(const int &INITIAL_CONDITION) {
+inline void Laboratory::set_initial_condition(const int &INITIAL_CONDITION) {
   initial_condition_ = INITIAL_CONDITION;
 }
 
@@ -80,42 +94,42 @@ void inline Laboratory::set_initial_condition(const int &INITIAL_CONDITION) {
 /* return the number of testparticles:
  * if equal to one a "testparticle" corresponds to a real particle
  */
-int inline Laboratory::testparticles(void) const {
+inline int Laboratory::testparticles(void) const {
   return testparticles_;
 }
 
 /* set the number of test particles */
-void inline Laboratory::set_testparticles(const int &TESTPARTICLES) {
+inline void Laboratory::set_testparticles(const int &TESTPARTICLES) {
   testparticles_ = TESTPARTICLES;
 }
 
 /* return the time step in use */
-float inline Laboratory::eps(void) const {
+inline float Laboratory::eps(void) const {
   return eps_;
 }
 
 /* set the time step */
-void inline Laboratory::set_eps(const float &EPS) {
+inline void Laboratory::set_eps(const float &EPS) {
   eps_ = EPS;
 }
 
 /* return the cross section */
-float inline Laboratory::cross_section(void) const {
+inline float Laboratory::cross_section(void) const {
   return cross_section_;
 }
 
 /* set the cross section */
-void inline Laboratory::set_cross_section(const float &sigma) {
+inline void Laboratory::set_cross_section(const float &sigma) {
   cross_section_ = sigma;
 }
 
 /* return the random seed */
-int64_t inline Laboratory::seed(void) const {
+inline int64_t Laboratory::seed(void) const {
   return seed_;
 }
 
 /* set the seed */
-void inline Laboratory::set_seed(const int64_t &randomseed) {
+inline void Laboratory::set_seed(const int64_t &randomseed) {
   seed_ = randomseed;
 }
 
