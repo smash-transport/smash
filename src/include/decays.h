1e8c9c76b4a11da05160d0e16f756f15ff943cae
Jussi Auvinen <auvinen@fias.uni-frankfurt.de>
2013-11-27 13:27:03 +0100

    Resonances+decays: Restructuring the source files
    Moved some decay-related stuff from resonances.cc to decays.cc.
    
    Signed-off-by: Jussi Auvinen <auvinen@fias.uni-frankfurt.de>

--- src/include/decays.h
+++ src/include/decays.h
@@ -24,4 +24,14 @@ void check_decays(Particles *particles, std::list<int> *decay_list,
 size_t decay_particles(Particles *particles, std::list<int> *decay_list,
                        size_t id_process);
 
+/* resonance decay process */
+int resonance_decay(Particles *particles, int particle_id);
+
+/* 1->2 process kinematics */
+int one_to_two(Particles *particles, int resonance_id, int type_a, int type_b);
+
+/* 1->3 process kinematics */
+int one_to_three(Particles *particles, int resonance_id,
+                 int type_a, int type_b, int type_c);
+
 #endif  // SRC_INCLUDE_DECAYS_H_
--- src/include/resonances.h
+++ src/include/resonances.h
@@ -30,16 +30,6 @@ std::map<int, double> resonance_cross_section(
   const ParticleType &type_particle1, const ParticleType &type_particle2,
   Particles *particles);
 
-/* 1->2 process kinematics */
-int one_to_two(Particles *particles, int resonance_id, int type_a, int type_b);
-
-/* 1->3 process kinematics */
-int one_to_three(Particles *particles, int resonance_id,
-                 int type_a, int type_b, int type_c);
-
-/* resonance decay process */
-int resonance_decay(Particles *particles, int particle_id);
-
 /* 2->1 resonance formation process */
 int resonance_formation(Particles *particles, int particle_id, int other_id,
   int resonance_type);
