9d2a4a3aa2a8a47f58ce364fcfefc1cac7fe74f7
Jussi Auvinen <auvinen@fias.uni-frankfurt.de>
2013-12-03 15:19:08 +0100

    DecayModes: ProcessBranch utilization added
    DecayModes now uses ProcessBranch structure.
    
    Signed-off-by: Jussi Auvinen <auvinen@fias.uni-frankfurt.de>

--- src/include/DecayModes.h
+++ src/include/DecayModes.h
@@ -9,43 +9,49 @@
 #define SRC_INCLUDE_DECAYMODES_H_
 
 #include <cstdio>
-#include <utility>
 #include <vector>
+
+#include "../include/ProcessBranch.h"
 
 class DecayModes {
  public:
   /* Add a decay mode */
   inline void add_mode(std::vector<int> particles, float ratio);
+  inline void add_mode(ProcessBranch mode);
   /* Make sure ratios add to 1 */
   inline void renormalize(float renormalization_constant);
   /* Remove all modes */
   inline void clear(void);
   /* Pass out the decay modes */
-  inline std::vector< std::pair<std::vector<int>, float> >
-    decay_mode_list(void) const;
+  inline std::vector<ProcessBranch> decay_mode_list(void) const;
  private:
   /* Vector of decay modes.
    * Each mode consists of a vector of the pdg codes of decay products
    * and a ratio of this decay mode compared to all possible modes
    */
-  std::vector< std::pair<std::vector<int>, float> > decay_modes_;
+  std::vector<ProcessBranch> decay_modes_;
 };
 
 /* Add a decay mode */
 inline void DecayModes::add_mode(std::vector<int> particles, float ratio) {
-  std::pair<std::vector<int>, float> decay_mode
-    = std::make_pair(particles, ratio);
-  decay_modes_.push_back(decay_mode);
+  ProcessBranch branch;
+  branch.add_particles(particles);
+  branch.add_ratio(ratio);
+  decay_modes_.push_back(branch);
+}
+
+inline void DecayModes::add_mode(ProcessBranch branch) {
+  decay_modes_.push_back(branch);
 }
 
 /* Make sure ratios add to 1 */
 inline void DecayModes::renormalize(float renormalization_constant) {
   printf("Renormalizing decay modes with %g \n", renormalization_constant);
   float new_sum = 0.0;
-  for (std::vector< std::pair<std::vector<int>, float> >::iterator mode
+  for (std::vector<ProcessBranch>::iterator mode
          = decay_modes_.begin(); mode != decay_modes_.end(); ++mode) {
-    mode->second = mode->second / renormalization_constant;
-    new_sum += mode->second;
+    mode->add_ratio(mode->ratio() / renormalization_constant);
+    new_sum += mode->ratio();
   }
   printf("After renormalization sum of ratios is %g. \n", new_sum);
 }
@@ -56,8 +62,7 @@ inline void DecayModes::clear(void) {
 }
 
 /* Pass out the decay modes */
-inline std::vector< std::pair<std::vector<int>, float> >
-  DecayModes::decay_mode_list(void) const {
+inline std::vector<ProcessBranch> DecayModes::decay_mode_list(void) const {
   return decay_modes_;
 }
 
