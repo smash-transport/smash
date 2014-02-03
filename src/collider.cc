ec4d509e6f7d726a6f63023eda5d4d3b60237b8b
Hannah Petersen <petersen@fias.uni-frankfurt.de>
2014-02-03 13:50:23 +0100

    Collider: starting time fixed
    The initial time has been set to 1.0 as in the box
    Signed-off-by: Hannah Petersen <petersen@fias.uni-frankfurt.de>

--- src/collider.cc
+++ src/collider.cc
@@ -75,10 +75,10 @@ void ColliderModus::initial_conditions(Particles *particles) {
     cms_gamma = sqrts_ / mass;
     cms_beta = sqrt(sqrts_ * sqrts_ - mass * mass / sqrts_);
     if (i->first == 0) {
-     i->second.set_position(0.0, 0.0, 0.0, -1.0);
+     i->second.set_position(1.0, 0.0, 0.0, -1.0);
      i->second.set_momentum(mass, 0.0, 0.0, cms_gamma * cms_beta * mass);
     } else if (i->first == 1) {
-     i->second.set_position(0.0, 0.0, 0.0, 1.0);
+     i->second.set_position(1.0, 0.0, 0.0, 1.0);
      i->second.set_momentum(mass, 0.0, 0.0, - cms_gamma *cms_beta * mass);
     }
   }
