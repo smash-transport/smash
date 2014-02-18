fbc6fcd26a3a0fee63fa72a444c25c681b8340c2
Matthias Kretz <kretz@kde.org>
2014-02-18 16:35:28 +0100

    config: testing how config merging can be done
    I implemented a test to ensure the way I use yaml-cpp yields the
    expected result: namely existing keys get overwritten and new ones
    merged in.
    
    Signed-off-by: Matthias Kretz <kretz@kde.org>

---
+++ src/tests/yamltest.cc
@@ -0,0 +1,59 @@
+/*
+ *
+ *    Copyright (c) 2014
+ *      SMASH Team
+ *
+ *    GNU General Public License (GPLv3 or later)
+ *
+ */
+
+#include "tests/unittest.h"
+
+#include <yaml-cpp/yaml.h>
+#include <iostream>
+
+namespace YAML {
+static Node operator|=(Node a, const Node &b) {
+  if (b.IsMap()) {
+    for (auto n0 : b) {
+      a[n0.first.Scalar()] |= n0.second;
+    }
+  } else {
+    a = b;
+  }
+  return a;
+}
+}  // namespace YAML
+
+TEST(merge) {
+  std::string yaml0 =
+      "Foo:\n"
+      "  Bar: Hallo\n"
+      "  Baz: 1.2\n"
+      "  Length: 1.2\n"
+      "  Height: 21.8\n"
+      "Test: 1\n";
+  std::string yaml1 =
+      "Foo:\n"
+      "  Bar: Hallo Welt\n"
+      "  Length: [1, 2, 3]\n"
+      "  Extra:\n"
+      "    radius: 5.2\n"
+      "    height: 1.3\n"
+      "Test: 2\n";
+  YAML::Node a = YAML::Load(yaml0);
+  YAML::Node b = YAML::Load(yaml1);
+  std::cout << a << "\n\n";
+  a |= b;
+  std::cout << a << "\n\n";
+
+  const auto foo = a["Foo"];
+  const auto extra = foo["Extra"];
+  COMPARE(foo["Bar"].Scalar(), "Hallo Welt");
+  COMPARE(foo["Baz"].Scalar(), "1.2");
+  VERIFY(foo["Length"].as<std::vector<int>>() == (std::vector<int>{1, 2, 3}));
+  COMPARE(foo["Height"].Scalar(), "21.8");
+  COMPARE(extra["radius"].Scalar(), "5.2");
+  COMPARE(extra["height"].Scalar(), "1.3");
+  COMPARE(a["Test"].Scalar(), "2");
+}
