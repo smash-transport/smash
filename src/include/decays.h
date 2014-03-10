247f516a49b4a9dad95fd9e2cad3b2fa7476b7d9
Jussi Auvinen <auvinen@fias.uni-frankfurt.de>
2014-03-10 17:31:15 +0100

    Doxygen: decays.h
    Added doxygen documentation to decays.h.
    
    Signed-off-by: Jussi Auvinen <auvinen@fias.uni-frankfurt.de>

--- src/include/decays.h
+++ src/include/decays.h
@@ -4,6 +4,12 @@
  *
  *    GNU General Public License (GPLv3 or later)
  */
+
+/**
+ * \file decays.h
+ * Functions related to resonance decays.
+ */
+
 #ifndef SRC_INCLUDE_DECAYS_H_
 #define SRC_INCLUDE_DECAYS_H_
 
@@ -18,20 +24,46 @@ namespace Smash {
 
 class ModusDefault;
 
-/* does_decay - does a resonance decay on this timestep? */
+/**
+ * Check resonances for decays in this time step.
+ *
+ * Go through the existing resonances and create a list
+ * of those which will decay in this time step.
+ */
 void check_decays(Particles *particles, std::list<int> *decay_list,
                   const float timestep);
 
+/**
+ * Process the decay list.
+ *
+ * Go through the list of resonances decaying in this time step.
+ * Boost to the resonance rest frame, do the decay,
+ * boost back the product particles and remove the decayed particle
+ * from the active particles data structure.
+ */
 size_t decay_particles(Particles *particles, std::list<int> *decay_list,
                        size_t id_process);
 
-/* resonance decay process */
+
+/// Given a resonance, select one of its decay modes and do the decay
 int resonance_decay(Particles *particles, int particle_id);
 
-/* 1->2 process kinematics */
+/**
+ * Kinematics of a 1-to-2 decay process.
+ *
+ * Given a resonance and the types of decay product particles,
+ * sample the momenta and position of the products and add them
+ * to the active particles data structure.
+ */
 int one_to_two(Particles *particles, int resonance_id, int type_a, int type_b);
 
-/* 1->3 process kinematics */
+/**
+ * Kinematics of a 1-to-3 decay process.
+ *
+ * Given a resonance and the types of decay product particles,
+ * sample the momenta and position of the products and add them
+ * to the active particles data structure.
+ */
 int one_to_three(Particles *particles, int resonance_id,
                  int type_a, int type_b, int type_c);
 
