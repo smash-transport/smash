874ea1877e168a0f1d56ba15f01f7ed86fc4fd9a
Jussi Auvinen <auvinen@fias.uni-frankfurt.de>
2014-03-10 17:01:49 +0100

    Doxygen: resonances.h
    Added doxygen documentation for resonances.h.
    
    Signed-off-by: Jussi Auvinen <auvinen@fias.uni-frankfurt.de>

--- src/include/resonances.h
+++ src/include/resonances.h
@@ -4,6 +4,12 @@
  *
  *    GNU General Public License (GPLv3 or later)
  */
+
+/**
+ * \file resonances.h
+ * Functions related to resonance production.
+ */
+
 #ifndef SRC_INCLUDE_RESONANCES_H_
 #define SRC_INCLUDE_RESONANCES_H_
 
@@ -18,52 +24,68 @@ class ParticleData;
 class ParticleType;
 class ProcessBranch;
 
-/* calculate_minimum_mass
- * - calculate the minimum rest energy the resonance must have
- * to be able to decay through any of its decay channels
+/**
+ * The minimum mass of the resonance.
+ *
+ * Calculate the minimum rest energy the resonance must have
+ * to be able to decay through any of its decay channels.
  * NB: This function assumes stable decay products!
  */
 float calculate_minimum_mass(Particles *particles, int pdgcode);
 
-/* resonance_cross_section - energy-dependent cross section
- * for producing a resonance
+/**
+ * Find all resonances and their production cross sections.
+ *
+ * Given two particles, create a list of possible resonance
+ * production processes and their cross sections.
+ * Process can be either 2-to-1 or 2-to-2.
  */
 std::vector<ProcessBranch> resonance_cross_section(
   const ParticleData &particle1, const ParticleData &particle2,
   const ParticleType &type_particle1, const ParticleType &type_particle2,
   Particles *particles);
 
-/* two_to_one_formation -- only the resonance in the final state */
+/**
+ * Given two initial particles and a resonance,
+ * compute the 2-to-1 resonance production cross section.
+ */
 double two_to_one_formation(Particles *particles,
   const ParticleType &type_particle1,
   const ParticleType &type_particle2, const ParticleType &type_resonance,
   double mandelstam_s, double cm_momentum_squared);
 
-/* two_to_two_formation -- resonance and another particle in final state */
+/**
+ * Given two initial particles and a resonance,
+ * compute cross sections for all possible 2-to-2 processes
+ * with that resonance in the final state.
+ */
 size_t two_to_two_formation(Particles *particles,
   const ParticleType &type_particle1,
   const ParticleType &type_particle2, const ParticleType &type_resonance,
   double mandelstam_s, double cm_momentum_squared,
   std::vector<ProcessBranch> *process_list);
 
-/* Function for 1-dimensional GSL integration  */
+/// Function for 1-dimensional GSL integration.
 void quadrature_1d(double (*integrand_function)(double, void*),
                    std::vector<double> *parameters,
                    double lower_limit, double upper_limit,
                    double *integral_value, double *integral_error);
 
-/* Spectral function of the resonance */
+/// Spectral function of the resonance.
 double spectral_function(double resonance_mass, double resonance_pole,
                          double resonance_width);
 
-/* Spectral function integrand for GSL integration */
+/// Spectral function integrand for GSL integration.
 double spectral_function_integrand(double mandelstam_s, void * parameters);
 
-/* Resonance mass sampling for 2-particle final state */
+/**
+ * Resonance mass sampling for 2-particle final state
+ * with one resonance and one stable particle.
+ */
 double sample_resonance_mass(Particles *particles, int pdg_resonance,
   int pdg_stable, double cms_energy);
 
-/* 2->1 resonance formation process */
+/// 2-to-1 resonance formation process.
 int resonance_formation(Particles *particles, int particle_id, int other_id,
   std::vector<int> produced_particles);
 
