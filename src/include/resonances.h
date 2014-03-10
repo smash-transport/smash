e34d1e4fc1a2a10241283f89614f021884c49ecf
Jussi Auvinen <auvinen@fias.uni-frankfurt.de>
2014-03-10 18:45:18 +0100

    Doxygen: resonances.h: param and return descriptions
    Added input parameter and return value descriptions for
    the most unclear cases.
    
    Signed-off-by: Jussi Auvinen <auvinen@fias.uni-frankfurt.de>

--- src/include/resonances.h
+++ src/include/resonances.h
@@ -39,6 +39,11 @@ float calculate_minimum_mass(Particles *particles, int pdgcode);
  * Given two particles, create a list of possible resonance
  * production processes and their cross sections.
  * Process can be either 2-to-1 or 2-to-2.
+ *
+ * \return A list of processes with resonance in the final state.
+ *  Each element in the list contains the type(s)
+ *  of the final state particle(s)
+ *  and the cross section for that particular process.
  */
 std::vector<ProcessBranch> resonance_cross_section(
   const ParticleData &particle1, const ParticleData &particle2,
@@ -58,6 +63,9 @@ double two_to_one_formation(Particles *particles,
  * Given two initial particles and a resonance,
  * compute cross sections for all possible 2-to-2 processes
  * with that resonance in the final state.
+ *
+ * \return The number of possible processes. Also adds elements
+ * to the process_list.
  */
 size_t two_to_two_formation(Particles *particles,
   const ParticleType &type_particle1,
@@ -65,7 +73,16 @@ size_t two_to_two_formation(Particles *particles,
   double mandelstam_s, double cm_momentum_squared,
   std::vector<ProcessBranch> *process_list);
 
-/// Function for 1-dimensional GSL integration.
+/**
+ * Function for 1-dimensional GSL integration.
+ *
+ * \param integrand_function Function of 1 variable to be integrated over.
+ * \param parameters Container for possible parameters needed by the integrand.
+ * \param lower_limit Lower limit of the integral.
+ * \param upper_limit Upper limit of the integral.
+ * \param integral_value Result of integration.
+ * \param integral_error Uncertainty of the result.
+ */
 void quadrature_1d(double (*integrand_function)(double, void*),
                    std::vector<double> *parameters,
                    double lower_limit, double upper_limit,
@@ -75,8 +92,20 @@ void quadrature_1d(double (*integrand_function)(double, void*),
 double spectral_function(double resonance_mass, double resonance_pole,
                          double resonance_width);
 
-/// Spectral function integrand for GSL integration.
-double spectral_function_integrand(double mandelstam_s, void * parameters);
+/**
+ * Spectral function integrand for GSL integration.
+ *
+ * The integrand is \f$2m A(m) p_{cm}^f\f$, where \f$m\f$ is the
+ * resonance mass, \f$A(m)\f$ is the spectral function
+ *  and \f$p_{cm}^f\f$ is the center-of-mass momentum of the final state.
+ *
+ * \param resonance_mass Actual mass of the resonance.
+ * \param parameters Container for the parameters needed
+ * by the spectral function: Width of the resonance,
+ * pole mass of the resonance, mass of the stable particle in the final state
+ * and mandelstam-s of the process.
+ */
+double spectral_function_integrand(double resonance_mass, void * parameters);
 
 /**
  * Resonance mass sampling for 2-particle final state
@@ -85,7 +114,18 @@ double spectral_function_integrand(double mandelstam_s, void * parameters);
 double sample_resonance_mass(Particles *particles, int pdg_resonance,
   int pdg_stable, double cms_energy);
 
-/// 2-to-1 resonance formation process.
+/**
+ * Resonance formation process.
+ *
+ * Creates one or two new particles, of which
+ * one is a resonance.
+ *
+ * \param particles Particles in the simulation.
+ * \param particle_id ID of the first initial state particle.
+ * \param other_id ID of the second initial state particle.
+ * \param produced_particles Final state particle type(s).
+ * \return ID of the (first) new particle.
+ */
 int resonance_formation(Particles *particles, int particle_id, int other_id,
   std::vector<int> produced_particles);
 
