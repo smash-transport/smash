4bc9d1817eabe5dcdf2dd6ca2f8b7f161de182b5
Bjørn Bäuchle <baeuchle@th.physik.uni-frankfurt.de>
2014-03-12 12:21:21 +0100

    Woods-Saxon: New test to check/demonstrate centering
    test woods-saxon demonstrates the difference between the direct
    woods-saxon distribution and that when each nucleus is separately
    centered. Thus, we can estimate the difference between a run with 1
    testparticle and N testparticles (the direct distribution will for
    N_runs -> infinity yield the same result independent of the number of
    testparticles, while the centered one will differ).
    
    SPOILER ALERT:
    
    The centered distribution is a little more narrow than the direct
    distribution.
    
    This refs #404
    
    Signed-off-by: Bjørn Bäuchle <baeuchle@th.physik.uni-frankfurt.de>

---
+++ src/tests/woods-saxon.cc
@@ -0,0 +1,112 @@
+/*
+ *
+ *    Copyright (c) 2014
+ *      SMASH Team
+ *
+ *    GNU General Public License (GPLv3 or later)
+ *
+ */
+#include "tests/unittest.h"
+
+#include <map>
+#include "include/nucleus.h"
+
+using namespace Smash;
+
+// we'll have equal particle tyoes for now so that center() ==
+// center_of_mass (which is not yet implemented).
+std::map<int, int> list = {{2212, 208}};
+
+constexpr bool PRINT = false;
+
+// we want to find the difference between initializing a vanilla
+// woods-saxon distribution and a centered distribution, where the
+// vanilla distribution is shifted so that its center-of-mass is at
+// 0/0/0.
+TEST(woods_saxon) {
+  // this is where we store the distributions. Components (0,1,2,3) =
+  // (x,y,z,r)
+  std::map<int, int> hist_vanilla[4] {};
+  std::map<int, int> hist_centerd[4] {};
+  char name[4] = {'x','y','z','r'};
+  // binning width for the distribution:
+  constexpr float dx = 0.05;
+  // this is the number of nuclei we create.
+  constexpr int N_TEST = 1000000;
+  for (int i = 0; i < N_TEST; i++) {
+    // initialize nucleus.
+    Nucleus projectile;
+    projectile.fill_from_list(list, 1);
+    projectile.arrange_nucleons();
+    // com = center of mass.
+    FourVector com = projectile.center();
+    // for all particles in the nucleus, save position in histograms.
+    for (auto p : projectile) {
+      double r = sqrt(-p.position().DotThree());
+      ++hist_vanilla[0][floor(p.position().x1()/dx)];
+      ++hist_vanilla[1][floor(p.position().x2()/dx)];
+      ++hist_vanilla[2][floor(p.position().x3()/dx)];
+      ++hist_vanilla[3][r/dx];
+      // we'll "center" the nucleus "by hand", i.e., we subtract com by
+      // hand instead of doing it in a dedicated function / loop.
+      FourVector centered = p.position() - com;
+      double R = sqrt(-centered.DotThree());
+      ++hist_centerd[0][floor(centered.x1()/dx)];
+      ++hist_centerd[1][floor(centered.x2()/dx)];
+      ++hist_centerd[2][floor(centered.x3()/dx)];
+      ++hist_centerd[3][R/dx];
+    }
+  }
+  constexpr int sigmabins = 4;
+  // these are all a little smaller than erf((i+1)/sqrt(2)).
+  constexpr double allowed[sigmabins] = {.682*.99, .954*.99, .997*.99, 1.0};
+  // mnemonic: c = component
+  for (int c = 0; c < 4; ++c) {
+    int diffbad[sigmabins] = {0};
+    int total = 0;
+    for (auto b : hist_vanilla[c]) {
+      // entries from vanilla distribution
+      int vanilla = b.second;
+      // corresponding entry in centered distribution
+      int centerd = hist_centerd[c][b.first];
+      // In order to make sensible comparisons, we require the number of
+      // entries in both histograms to be >= 10.
+      if (vanilla < 10 || centerd < 10) {
+        continue;
+      }
+      // margin we allow. The statistical error at each point is
+      // sqrt(N) (= N/sqrt(N)); the statistical error for the difference
+      // between two points is hence sqrt(N1 + N2).
+      double margin = sqrt(vanilla + centerd);
+      // we'll make another histogram from the errors divided by the
+      // standard deviation (stored in margin).
+      int diffbin = std::abs(vanilla - centerd)/margin;
+      // everything with a larger deviation than sigmabins*margin is
+      // collected in highest bin.
+      diffbin = (diffbin >= sigmabins) ? sigmabins - 1 : diffbin;
+      ++diffbad[diffbin];
+      ++total;
+      // maybe we want to print the distributions:
+      if (PRINT) {
+        printf("%d %7.3f %8d %8d\n", c, b.first*dx, vanilla, centerd);
+      }
+    }
+    // if we don't want to print the distributions, then we want to
+    // verify them.
+    if (!PRINT) {
+      // this is the integral over diffbad.
+      int totalbad = 0;
+      // now, we check diffbad one by one (don't need to check the last
+      // one)
+      for (int unit = 0; unit < sigmabins - 1; ++unit) {
+        totalbad += diffbad[unit];
+        double fraction = (totalbad + 0.0) / (total + 0.0);
+        VERIFY(fraction > allowed[unit]) << " too few entries have less than "
+                  << unit+1 << " sigma deviation ("
+                  << totalbad << "/" << total << "=" << fraction
+                  << ", required minimal fraction: " << allowed[unit]
+                  << " at component " << name[c];
+      }
+    }
+  }
+}
