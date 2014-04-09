a9d45c0b3092366e26dbe65a1b56a9a0041eac6b
Bjørn Bäuchle <baeuchle@th.physik.uni-frankfurt.de>
2014-04-09 17:16:42 +0200

    PDGCode: First Quantum numbers
    Class PDGCode can:
        - convert to an integer
        - dump the bits (although I'm certain that can be done better)
        - extract quantum numbers:
            - Isospin
            - Strangeness
            - Charmness
            - Bottomness
            - (Topness, B'ness, T'ness)
            - Charge
    
    See #532
    
    Signed-off-by: Bjørn Bäuchle <baeuchle@th.physik.uni-frankfurt.de>

--- src/include/pdgcode.h
+++ src/include/pdgcode.h
@@ -48,33 +48,43 @@ class PDGCode {
   //   codestring >> std::hex >> pdgcode_;
   // }
   PDGCode(const int codenumber) {
-    unsigned int bitmask = 0xf;
-    int abscode = std::abs(codenumber);
-    printf("bitmask: 0x%08x\nabscode: 0x%08x\nVALUE:   0x%08x\n", bitmask, abscode, abscode & bitmask);
+    unsigned int abscode = std::abs(codenumber);
+    antiparticle_ = codenumber < 0;
+    set_fields(abscode);
+    int test = test_code();
+    if (test > 0) {
+      throw InvalidPDGCode("Invalid digits in PDG Code " + std::to_string(code()));
+     // throw InvalidPDGCode(std::string("Invalid digits in PDG Code ")
+     //                    + code() + std::string(" they need to be Hex-encoded!"));
+    }
+  }
+  PDGCode(unsigned int abscode) {
+    set_fields(abscode);
+    abscode >>= 31;
+    antiparticle_ = abscode;
+    int test = test_code();
+    if (test > 0) {
+      throw InvalidPDGCode("Invalid digits in PDG Code " + std::to_string(code()));
+     // throw InvalidPDGCode(std::string("Invalid digits in PDG Code ")
+     //                    + code() + std::string(" they need to be Hex-encoded!"));
+    }
+  }
+
+  inline void set_fields(unsigned int& abscode) {
+    constexpr unsigned int bitmask = 0xf;
     n_J_ = abscode & bitmask;
     abscode >>= 4;
-    printf("bitmask: 0x%08x\nabscode: 0x%08x\nVALUE:   0x%08x\n", bitmask, abscode, abscode & bitmask);
     n_q3_ = abscode & bitmask;
     abscode >>= 4;
-    printf("bitmask: 0x%08x\nabscode: 0x%08x\nVALUE:   0x%08x\n", bitmask, abscode, abscode & bitmask);
     n_q2_ = abscode & bitmask;
     abscode >>= 4;
-    printf("bitmask: 0x%08x\nabscode: 0x%08x\nVALUE:   0x%08x\n", bitmask, abscode, abscode & bitmask);
     n_q1_ = abscode & bitmask;
     abscode >>= 4;
-    printf("bitmask: 0x%08x\nabscode: 0x%08x\nVALUE:   0x%08x\n", bitmask, abscode, abscode & bitmask);
     n_L_  = abscode & bitmask;
     abscode >>= 4;
-    printf("bitmask: 0x%08x\nabscode: 0x%08x\nVALUE:   0x%08x\n", bitmask, abscode, abscode & bitmask);
     n_R_  = abscode & bitmask;
     abscode >>= 4;
-    printf("bitmask: 0x%08x\nabscode: 0x%08x\nVALUE:   0x%08x\n", bitmask, abscode, abscode & bitmask);
     n_    = abscode & bitmask;
-    antiparticle_ = codenumber < 0;
-    //if (! test_code()) {
-    //  printf("%d %x\n", pdgcode_, pdgcode_);
-    //  throw InvalidPDGCode("Invalid digits in PDG Code, they need to be Hex-encoded!");
-    //}
   }
 
   /** Checks the integer for hex digits that are > 9.
@@ -83,26 +93,24 @@ class PDGCode {
    * something went wrong - maybe some user of this class forgot to
    * prefix the input with '0x' and thus passed 221 instead of 0x221.
    *
-   * This routine only returns a boolean; it should be in the hands of
-   * the calling function to raise this to an exception.
+   * \return fail a bitmask indicating the offending digits. In the
+   * above example, 221 = 0xd3, the second-to-last-digit is the
+   * offending one, to the return value is 0b10 = 0x2 = 2.
    *
    **/
-  //inline bool test_code() {
-  //  constexpr int bitmask = 0xf;
-  //  // go through the hex digits:
-  //  for (unsigned int bit = 0; bit < sizeof(int)*8; bit += 4) {
-  //    // get the digit at hand: First, shift all bits to the small end,
-  //    // so that the bits of interest are in the 4 least registers, then
-  //    // use the bit mask to cut away the rest.
-  //    int this_digit = (code_ >> bit) & bitmask;
-  //    if (this_digit > 9 && bit < sizeof(int)*8 - 4) {
-  //      return false;
-  //    }
-  //  }
-  //  return true;
-  //}
+  inline int test_code() {
+    int fail = 0;
+    if (n_    > 9) fail |= 1<<6;
+    if (n_R_  > 9) fail |= 1<<5;
+    if (n_L_  > 9) fail |= 1<<4;
+    if (n_q1_ > 9) fail |= 1<<3;
+    if (n_q2_ > 9) fail |= 1<<2;
+    if (n_q3_ > 9) fail |= 1<<1;
+    if (n_J_  > 9) fail |= 1;
+    return fail;
+  }
 
-  inline int code() const {
+  inline unsigned int dump() const {
     return (antiparticle_ << 31)
          | (n_    << 24)
          | (n_R_  << 20)
@@ -113,27 +121,140 @@ class PDGCode {
          | (n_J_); 
   }
 
+  inline int code() const {
+    return (antiparticle_ ? -1 : 1)
+         * ( (n_    << 24)
+           | (n_R_  << 20)
+           | (n_L_  << 16)
+           | (n_q1_ << 12)
+           | (n_q2_ <<  8)
+           | (n_q3_ <<  4)
+           | (n_J_)); 
+  }
+
+  /// returns true if this is a baryon OR antibaryon!.
+  /// returns true if \f$n_{q_1} \ne 0\f$ and \f$n_{q_2} \ne 0\f$.
+  inline bool is_hadron() const {
+    return (n_q3_ != 0 && n_q2_ != 0);
+  }
+  /// returns true if this is a meson.
+  inline int baryon_number() const {
+    if (! is_hadron() || n_q1_ == 0)
+      return  0;
+    return antiparticle_sign();
+  }
+  /// returns twice the isospin-3 component.
+  inline int isospin3() const {
+    // heavy_quarkness(2) is the number of u quarks,
+    // heavy_quarkness(1) is minus the number of d quarks.
+    return heavy_quarkness(2)+heavy_quarkness(1);
+  }
+  inline int strangeness() const {
+    return heavy_quarkness(3);
+  }
+  inline int charmness() const {
+    return heavy_quarkness(4);
+  }
+  inline int bottomness() const {
+    return heavy_quarkness(5);
+  }
+  /** this looks for the quantum number associated with quark number
+   * quark.
+   *
+   * Despite the name, it is also applicable to light quarks.
+   *
+   **/
+  int heavy_quarkness(const int quark) const {
+    // non-hadrons and those that have none of this quark type: 0.
+    if (! is_hadron() || (n_q1_ != quark && n_q2_ != quark && n_q3_ != quark))
+      return 0;
+    // baryons: count quarks.
+    if (baryon_number() != 0) {
+      // u,s,b quarks get negative *ness; d,c,t have positive *ness:
+      int sign = (quark%2 == 0) ? +1 : -1;
+      // and for anti-baryons, the sign changes:
+      sign *= antiparticle_sign();
+      return sign*((n_q1_ == quark) + (n_q2_ == quark) + (n_q3_ == quark));
+    }
+    // mesons.
+    // quarkonium state? Not open heavy-quarkness.
+    if (n_q3_ == quark && n_q2_ == quark)
+      return 0;
+    // this has covered all the easy stuff
+    // get the "other" quark. (We know this must exist, since they are
+    // not both the right one and one of them is the right one).
+    int otherquark = (n_q2_ == quark) ? n_q3_ : n_q2_;
+    // "our" quark is the heavier one: 1 for particles
+    if (otherquark < quark) {
+      return antiparticle_sign();
+    }
+    // ours is the lighter: If the heavier quark has the same SIGN, we
+    // get a -1, else +1.
+    if (otherquark%2 == quark%2)
+      return -1*antiparticle_sign();
+    return antiparticle_sign();
+  }
+  int charge() const {
+    if (is_hadron()) {
+      // this will accumulate 3*charge
+      int Q = 0;
+      // this loops over d,u,s,c,b,t,b',t' quarks (the latter three can
+      // be safely ignored, but I don't think this will be a bottle
+      // neck.
+      for (int i = 1; i < 9; i++) {
+        // the appropriate sign is already in heavy_quarkness; now
+        // u,c,t,t' quarks have charge = 2/3 e, while d,s,b,b' quarks
+        // have -1/3 e.
+        Q += (i%2 == 0 ? 2 : 1)*heavy_quarkness(i);
+      }
+      return Q/3;
+    }
+    // non-hadron:
+    // Leptons: 11, 13, 15, 17 are e, μ, τ, τ' and have a charge -1,
+    // while    12, 14, 16, 18 are the neutrinos that have no charge.
+    if (n_q3_ == 1)
+      return -1*(n_J_%2)*antiparticle_sign();
+    // Bosons: 24 is the W+, all else is uncharged.
+    if (n_q3_ == 2 && n_J_ == 4)
+      return antiparticle_sign();
+    // default (this includes all other Bosons) is 0.
+    return 0;
+  }
+  inline int antiparticle_sign() const {
+    return (antiparticle_ ? -1 : +1);
+  }
+
+ private:
   /** The PDG code we're dealing with here.
    *
    * Everything else in this class is simpy smart accessors to this
-   * integer.
+   * bitfield.
    *
-   * This integer is interpreted with hexadecimal digits, i.e., '545' is
+   * The number is interpreted with hexadecimal digits, i.e., '545' is
    * interpreted as '0x221', which is what an eta-meson has (that 545 is
    * also its approximate mass in MeV is, I promise, a complete
    * coincidence).
+   *
+   * These digits are stored in the bit fields.
    **/
+   /// first bit: stores the sign.
    bool antiparticle_  : 1,
    // we don't use these bits.
                        : 3;
+   /// first field: "counter"
    std::uint32_t n_    : 4;
+   /// "radial excitation"
    std::uint32_t n_R_  : 4;
+   /// "angular momentum"
    std::uint32_t n_L_  : 4;
+   /// first quark field. 0 for mesons.
    std::uint32_t n_q1_ : 4;
+   /// second quark field
    std::uint32_t n_q2_ : 4;
+   /// third quark field
    std::uint32_t n_q3_ : 4;
+   /// spin quantum number \f$n_J = 2 J + 1\f$.
    std::uint32_t n_J_  : 4;
- private:
 };
 
 } // namespace SMASH
--- src/tests/pdgcode.cc
+++ src/tests/pdgcode.cc
@@ -12,30 +12,244 @@
 
 using namespace Smash;
 
-constexpr double accuracy = 1e-10;
+// non-hadrons:
+PDGCode electron( 0x11);
+PDGCode   antimu(-0x13);
+PDGCode   photon( 0x22);
+// mesons:
+PDGCode   pion( 0x211);
+PDGCode   kaon( 0x311);
+PDGCode kminus(-0x321);
+PDGCode dminus(-0x411);
+PDGCode bnulls( 0x531);
+PDGCode bPcbar(-0x541);
+PDGCode eta_pr( 0x331);
+PDGCode  j_psi( 0x443);
+// baryons:
+PDGCode      proton( 0x2212);
+PDGCode   antidelta(-0x2224);
+PDGCode       sigma( 0x3222);
+PDGCode      lambda( 0x3122);
+PDGCode      antixi(-0x3312);
+PDGCode   omega_bar(-0x3334);
+PDGCode    lambda_c( 0x4122);
+PDGCode sigma_c_bar(-0x4114);
+PDGCode        xi_c( 0x4322);
+PDGCode omega_c_bar(-0x4332);
+PDGCode   xi_cc_bar(-0x4422);
+PDGCode    omega_bc( 0x5432);
 
-TEST(set_code) {
-  printf ("%08x\n", (0xf << 8) | 0xf0);
+TEST(write_codes) {
+  printf("################# Non-Hadrons:\n");
+  printf("e^-:       %8x 0x%08x\n",   electron.code(),   electron.dump());
+  printf("μ^+:       %8x 0x%08x\n",     antimu.code(),     antimu.dump());
+  printf("γ:         %8x 0x%08x\n",     photon.code(),     photon.dump());
+  printf("##################### Mesons:\n");
+  printf("π^+:       %8x 0x%08x\n",       pion.code(),       pion.dump());
+  printf("K^0:       %8x 0x%08x\n",       kaon.code(),       kaon.dump());
+  printf("K^-:       %8x 0x%08x\n",     kminus.code(),     kminus.dump());
+  printf("D^-:       %8x 0x%08x\n",     dminus.code(),     dminus.dump());
+  printf("B^0_s:     %8x 0x%08x\n",     bnulls.code(),     bnulls.dump());
+  printf("bar B^+_c: %8x 0x%08x\n",     bPcbar.code(),     bPcbar.dump());
+  printf("η^-:       %8x 0x%08x\n",     eta_pr.code(),     eta_pr.dump());
+  printf("J/Ψ:       %8x 0x%08x\n",      j_psi.code(),      j_psi.dump());
+  printf("##################### Baryons:\n");
+  printf("p:         %8x 0x%08x\n",      proton.code(),      proton.dump());
+  printf("bar Δ:     %8x 0x%08x\n",   antidelta.code(),   antidelta.dump());
+  printf("Σ:         %8x 0x%08x\n",       sigma.code(),       sigma.dump());
+  printf("Λ:         %8x 0x%08x\n",      lambda.code(),      lambda.dump());
+  printf("bar Ξ:     %8x 0x%08x\n",      antixi.code(),      antixi.dump());
+  printf("bar Ω:     %8x 0x%08x\n",   omega_bar.code(),   omega_bar.dump());
+  printf("Λ_c:       %8x 0x%08x\n",    lambda_c.code(),    lambda_c.dump());
+  printf("bar Σ_c:   %8x 0x%08x\n", sigma_c_bar.code(), sigma_c_bar.dump());
+  printf("Ξ_c:       %8x 0x%08x\n",        xi_c.code(),        xi_c.dump());
+  printf("bar Ω_c:   %8x 0x%08x\n", omega_c_bar.code(), omega_c_bar.dump());
+  printf("bar Ξ_cc:  %8x 0x%08x\n",   xi_cc_bar.code(),   xi_cc_bar.dump());
+  printf("Ω_bc:      %8x 0x%08x\n",    omega_bc.code(),    omega_bc.dump());
+}
+TEST(code) {
+  COMPARE( electron.code(),  0x11);
+  COMPARE(   antimu.code(),  0xffffffed);
+  COMPARE(   photon.code(),  0x22);
+  COMPARE(     pion.code(),  0x211);
+  COMPARE(     kaon.code(),  0x311);
+  COMPARE(   proton.code(),  0x2212);
+  COMPARE(antidelta.code(),  0xffffdddc);
+  COMPARE(   lambda.code(),  0x3122);
+  COMPARE(   antixi.code(),  0xffffccee);
+}
+TEST(dump) {
+  COMPARE( electron.dump(),  0x11);
+  COMPARE(   antimu.dump(),  0x80000013u);
+  COMPARE(   photon.dump(),  0x22);
+  COMPARE(     pion.dump(),  0x211);
+  COMPARE(     kaon.dump(),  0x311);
+  COMPARE(   proton.dump(),  0x2212);
+  COMPARE(antidelta.dump(),  0x80002224u);
+  COMPARE(   lambda.dump(),  0x3122);
+  COMPARE(   antixi.dump(),  0x80003312u);
+}
 
-
-  PDGCode pion(0x211);
-  printf ("pion: Anti: %08x\n", pion.antiparticle_);
-  printf ("pion: n   : %08x\n", pion.n_);
-  printf ("pion: nR  : %08x\n", pion.n_R_);
-  printf ("pion: nL  : %08x\n", pion.n_L_);
-  printf ("pion: nq1 : %08x\n", pion.n_q1_);
-  printf ("pion: nq2 : %08x\n", pion.n_q2_);
-  printf ("pion: nq3 : %08x\n", pion.n_q3_);
-  printf ("pion: nJ  : %08x\n", pion.n_J_);
-  printf ("pion: %08x\n", pion.code());
-
-  FUZZY_COMPARE(pion.code(), 0x211) << "Pion";
-  PDGCode proton(0x2212);
-  FUZZY_COMPARE(proton.code(), 0x2212) << "Proton";
-  PDGCode anti_proton(-0x2212);
-  FUZZY_COMPARE(anti_proton.code(), ((2<<31)+0x2212)) << "Antiproton";
+TEST(hadron) {
+  VERIFY(! electron.is_hadron());
+  VERIFY(      pion.is_hadron());
+  VERIFY(    proton.is_hadron());
+  VERIFY( antidelta.is_hadron());
+}
+TEST(baryon_number) {
+  COMPARE(   electron.baryon_number(),  0);
+  COMPARE(     antimu.baryon_number(),  0);
+  COMPARE(     photon.baryon_number(),  0);
+  COMPARE(       pion.baryon_number(),  0);
+  COMPARE(       kaon.baryon_number(),  0);
+  COMPARE(     kminus.baryon_number(),  0);
+  COMPARE(     dminus.baryon_number(),  0);
+  COMPARE(     bnulls.baryon_number(),  0);
+  COMPARE(     bPcbar.baryon_number(),  0);
+  COMPARE(     eta_pr.baryon_number(),  0);
+  COMPARE(      j_psi.baryon_number(),  0);
+  COMPARE(     proton.baryon_number(),  1);
+  COMPARE(  antidelta.baryon_number(), -1);
+  COMPARE(      sigma.baryon_number(),  1);
+  COMPARE(     lambda.baryon_number(),  1);
+  COMPARE(     antixi.baryon_number(), -1);
+  COMPARE(  omega_bar.baryon_number(), -1);
+  COMPARE(   lambda_c.baryon_number(),  1);
+  COMPARE(sigma_c_bar.baryon_number(), -1);
+  COMPARE(       xi_c.baryon_number(),  1);
+  COMPARE(omega_c_bar.baryon_number(), -1);
+  COMPARE(  xi_cc_bar.baryon_number(), -1);
+  COMPARE(   omega_bc.baryon_number(),  1);
+}
+TEST(isospin3) {
+  COMPARE(   electron.isospin3(),  0);
+  COMPARE(     antimu.isospin3(),  0);
+  COMPARE(     photon.isospin3(),  0);
+  COMPARE(       pion.isospin3(), +2);
+  COMPARE(       kaon.isospin3(), -1);
+  COMPARE(     kminus.isospin3(), -1);
+  COMPARE(     dminus.isospin3(), -1);
+  COMPARE(     bnulls.isospin3(),  0);
+  COMPARE(     bPcbar.isospin3(),  0);
+  COMPARE(     eta_pr.isospin3(),  0);
+  COMPARE(      j_psi.isospin3(),  0);
+  COMPARE(     proton.isospin3(),  1);
+  COMPARE(  antidelta.isospin3(), -3);
+  COMPARE(      sigma.isospin3(), +2);
+  COMPARE(     lambda.isospin3(),  0);
+  COMPARE(     antixi.isospin3(), +1);
+  COMPARE(  omega_bar.isospin3(),  0);
+  COMPARE(   lambda_c.isospin3(),  0);
+  COMPARE(sigma_c_bar.isospin3(), +2);
+  COMPARE(       xi_c.isospin3(), +1);
+  COMPARE(omega_c_bar.isospin3(),  0);
+  COMPARE(  xi_cc_bar.isospin3(), -1);
+  COMPARE(   omega_bc.isospin3(),  0);
+}
+TEST(strangeness) {
+  COMPARE(   electron.strangeness(),  0);
+  COMPARE(     antimu.strangeness(),  0);
+  COMPARE(     photon.strangeness(),  0);
+  COMPARE(       pion.strangeness(),  0);
+  COMPARE(       kaon.strangeness(),  1);
+  COMPARE(     kminus.strangeness(), -1);
+  COMPARE(     dminus.strangeness(),  0);
+  COMPARE(     bnulls.strangeness(), -1);
+  COMPARE(     bPcbar.strangeness(),  0);
+  COMPARE(     eta_pr.strangeness(),  0);
+  COMPARE(      j_psi.strangeness(),  0);
+  COMPARE(     proton.strangeness(),  0);
+  COMPARE(  antidelta.strangeness(),  0);
+  COMPARE(      sigma.strangeness(), -1);
+  COMPARE(     lambda.strangeness(), -1);
+  COMPARE(     antixi.strangeness(), +2);
+  COMPARE(  omega_bar.strangeness(), +3);
+  COMPARE(   lambda_c.strangeness(),  0);
+  COMPARE(sigma_c_bar.strangeness(),  0);
+  COMPARE(       xi_c.strangeness(), -1);
+  COMPARE(omega_c_bar.strangeness(), +2);
+  COMPARE(  xi_cc_bar.strangeness(),  0);
+  COMPARE(   omega_bc.strangeness(), -1);
+}
+TEST(charmness) {
+  COMPARE(   electron.charmness(),  0);
+  COMPARE(     antimu.charmness(),  0);
+  COMPARE(     photon.charmness(),  0);
+  COMPARE(       pion.charmness(),  0);
+  COMPARE(       kaon.charmness(),  0);
+  COMPARE(     kminus.charmness(),  0);
+  COMPARE(     dminus.charmness(), -1);
+  COMPARE(     bnulls.charmness(),  0);
+  COMPARE(     bPcbar.charmness(), -1);
+  COMPARE(     eta_pr.charmness(),  0);
+  COMPARE(      j_psi.charmness(),  0);
+  COMPARE(     proton.charmness(),  0);
+  COMPARE(  antidelta.charmness(),  0);
+  COMPARE(      sigma.charmness(),  0);
+  COMPARE(     lambda.charmness(),  0);
+  COMPARE(     antixi.charmness(),  0);
+  COMPARE(  omega_bar.charmness(),  0);
+  COMPARE(   lambda_c.charmness(), +1);
+  COMPARE(sigma_c_bar.charmness(), -1);
+  COMPARE(       xi_c.charmness(), +1);
+  COMPARE(omega_c_bar.charmness(), -1);
+  COMPARE(  xi_cc_bar.charmness(), -2);
+  COMPARE(   omega_bc.charmness(), +1);
+}
+TEST(bottomness) {
+  COMPARE(   electron.bottomness(),  0);
+  COMPARE(     antimu.bottomness(),  0);
+  COMPARE(     photon.bottomness(),  0);
+  COMPARE(       pion.bottomness(),  0);
+  COMPARE(       kaon.bottomness(),  0);
+  COMPARE(     kminus.bottomness(),  0);
+  COMPARE(     dminus.bottomness(),  0);
+  COMPARE(     bnulls.bottomness(),  1);
+  COMPARE(     bPcbar.bottomness(), -1);
+  COMPARE(     eta_pr.bottomness(),  0);
+  COMPARE(      j_psi.bottomness(),  0);
+  COMPARE(     proton.bottomness(),  0);
+  COMPARE(  antidelta.bottomness(),  0);
+  COMPARE(      sigma.bottomness(),  0);
+  COMPARE(     lambda.bottomness(),  0);
+  COMPARE(     antixi.bottomness(),  0);
+  COMPARE(  omega_bar.bottomness(),  0);
+  COMPARE(   lambda_c.bottomness(),  0);
+  COMPARE(sigma_c_bar.bottomness(),  0);
+  COMPARE(       xi_c.bottomness(),  0);
+  COMPARE(omega_c_bar.bottomness(),  0);
+  COMPARE(  xi_cc_bar.bottomness(),  0);
+  COMPARE(   omega_bc.bottomness(), -1);
+}
+TEST(charge) {
+  COMPARE(   electron.charge(), -1);
+  COMPARE(     antimu.charge(), +1);
+  COMPARE(     photon.charge(),  0);
+  COMPARE(       pion.charge(), +1);
+  COMPARE(       kaon.charge(),  0);
+  COMPARE(     kminus.charge(), -1);
+  COMPARE(     dminus.charge(), -1);
+  COMPARE(     bnulls.charge(),  0);
+  COMPARE(     bPcbar.charge(), -1);
+  COMPARE(     eta_pr.charge(),  0);
+  COMPARE(      j_psi.charge(),  0);
+  COMPARE(     proton.charge(), +1);
+  COMPARE(  antidelta.charge(), -2);
+  COMPARE(      sigma.charge(), +1);
+  COMPARE(     lambda.charge(),  0);
+  COMPARE(     antixi.charge(), +1);
+  COMPARE(  omega_bar.charge(), +1);
+  COMPARE(   lambda_c.charge(), +1);
+  COMPARE(sigma_c_bar.charge(),  0);
+  COMPARE(       xi_c.charge(), +1);
+  COMPARE(omega_c_bar.charge(),  0);
+  COMPARE(  xi_cc_bar.charge(), -2);
+  COMPARE(   omega_bc.charge(),  0);
 }
 
 TEST_CATCH(set_invalid_code, PDGCode::InvalidPDGCode) {
-  PDGCode code(211);
+  PDGCode validparticle(211);
+}
+TEST_CATCH(set_invalid_code_hex, PDGCode::InvalidPDGCode) {
+  PDGCode invalidparticle(0xfedcba98);
 }
