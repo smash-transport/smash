ee68badb598d63a053e3a0e8aef22774b1e33799
Matthias Kretz <kretz@kde.org>
2014-05-06 16:37:00 +0200

    forward decls: start of a single header for forward decls
    some forward decls are hard to get right and this is boring and
    repetitive work. Especially if a declaration additionally needs be
    hidden from doxygen... Including a single header for all possible
    forward decls seems like a good idea to clean this up.
    
    Forward decls for std::vector and Particle{Data,Type,s,List}.
    
    refs #236
    
    Signed-off-by: Matthias Kretz <kretz@kde.org>

---
+++ src/include/forwarddeclarations.h
@@ -0,0 +1,30 @@
+/*
+ *
+ *    Copyright (c) 2014
+ *      SMASH Team
+ *
+ *    GNU General Public License (GPLv3 or later)
+ *
+ */
+
+#ifndef SRC_INCLUDE_FORWARDDECLARATIONS_H_
+#define SRC_INCLUDE_FORWARDDECLARATIONS_H_
+
+namespace std {
+template <typename T>
+class allocator;
+template <typename T, typename A>
+class vector;
+}  // namespace std
+
+namespace Smash {
+
+class ParticleData;
+class ParticleType;
+class Particles;
+
+using ParticleList = std::vector<ParticleData, std::allocator<ParticleData>>;
+
+}  // namespace Smash
+
+#endif  // SRC_INCLUDE_FORWARDDECLARATIONS_H_
