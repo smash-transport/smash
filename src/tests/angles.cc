3ed42747aeb05dcd47084af3401e7b954f306d5a
Bjørn Bäuchle <baeuchle@th.physik.uni-frankfurt.de>
2014-06-03 15:13:30 +0200

    Angles: Using single-precision floats now
    This refs #618
    
    Signed-off-by: Bjørn Bäuchle <baeuchle@th.physik.uni-frankfurt.de>

--- src/tests/angles.cc
+++ src/tests/angles.cc
@@ -13,19 +13,19 @@
 using namespace Smash;
 
 Angles dir;
-constexpr double accuracy = 1e-10;
+constexpr float accuracy = 1e-6;
 
 TEST(set_angles) {
   dir.set_phi(.5);
-  FUZZY_COMPARE(dir.phi(), .5);
+  FUZZY_COMPARE(dir.phi(), .5f);
   dir.set_phi(4*M_PI);
-  FUZZY_COMPARE(dir.phi(), 0.0);
+  COMPARE_ABSOLUTE_ERROR(dir.phi(), 0.f, accuracy);
   dir.set_costheta(.3);
-  FUZZY_COMPARE(dir.costheta(), 0.3);
+  FUZZY_COMPARE(dir.costheta(), 0.3f);
   dir.set_theta(.3);
   // for cos(acos()), we need greater leniency
-  UnitTest::setFuzzyness<double>(3);
-  FUZZY_COMPARE(dir.theta(), 0.3);
+  UnitTest::setFuzzyness<float>(3);
+  FUZZY_COMPARE(dir.theta(), 0.3f);
 }
 
 TEST(accessors_and_relations) {
@@ -36,57 +36,58 @@ TEST(accessors_and_relations) {
     // sintheta**2 + costheta**2 = 1
     COMPARE_ABSOLUTE_ERROR(dir.sintheta()*dir.sintheta()
                          + dir.costheta()*dir.costheta(),
-                           1.0, accuracy);
+                           1.0f, accuracy);
     // x**2 + y**2 + z**2 = 1
-    double xyz_one = dir.x()*dir.x()
-                   + dir.y()*dir.y()
-                   + dir.z()*dir.z();
-    COMPARE_ABSOLUTE_ERROR(xyz_one, 1.0, accuracy);
+    float xyz_one = dir.x()*dir.x()
+                  + dir.y()*dir.y()
+                  + dir.z()*dir.z();
+    COMPARE_ABSOLUTE_ERROR(xyz_one, 1.0f, accuracy);
 
     ThreeVector direction = dir.threevec();
-    COMPARE_ABSOLUTE_ERROR(xyz_one, direction.abs(), accuracy);
+    COMPARE_ABSOLUTE_ERROR(xyz_one, float(direction.abs()), accuracy);
 
     // compare cos(theta) and costheta:
-    COMPARE_RELATIVE_ERROR(cos(dir.theta()), dir.costheta(), accuracy)
+    COMPARE_ABSOLUTE_ERROR(std::cos(dir.theta()), dir.costheta(), accuracy)
                         << " (trial #" << c << " of " << NumberOfTries << ")";
-    COMPARE_RELATIVE_ERROR(dir.theta(), acos(dir.costheta()), accuracy)
+    COMPARE_RELATIVE_ERROR(dir.theta(), std::acos(dir.costheta()), accuracy)
                         << " (trial #" << c << " of " << NumberOfTries << ")";
   }
 }
 
 TEST(unusual_set_phi) {
+  UnitTest::setFuzzyness<float>(64);
   constexpr int kMinM = -6;
   constexpr int kMaxM = 12;
   for (int m = kMinM; m < kMaxM; m++) {
     // set phi outside [0..2pi]
     dir.set_phi(2.0 * M_PI * m + .5);
-    FUZZY_COMPARE(dir.phi(), .5) << " (m = " << m << ")";
+    FUZZY_COMPARE(dir.phi(), .5f) << " (m = " << m << ")";
   }
 }
 
 TEST(unusual_set_theta_even) {
-  UnitTest::setFuzzyness<double>(64);
+  UnitTest::setFuzzyness<float>(64);
   constexpr int kMinM = -6;
   constexpr int kMaxM = 12;
   for (int m = kMinM; m < kMaxM; m++) {
     // set theta in [2*n*pi .. (2*n+1)*pi]
     dir.set_theta(2.0 * M_PI * m + .7);
-    FUZZY_COMPARE(dir.theta(), .7) << " (m = " << m << ")";
+    FUZZY_COMPARE(dir.theta(), .7f) << " (m = " << m << ")";
   }
 }
 TEST(unusual_set_theta_odd) {
-  UnitTest::setFuzzyness<double>(128);
+  UnitTest::setFuzzyness<float>(128);
   constexpr int kMinM = -6;
   constexpr int kMaxM = 12;
   for (int m = kMinM; m < kMaxM; m++) {
     // set theta in [(2*n-1)*pi .. 2*n*pi]
     dir.set_theta(2.0 * M_PI * m - .7);
-    FUZZY_COMPARE(dir.theta(), .7) << " (m = " << m << ")";
+    FUZZY_COMPARE(dir.theta(), .7f) << " (m = " << m << ")";
   }
 }
 
 TEST(catch_invalid_cosine) {
-  for (double newcos = -8.0; newcos < 8.0; newcos += .2) {
+  for (float newcos = -8.0; newcos < 8.0; newcos += .2) {
     bool invalid_input = (newcos < -1 || newcos > 1);
     // Did I catch an exception?
     bool caught = false;
@@ -108,33 +109,35 @@ TEST(catch_invalid_cosine) {
 
 TEST(setting_costheta_does_not_change_phi) {
   dir.set_phi(3.0);
-  double old_phi = dir.phi();
+  float old_phi = dir.phi();
   dir.set_costheta(.2);
   FUZZY_COMPARE(old_phi, dir.phi());
 }
 
 TEST(setting_theta_does_not_change_phi) {
   dir.set_phi(3.0);
-  double old_phi = dir.phi();
+  float old_phi = dir.phi();
   dir.set_theta(.2);
   FUZZY_COMPARE(old_phi, dir.phi());
 }
 
 TEST(setting_phi_does_not_change_costheta) {
-  double old_costheta = dir.costheta();
+  float old_costheta = dir.costheta();
   dir.set_phi(.4);
   FUZZY_COMPARE(old_costheta, dir.costheta());
 }
 
 TEST(setting_phi_does_not_change_z) {
-  double old_z = dir.z();
+  float old_z = dir.z();
   dir.set_phi(.4);
   FUZZY_COMPARE(old_z, dir.z());
 }
 
 TEST(add_theta) {
-  UnitTest::setFuzzyness<double>(64);
-  for (double current_phi = 0.0; current_phi < 2 * M_PI;
+  UnitTest::setFuzzyness<float>(64);
+  // float typed PI:
+  const float M_PI_f = M_PI;
+  for (float current_phi = 0.0; current_phi < 2 * M_PI;
                                  current_phi += M_PI / 180.0) {
     dir.set_phi(current_phi);
     // we'll start at what I call the north pole:
@@ -145,44 +148,47 @@ TEST(add_theta) {
     // the sign shouldn't have changed.
     VERIFY(!sign) << " (phi = " << current_phi << ")";
     // theta, though, should have changed.
-    FUZZY_COMPARE(dir.theta(), M_PI / 2.0) << " (phi = " << current_phi << ")";
+    FUZZY_COMPARE(dir.theta(), M_PI_f / 2.f)
+                                          << " (phi = " << current_phi << ")";
     // 90+120 degrees: phi changed, theta is at 150 degrees
     sign = dir.add_to_theta(2.0 * M_PI / 3.0);
     if (current_phi < M_PI) {
-      FUZZY_COMPARE(current_phi, dir.phi() - M_PI) << " (phi = " << current_phi
-                                                                << ")";
+      FUZZY_COMPARE(current_phi, dir.phi() - M_PI_f) << " (phi = "
+                                                          << current_phi << ")";
     } else {
-      FUZZY_COMPARE(current_phi, dir.phi() + M_PI) << " (phi = " << current_phi
-                                                                << ")";
+      FUZZY_COMPARE(current_phi, dir.phi() + M_PI_f) << " (phi = "
+                                                          << current_phi << ")";
     }
     VERIFY(sign) << " (phi = " << current_phi << ")";
-    FUZZY_COMPARE(dir.theta(), 5.0 * M_PI / 6.0) << " (phi = " << current_phi
+    FUZZY_COMPARE(dir.theta(), 5.f * M_PI_f / 6.f) << " (phi = " << current_phi
                                                               << ")";
     // go back over the (south) pole: +60 degrees
     sign = dir.add_to_theta(M_PI / 3.0);
     // phi is back to original;
-    FUZZY_COMPARE(current_phi, dir.phi()) << " (phi = " << current_phi << ")";
+    COMPARE_ABSOLUTE_ERROR(current_phi, dir.phi(), accuracy) << " (phi = "
+                                                             << current_phi << ")";
     VERIFY(sign) << " (phi = " << current_phi << ")";
     // theta is the same as before (150+60 = 210 equiv 150)
-    FUZZY_COMPARE(dir.theta(), 5.0 * M_PI / 6.0) << " (phi = " << current_phi
+    FUZZY_COMPARE(dir.theta(), 5.f * M_PI_f / 6.f) << " (phi = " << current_phi
                                                               << ")";
     // go over two poles: +120 + 120 (going over more than 180 degrees
     // in one step is forbidden; we'll check that later)
     // First, over the south pole
-    sign = dir.add_to_theta(2.0 * M_PI / 3.0);
+    sign = dir.add_to_theta(2.f * M_PI_f / 3.f);
     // sign is true because we are in between south- and north pole
     VERIFY(sign) << " (phi = " << current_phi << ")";
     // second, we want to continue in the same direction along the great
     // circle, so we want to go over the north pole now. This needs to
     // be told to add_to_theta by feeding it sign.
-    bool sign2 = dir.add_to_theta(2.0 * M_PI / 3.0, sign);
+    bool sign2 = dir.add_to_theta(2.f * M_PI_f / 3.f, sign);
     // sign2 is FALSE (despite the crossing), because now we are in the
     // semi circle between north- and south pole (mind the direction!)
     VERIFY(!sign2) << " (phi = " << current_phi << ")";
     // phi stays the same (two changes)
-    FUZZY_COMPARE(current_phi, dir.phi()) << " (phi = " << current_phi << ")";
+    COMPARE_ABSOLUTE_ERROR(current_phi, dir.phi(), accuracy) << " (phi = "
+                                                             << current_phi << ")";
     // theta is now 30 degrees:
-    FUZZY_COMPARE(dir.theta(), M_PI / 6.0) << " (phi = " << current_phi << ")";
+    FUZZY_COMPARE(dir.theta(), M_PI_f / 6.f) << " (phi = " << current_phi << ")";
   }
 }
 
