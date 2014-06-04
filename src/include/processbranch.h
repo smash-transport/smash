da5159ecebc41b1346a64ab6c783ed50500d9419
Bjørn Bäuchle <baeuchle@th.physik.uni-frankfurt.de>
2014-06-04 11:38:24 +0200

    Documentation: Added more documentation
    Especially for particledata, it was missleading to claim that
    set_momentum(double, double, double, double) sets the Four-Momentum by
    components, when the first double is actually the mass.
    
    Signed-off-by: Bjørn Bäuchle <baeuchle@th.physik.uni-frankfurt.de>

--- src/include/processbranch.h
+++ src/include/processbranch.h
@@ -46,9 +46,9 @@ class ProcessBranch {
  public:
   /// Default constructor
   ProcessBranch() : branch_weight_(-1.0), interaction_type_(0) {}
-  // Constructor with 1 particle
+  /// Constructor with 1 particle
   inline ProcessBranch(PdgCode p, float w, int t);
-  // Constructor with 2 particles
+  /// Constructor with 2 particles
   inline ProcessBranch(PdgCode p1, PdgCode p2, float w, int t);
   /// Add one particle to the list
   inline void add_particle(PdgCode particle_pdg);
