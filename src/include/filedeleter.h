4202a16d1cbcd053f2e32f5d4586c0834af1faab
Matthias Kretz <kretz@kde.org>
2014-06-04 11:32:02 +0200

    docs: document FileDeleter and FilePtr
    Signed-off-by: Matthias Kretz <kretz@kde.org>

--- src/include/filedeleter.h
+++ src/include/filedeleter.h
@@ -17,8 +17,21 @@
 #include <stdexcept>
 
 namespace Smash {
+
+/**
+ * FileDeleter is the deleter class for std::unique_ptr of std::FILE.
+ *
+ * std::unique_ptr takes a second template argument which determines what
+ * happens when the resource it holds needs to be freed. The default
+ * implementation calls `delete`. For std::FILE the resource needs to be freed
+ * with a call to std::fclose instead. Therefore FilePtr requires a custom
+ * deleter class to correctly free the resource.
+ */
 struct FileDeleter {
+  /// The class has no members, so this is a noop.
   constexpr FileDeleter() = default;
+
+  /// frees the std::FILE resource if it is non-zero.
   void operator()(std::FILE *f) const {
     if (f == nullptr) {
       return;
@@ -29,6 +42,14 @@ struct FileDeleter {
   }
 };
 
+/**
+ * A RAII type to replace `std::FILE *`.
+ *
+ * This is an alias type for std::unique_ptr to automatically free the std::FILE
+ * resource after the last reference goes out of scope. It is important to use a
+ * custom deleter type, and therefore SMASH code should never use
+ * std::unique_ptr directly with std::FILE.
+ */
 using FilePtr = std::unique_ptr<std::FILE, FileDeleter>;
 }  // namespace Smash
 
