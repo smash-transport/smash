d28422ed05f03a7430064d32f06efb084c1f146a
Dmytro Oliinychenko <oliiny@fias.uni-frankfurt.de>
2014-06-13 19:30:09 +0200

    Binary output: according to Mattias suggestions
    * Start the file with a "magic number" followed by a file format version
      number (int)
    * Use VERSION_MAJOR as the version information for the header.
    * no need for dummy entries (padding) - if the format changes, the file
      format version number is incremented and thus the change is well
      defined
    * removed unnecessary empty dtor
    * simplified the write(Particles) function using type-safe write
      function overloads => much less data copying required, which should
      reduce the overhead of the output.
    * strings are now stored as <size><data> instead of a fixed length
    
    refs #677
    
    Signed-off-by: Dmytro Oliinychenko <oliiny@fias.uni-frankfurt.de>

--- src/binaryoutput.cc
+++ src/binaryoutput.cc
@@ -11,80 +11,70 @@
 #include "include/clock.h"
 #include "include/forwarddeclarations.h"
 #include "include/particles.h"
+#include <include/config.h>
 
 #include <boost/filesystem.hpp>
+#include <string>
 
 namespace Smash {
 
 BinaryOutput::BinaryOutput(bf::path path)
-  : file_{std::fopen((path / "particles_binary.bin").native().c_str(), "wb")} {
+    : file_{
+          std::fopen((path / "particles_binary.bin").native().c_str(), "wb")} {
 
-  fwrite (smash_version_string , sizeof(char), 80, file_.get());
-  fwrite (format_version_string , sizeof(char), 80, file_.get());
-  fwrite (contents_units, sizeof(char), 300, file_.get());
+  fwrite("SMSH", 4, 1, file_.get());     // magic number
+  write(0);                              // file format version number
+  write(std::to_string(VERSION_MAJOR));  // version of SMASH
+
+  // the following header information is unnecessary because it is implicitly
+  // defined by the "file format version number" above already.
+  write(10);  // number of records
+  write(
+      "id PDGid t[fm/c] x[fm] y[fm] z[fm] E[GeV] px[GeV/c] py[GeV/c] "
+      "pz[GeV/c]");
 }
 
-
-BinaryOutput::~BinaryOutput() {}
-
-void BinaryOutput::at_eventstart(const Particles &/*particles*/,
-                                const int /*event_number*/) { 
-}
+void BinaryOutput::at_eventstart(const Particles &, const int) {}
 
 void BinaryOutput::at_eventend(const Particles &particles,
-                              const int event_number) {
- 
+                               const int event_number) {
   write(particles, event_number);
 
   /* Flush to disk */
   std::fflush(file_.get());
 }
 
+void BinaryOutput::write_interaction(const ParticleList &,
+                                     const ParticleList &) {}
+
+void BinaryOutput::after_Nth_timestep(const Particles &, const int,
+                                      const Clock &) {}
+
+// write functions:
+inline void BinaryOutput::write(const std::string &s) {
+  std::int32_t size = s.size();
+  std::fwrite(&size, sizeof(std::int32_t), 1, file_.get());
+  std::fwrite(s.c_str(), s.size(), 1, file_.get());
+}
+
+inline void BinaryOutput::write(const FourVector &v) {
+  std::fwrite(v.begin(), sizeof(*v.begin()), 4, file_.get());
+}
+
+inline void BinaryOutput::write(std::int32_t x) {
+  std::fwrite(&x, sizeof(x), 1, file_.get());
+}
+
 void BinaryOutput::write(const Particles &particles, const int event_number) {
-
-  double r[4], mom[4], dummy_double;
-  int id, pdgcode, dummy_int[2];
-  size_t psize = particles.size();
-
-  fwrite (&psize,         sizeof(size_t), 1, file_.get());
-  fwrite (&event_number,  sizeof(int), 1, file_.get());
+  write(particles.size());
+  write(event_number);
 
   for (const auto &p : particles.data()) {
-
-    r[0] = p.position().x0();
-    r[1] = p.position().x1();
-    r[2] = p.position().x2();
-    r[3] = p.position().x3();
-
-    mom[0] = p.momentum().x0();
-    mom[1] = p.momentum().x1();
-    mom[2] = p.momentum().x2();
-    mom[3] = p.momentum().x3();
-
-    pdgcode = p.pdgcode().get_decimal();
-    id = p.id();
- 
-    fwrite (&id,       sizeof(int), 1, file_.get());
-    fwrite (&pdgcode,  sizeof(int), 1, file_.get());
-
-    fwrite (r,   sizeof(double), 4, file_.get());
-    fwrite (mom, sizeof(double), 4, file_.get());
-
-    // Reserved fields
-    fwrite (dummy_int, sizeof(int), 2, file_.get());
-    fwrite (&dummy_double, sizeof(double), 1, file_.get());
+    write(p.id());
+    write(p.pdgcode().get_decimal());
+    write(p.position());
+    write(p.momentum());
   }
-
-}
-
-void BinaryOutput::write_interaction(
-  const ParticleList &/*incoming_particles*/,
-  const ParticleList &/*outgoing_particles*/) {
-}
-
-void BinaryOutput::after_Nth_timestep(const Particles & /*particles*/,
-                                                const int /*event_number*/,
-                                     const Clock& /*clock*/) {
 }
 
 }  // namespace Smash
--- src/include/binaryoutput.h
+++ src/include/binaryoutput.h
@@ -20,7 +20,6 @@ namespace Smash {
 class BinaryOutput : public OutputInterface {
  public:
   BinaryOutput(bf::path path);
-  ~BinaryOutput();
 
   /// writes the initial particle information of an event
   void at_eventstart(const Particles &particles,
@@ -35,14 +34,13 @@ class BinaryOutput : public OutputInterface {
   void after_Nth_timestep(const Particles &particles, const int event_number,
                           const Clock &clock) override;
 
- protected:
-  
+ private:
+  void write(const std::string &s);
+  void write(const FourVector &v);
+  void write(std::int32_t x);
   void write(const Particles &particles, const int event_number);
-  FilePtr file_;
-  const char* smash_version_string = "SMASH V0.3\n";
-  const char* format_version_string = "BINARY TEST 0.1\n";
-  const char* contents_units = "id PDGid t[fm/c] x[fm] y[fm] z[fm] E[GeV] px[GeV/c] py[GeV/c] pz[GeV/c] int(reserved) int(reserved) double(reserved)\n";
 
+  FilePtr file_;
 };
 }  // namespace Smash
 
