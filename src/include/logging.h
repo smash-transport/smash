a14997c2b921e801feb8548d4b50db395f40e5fe
Matthias Kretz <kretz@kde.org>
2014-08-26 13:32:19 +0200

    New logging helper code building upon einhard
    * Log areas are declared on top and are used to set up one
    einhard::Logger object per area that can then be accessed via the
    logger<Area>() function.
    * Log configuration is extracted from the Configuration object (and thus
    any area's verbosity can be configured independently in config.yaml).
    * Contains the necessary YAML-cpp glue to convert einhard::LogLevel to
    YAML Nodes and back
    * helper format function to shorten use of setw & setprecision & add
    unit
    
    refs #853
    
    Signed-off-by: Matthias Kretz <kretz@kde.org>

---
+++ src/include/logging.h
@@ -0,0 +1,161 @@
+/*
+ *
+ *    Copyright (c) 2014
+ *      SMASH Team
+ *
+ *    GNU General Public License (GPLv3 or later)
+ *
+ */
+
+#ifndef SRC_INCLUDE_LOGGING_H_
+#define SRC_INCLUDE_LOGGING_H_
+
+#include <einhard.hpp>
+#include <tuple>
+#include <yaml-cpp/yaml.h>
+#include <stdexcept>
+
+#include "macros.h"
+
+namespace Smash {
+class Configuration;
+
+/**
+ * Declares the necessary interface to identify a new log area.
+ */
+#define DECLARE_LOGAREA(id__, name__)                                     \
+  struct name__ {                                                         \
+    static constexpr int id = id__;                                       \
+    static constexpr const char *textual() { return #name__; }            \
+    static constexpr int textual_length() { return sizeof(#name__) - 1; } \
+  }
+
+/**
+ * The namespace where log areas are declared.
+ *
+ * To add a new area add one more line with DECLARE_LOGAREA at the bottom: Pick
+ * the next number for the id and a name to identify it in the log and source
+ * code. Then add the name to the end of the AreaTuple.
+ */
+namespace LogArea {
+DECLARE_LOGAREA( 0, Main);
+DECLARE_LOGAREA( 1, Experiment);
+DECLARE_LOGAREA( 2, Box);
+DECLARE_LOGAREA( 3, Collider);
+DECLARE_LOGAREA( 4, Nucleus);
+DECLARE_LOGAREA( 5, Sphere);
+DECLARE_LOGAREA( 6, Action);
+DECLARE_LOGAREA( 7, InputParser);
+DECLARE_LOGAREA( 8, ParticleType);
+DECLARE_LOGAREA( 9, FindScatter);
+DECLARE_LOGAREA(10, Legacy);
+DECLARE_LOGAREA(11, Clock);
+DECLARE_LOGAREA(12, DecayModes);
+DECLARE_LOGAREA(13, Resonances);
+
+/// This type collects all existing log areas so they will be created with the
+/// correct log level automatically.
+using AreaTuple = std::tuple<Main, Experiment, Box, Collider, Nucleus, Sphere,
+                             Action, InputParser, ParticleType, FindScatter,
+                             Legacy, Clock, DecayModes, Resonances>;
+}  // namespace LogArea
+
+/**
+ * Called from main() right after the Configuration object is fully set up to
+ * create all logger objects (as defined by LogArea::AreaTuple) with the correct
+ * area names and log levels.
+ *
+ * \param A configuration object with the log area names as toplevel keys.
+ */
+void create_all_loggers(Configuration config);
+
+/** \internal
+ * Returns the einhard::Logger object created for the area with the associated
+ * index \p id.
+ */
+einhard::Logger<> &retrieve_logger_impl(int id);
+
+/**
+ * Returns the einhard::Logger object created for the named area (see the LogArea types).
+ */
+template <typename LogAreaTag>
+inline einhard::Logger<> &logger() {
+  static_assert(LogAreaTag::id < std::tuple_size<LogArea::AreaTuple>::value &&
+                    LogAreaTag::id >= 0,
+                "The LogArea::AreaTuple is out of sync with the declared log "
+                "areas. Please fix! (see top of 'include/logging.h')");
+  return retrieve_logger_impl(LogAreaTag::id);
+}
+
+/**
+ * Hackery that is required to output the location in the source code where the
+ * log statement occurs.
+ */
+#define source_location \
+  __FILE__ ":" + std::to_string(__LINE__) + " (" + __func__ + ')'
+
+/**
+ * Return the default log level to use if no specific level is configured.
+ */
+einhard::LogLevel default_loglevel();
+
+/**
+ * Set the default log level (what will be returned from subsequent
+ * default_loglevel calls).
+ *
+ * \param level The new log level. See einhard::LogLevel.
+ */
+void set_default_loglevel(einhard::LogLevel level);
+
+/**
+ * Formatting helper
+ */
+template <typename T>
+struct FormattingHelper {
+  const T &value;
+  const int width;
+  const int precision;
+  const char *const unit;
+  friend std::ostream &operator<<(std::ostream &out,
+                                  const FormattingHelper &h) {
+    if (h.width > 0) {
+      out << std::setfill(' ') << std::setw(h.width);
+    }
+    if (h.precision >= 0) {
+        out << std::setprecision(h.precision);
+    }
+    out << h.value;
+    if (h.unit) {
+      out << ' ' << h.unit;
+    }
+    return out;
+  }
+};
+template <typename T>
+FormattingHelper<T> format(const T &value, const char *unit = 0, int width = -1,
+                           int precision = -1) {
+  return {value, width, precision, unit};
+}
+}  // namespace Smash
+
+namespace YAML {
+/** \internal
+ * Enables YAML-cpp to auto-convert a YAML Node to and from an einhard::LogLevel.
+ */
+template <>
+struct convert<einhard::LogLevel> {
+  static Node encode(const einhard::LogLevel &x) {
+    return Node{einhard::getLogLevelString(x)};
+  }
+  static bool decode(const Node &node, einhard::LogLevel &x) {
+    if (!node.IsScalar()) {
+      return false;
+    } else {
+      x = einhard::getLogLevel(node.Scalar());
+      return true;
+    }
+  }
+};
+}  // namespace YAML
+
+#endif  // SRC_INCLUDE_LOGGING_H_
---
+++ src/logging.cc
@@ -0,0 +1,87 @@
+/*
+ *
+ *    Copyright (c) 2014
+ *      SMASH Team
+ *
+ *    GNU General Public License (GPLv3 or later)
+ *
+ */
+
+#include "include/configuration.h"
+#include "include/logging.h"
+#include "include/stringfunctions.h"
+#include <array>
+
+namespace Smash {
+
+static einhard::LogLevel global_default_loglevel = einhard::ALL;
+
+einhard::LogLevel default_loglevel() {
+  return global_default_loglevel;
+}
+
+void set_default_loglevel(einhard::LogLevel level) {
+  global_default_loglevel = level;
+}
+
+/**
+ * An array that stores all pre-configured Logger objects. The objects can be
+ * accessed via the logger function.
+ */
+static std::array<einhard::Logger<>, std::tuple_size<LogArea::AreaTuple>::value>
+    global_logger_collection;
+
+einhard::Logger<> &retrieve_logger_impl(int id) {
+  return global_logger_collection[id];
+}
+
+template <std::size_t index>
+constexpr typename std::enable_if<(index == 0), int>::type
+find_longest_logger_name() {
+  return 0;
+}
+template <std::size_t index>
+constexpr typename std::enable_if<(index != 0), int>::type
+find_longest_logger_name() {
+  using LogAreaTag = typename std::remove_reference<decltype(
+      std::get<index - 1>(std::declval<LogArea::AreaTuple &>()))>::type;
+  return LogAreaTag::textual_length() > find_longest_logger_name<index - 1>()
+             ? LogAreaTag::textual_length()
+             : find_longest_logger_name<index - 1>();
+}
+
+template <std::size_t index, int>
+inline typename std::enable_if<(index == 0)>::type create_all_loggers_impl(
+    Configuration &) {}  // do nothing to end the recursion
+
+/*!
+ * \internal
+ * Recurse over the log areas in the LogArea::AreaTuple type. (The recursion is
+ * ended via the overload above.)
+ *
+ * For every entry in the list the corresponding Logger object in
+ * global_logger_collection is set up with area name and verbosity.
+ */
+template <std::size_t index,
+          int longest_name = find_longest_logger_name<index>()>
+inline typename std::enable_if<(index != 0)>::type create_all_loggers_impl(
+    Configuration &config) {
+  using LogAreaTag = typename std::remove_reference<decltype(
+      std::get<index - 1>(std::declval<LogArea::AreaTuple &>()))>::type;
+  static_assert(LogAreaTag::id == index - 1,
+                "The order of types in LogArea::AreaTuple does not match the "
+                "id values in the LogArea types. Please fix! (see top of "
+                "'include/logging.h')");
+  auto &logger = global_logger_collection[LogAreaTag::id];
+  const auto tmp = fill_both(LogAreaTag::textual(), longest_name);
+  logger.setAreaName(tmp);
+  logger.setVerbosity(
+      config.take({LogAreaTag::textual()}, global_default_loglevel));
+  create_all_loggers_impl<index - 1, longest_name>(config);
+}
+
+void create_all_loggers(Configuration config) {
+  create_all_loggers_impl<std::tuple_size<LogArea::AreaTuple>::value>(config);
+}
+
+}  // namespace Smash
