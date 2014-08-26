cd937803ebd1b77673a115f5a1aaf1822cd7360a
Matthias Kretz <kretz@kde.org>
2014-08-26 09:21:42 +0200

    simple string helper functions for aligning text
    A different method for aligning text (to std::setw or Smash::field) is
    to adjust the string itself to a fixed width. The fill_left/right/both
    functions do that by filling as many fill characters (default ' ') on
    the specified side to make the string a certain length.
    
    refs #853
    
    Signed-off-by: Matthias Kretz <kretz@kde.org>

---
+++ src/include/stringfunctions.h
@@ -0,0 +1,23 @@
+/*
+ *
+ *    Copyright (c) 2014
+ *      SMASH Team
+ *
+ *    GNU General Public License (GPLv3 or later)
+ *
+ */
+
+#ifndef SRC_INCLUDE_STRINGFUNCTIONS_H_
+#define SRC_INCLUDE_STRINGFUNCTIONS_H_
+
+#include <string>
+
+namespace Smash {
+
+std::string fill_left(const std::string &s, int width, char fill = ' ');
+std::string fill_right(const std::string &s, int width, char fill = ' ');
+std::string fill_both(const std::string &s, int width, char fill = ' ');
+
+}  // namespace Smash
+
+#endif  // SRC_INCLUDE_STRINGFUNCTIONS_H_
---
+++ src/stringfunctions.cc
@@ -0,0 +1,60 @@
+/*
+ *
+ *    Copyright (c) 2014
+ *      SMASH Team
+ *
+ *    GNU General Public License (GPLv3 or later)
+ *
+ */
+
+#include "include/stringfunctions.h"
+
+namespace Smash {
+
+inline static int utf8_adjust(const std::string &s, int width) {
+  for (unsigned char c : s) {
+    if (c >= 0xFC) {
+      width += 5;
+    } else if (c >= 0xF8) {
+      width += 4;
+    } else if (c >= 0xF0) {
+      width += 3;
+    } else if (c >= 0xE0) {
+      width += 2;
+    } else if (c == 0xCC || c == 0xCD) {
+      // combining character (2 Bytes) - doesn't appear at all
+      width += 2;
+    } else if (c >= 0xC0) {
+      width += 1;
+    }
+  }
+  return width;
+}
+
+std::string fill_left(const std::string &s, int width, char fill) {
+  width = utf8_adjust(s, width - s.size());
+  if (width > 0) {
+    return std::string(width, fill) + s;
+  }
+  return s;
+}
+
+std::string fill_right(const std::string &s, int width, char fill) {
+  width = utf8_adjust(s, width - s.size());
+  if (width > 0) {
+    return s + std::string(width, fill);
+  }
+  return s;
+}
+
+std::string fill_both(const std::string &s, int width, char fill) {
+  width = utf8_adjust(s, width - s.size());
+  if (width > 0) {
+    const int l = width / 2;
+    const int r = width - l;
+    return std::string(l, fill) + s + std::string(r, fill);
+  }
+  return s;
+}
+
+}  // namespace Smash
