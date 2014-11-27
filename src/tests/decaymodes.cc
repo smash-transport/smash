3e8a38546b9de4317a8170c527777c502b626266
Matthias Kretz <kretz@kde.org>
2014-11-28 00:45:35 +0100

    DecayModes test: adjust find call
    refs #1206
    
    Signed-off-by: Matthias Kretz <kretz@kde.org>

--- src/tests/decaymodes.cc
+++ src/tests/decaymodes.cc
@@ -63,7 +63,7 @@ TEST(load_decay_modes) {
   DecayModes::load_decaymodes(decays_input);
 
   {
-    const auto &rho0 = DecayModes::find(0x113);
+    const auto &rho0 = ParticleType::find(0x113).decay_modes();
     VERIFY(!rho0.is_empty());
     const auto &modelist = rho0.decay_mode_list();
     COMPARE(modelist.size(), 1u);
@@ -73,7 +73,7 @@ TEST(load_decay_modes) {
     COMPARE(modelist[0].particle_types()[1]->pdgcode().dump(), 0x80000211u);
   }
   {
-    const auto &rhoplus = DecayModes::find(0x213);
+    const auto &rhoplus = ParticleType::find(0x213).decay_modes();
     VERIFY(!rhoplus.is_empty());
     const auto &modelist = rhoplus.decay_mode_list();
     COMPARE(modelist.size(), 1u);
@@ -84,7 +84,7 @@ TEST(load_decay_modes) {
   }
   {
     // the rho- decay is auto-generated from the rho+ decay
-    const auto &rhominus = DecayModes::find(-0x213);
+    const auto &rhominus = ParticleType::find(-0x213).decay_modes();
     VERIFY(!rhominus.is_empty());
     const auto &modelist = rhominus.decay_mode_list();
     COMPARE(modelist.size(), 1u);
@@ -94,7 +94,7 @@ TEST(load_decay_modes) {
     COMPARE(modelist[0].particle_types()[1]->pdgcode().dump(), 0x111u);
   }
   {
-    const auto &omega = DecayModes::find(0x223);
+    const auto &omega = ParticleType::find(0x223).decay_modes();
     VERIFY(!omega.is_empty());
     const auto &modelist = omega.decay_mode_list();
     COMPARE(modelist.size(), 3u);
