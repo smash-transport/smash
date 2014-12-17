390259109e0558b0482c93806d2dcc598e8fe933
Dmytro Oliinychenko <oliiny@fias.uni-frankfurt.de>
2014-12-17 17:00:09 +0100

    remove obsolete Skyrme parameters from constants
    Signed-off-by: Dmytro Oliinychenko <oliiny@fias.uni-frankfurt.de>

--- src/include/constants.h
+++ src/include/constants.h
@@ -21,13 +21,6 @@ constexpr double twopi = 2. * M_PI;
 /* Ground state density of symmetric nuclear matter, fm^-3*/
 constexpr double rho0 = 0.168;
 
-/* Parameters of Skyrme potential */
-// a (rho/rho0) + b (rho/rho0)^tau
-// a, b - in MeV
-constexpr double skyrme_a = -209.2;
-constexpr double skyrme_b = 156.4;
-constexpr double skyrme_tau = 1.35;
-
 }  // namespace Smash
 
 #endif  // SRC_INCLUDE_CONSTANTS_H_
