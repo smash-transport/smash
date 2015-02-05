0045c6f2dabe68f8362ebe27a048ac3d187ade9a
Matthias Kretz <kretz@kde.org>
2015-02-05 10:17:15 +0100

    resonances: one subtraction less on the critical path
    Signed-off-by: Matthias Kretz <kretz@kde.org>

--- src/resonances.cc
+++ src/resonances.cc
@@ -94,8 +94,8 @@ double spectral_function_integrand(double resonance_mass,
   double resonance_width = params->type->total_width(resonance_mass);
 
   /* center-of-mass momentum of final state particles */
-  if (mandelstam_s - (stable_mass + resonance_mass)
-      * (stable_mass + resonance_mass) > 0.0) {
+  if (mandelstam_s >
+      (stable_mass + resonance_mass) * (stable_mass + resonance_mass)) {
     double cm_momentum_final
       = std::sqrt((mandelstam_s - (stable_mass + resonance_mass)
               * (stable_mass + resonance_mass))
