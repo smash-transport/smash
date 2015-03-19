31e954cb967f154ea56aa6e208bfe77c00d979d1
Matthias Kretz <kretz@kde.org>
2015-03-19 16:51:55 +0100

    main: setup fp traps first thing in main()
    If _GNU_SOURCE is defined, use feenableexcept to enable traps on
    FE_DIVBYZERO, FE_INVALID, FE_OVERFLOW, and FE_UNDERFLOW. If any of those
    fails, or _GNU_SOURCE is not defined, then print a warning.
    
    refs #748
    
    Signed-off-by: Matthias Kretz <kretz@kde.org>

--- src/smash.cc
+++ src/smash.cc
@@ -10,6 +10,7 @@
 #include <boost/filesystem.hpp>
 #include <boost/filesystem/fstream.hpp>
 
+#include <fenv.h>
 #include <getopt.h>
 #include <cstdio>
 #include <cstdlib>
@@ -125,6 +126,43 @@ void ensure_path_is_valid(const bf::path &path) {
   }
 }
 
+void setup_floatingpoint_traps() {
+  const auto &log = logger<LogArea::Main>();
+
+  // standard C/C++ don't have a function to modify the trapping behavior. You
+  // can only save and restore the setup. With glibc you can change it via
+  // feenableexcept and fedisableexcept.
+#ifdef _GNU_SOURCE
+  // pole error occurred in a floating-point operation:
+  if (-1 == feenableexcept(FE_DIVBYZERO)) {
+    log.warn("Failed to setup trap on pole error.");
+  }
+
+  // domain error occurred in an earlier floating-point operation:
+  if (-1 == feenableexcept(FE_INVALID)) {
+    log.warn("Failed to setup trap on domain error.");
+  }
+
+  // the result of the earlier floating-point operation was too large to be
+  // representable:
+  if (-1 == feenableexcept(FE_OVERFLOW)) {
+    log.warn("Failed to setup trap on overflow.");
+  }
+
+  // the result of the earlier floating-point operation was subnormal with a
+  // loss of precision:
+  if (-1 == feenableexcept(FE_UNDERFLOW)) {
+    log.warn("Failed to setup trap on underflow.");
+  }
+
+  // there's also FE_INEXACT, but this traps if "rounding was necessary to store
+  // the result of an earlier floating-point operation". This is common and not
+  // really an error condition.
+#else
+  log.warn("Failed to setup floating-point traps.");
+#endif
+}
+
 }  // unnamed namespace
 
 }  // namespace Smash
@@ -132,6 +170,8 @@ void ensure_path_is_valid(const bf::path &path) {
 /* main - do command line parsing and hence decides modus */
 int main(int argc, char *argv[]) {
   using namespace Smash;
+  setup_floatingpoint_traps();
+
   const auto &log = logger<LogArea::Main>();
 
   constexpr option longopts[] = {
