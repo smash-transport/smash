16ba7ecb64765b5d88827081c8378e4d9fbdc4d3
Dmytro Oliinychenko <oliiny@fias.uni-frankfurt.de>
2015-05-04 11:36:45 +0200

    Test width of stable and semistable channels
    Currently the test is failing, probably because of
    issue #1680
    
    This refs #648, #1680
    
    Signed-off-by: Dmytro Oliinychenko <oliiny@fias.uni-frankfurt.de>

--- src/tests/decayaction.cc
+++ src/tests/decayaction.cc
@@ -42,28 +42,78 @@ TEST(init_decay_channels) {
 }
 
 TEST(create_decayaction) {
+  // Create particle H, A1 and make sure their properties are as intended
   ParticleData H{ParticleType::find(0x50661)};
-  H.set_4momentum(H.type().mass() + 0.1, ThreeVector(1.0, 0.0, 0.0));
-  const float time_of_execution = 4.5f;
-  // H.type() - ParticleType
-  // H.type().decay_modes() - DecayModes
-  // H.type().decay_modes().decay_mode_list() - DecayBranchList
+  ParticleData A1{ParticleType::find(0x10661)};
+  const float m0_H = H.type().mass();
+  const float G0_H = H.type().width_at_pole();
+  const float m0_A1 = A1.type().mass();
+  H.set_4momentum(H.type().mass() + 3.0f, ThreeVector(1.0, 0.0, 0.0));
+  const float m_H = H.effective_mass();
+  COMPARE(m0_H, 8.0f);
+  COMPARE(G0_H, 1.0f);
+  COMPARE(m_H, 11.0f);
+  COMPARE(m0_A1, 1.0f);
+
+  // Initialize decays of H and check their properties
+  DecayBranchList H_decays = H.type().get_partial_widths(m_H);
+  VERIFY(H_decays.size() == 3);
+  float tmp1, tmp2, width_expected;
+
   int decaymodes_counter = 0;
-  for (const auto &mode : H.type().decay_modes().decay_mode_list()) {
-    // mode - unique pointer to DecayBranch
-    // mode->type() - DecayType
+  for (const auto &mode : H_decays) {
+    float ang_mom = mode->type().angular_momentum();
+    float width = mode->type().width(m0_H, G0_H, m_H);
     std::cout << "Decaymode " << decaymodes_counter << ": " <<
                  typeid(mode->type()).name() << ", " <<
-                 "angular momentum: " << mode->type().angular_momentum() <<
-                 ", width: " << mode->type().width(H.type().mass(),
-                                                   H.type().width_at_pole(),
-                                                    H.effective_mass())
-                 << std::endl;
+                 "angular momentum: " << ang_mom <<
+                 ", width: " << width << std::endl;
+    VERIFY(ang_mom == 0);
+    // Check if mass-dependent width behaves as expected
+    switch (decaymodes_counter) {
+      // Semistable two-body decay H -> A3 + A2
+      case 0:
+        // Here the following mathematica code was used for check:
+        /* mA2 = 1.5; m0A3 = 3.0; G0A3 = 0.2; m0H = 8.0; mH = 11.0;
+           mA3min = 2.5; G0H = 1.0; L = 1.6;
+           PostCutoff[m_] := (L^4 + ((mA3min + mA2)^2 - m0H^2)^2/4)/
+                      (L^4 + (m^2 - ((mA3min + mA2)^2 + m0H^2)/2)^2)
+          rho[m_] :=  NIntegrate[
+             2 mA3*( mA3*G0A3/Pi)/((mA3^2 - m0A3^2)^2 + mA3^2 G0A3^2)*
+             Sqrt[(m^2 + mA2^2 - mA3^2)^2/m^2/4 - mA2^2], {mA3, mA3min,
+             m - mA2} ]
+          rho[mH]/rho[m0H]*PostCutoff[mH]^2*G0H  // results in 0.0122167
+        */
+        /* It might seem weird that resulting width is so unphysically
+           small. This is because of the Post form-factor, which is intended
+           for resonanse masses smaller than 2 GeV. For higher masses it
+           does not give physically reasonable results. But this is only
+           code test, so we can live with it.
+        */
+        COMPARE_RELATIVE_ERROR(width, 0.0122167f, 1.e-7);
+        break;
+      // Stable two-body decay H -> A1 + A1
+      case 1:
+        /* In the case of equal product masses expressions simplify
+           and one gets Gamma(m) / Gamma(m0) =
+           = \sqrt((1 - 4 m_{A1}^2 / m_H^2) / (1 - 4 m_{A1}^2 / m0_H^2))
+        */
+        tmp1 = 2 * m0_A1 / m_H;
+        tmp2 = 2 * m0_A1 / m0_H;
+        width_expected = G0_H * std::sqrt((1.f - tmp1*tmp1)/(1.f - tmp2*tmp2));
+        COMPARE_RELATIVE_ERROR(width, width_expected, 1.e-6);
+        break;
+      // three-body decay H -> A2 + A2 + A1
+      case 2:
+        break;
+      // Should never get here
+      default: VERIFY(0 == 1);
+    }
     decaymodes_counter++;
   }
-  DecayBranchList H_decays =
-          H.type().get_partial_widths(H.effective_mass());
+  VERIFY(decaymodes_counter == 3);
 
+  const float time_of_execution = 4.5f;
   const auto act = make_unique<DecayAction>(H, time_of_execution);
   std::cout << *act << std::endl;
 }
