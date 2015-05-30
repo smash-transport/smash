26efddf943a3b20fd40e9d6c7bfa9d8adb197f56
Matthias Kretz <kretz@kde.org>
2015-05-30 10:29:16 +0200

    FPE traps test: don't rely on compiler specifics
    * I need two tests because a SIGFPE can be delivered to a process only
    once. The second time anything may happen (i.e. undefined behavior).
    * Use longjmp to get back out of the signal handler and then an
    exception to communicate the FPE back to the test function.
    * Test that enable_float_traps works as expected (fpe.cc).
    * Test that without_float_traps works as expected
    (without_float_traps.cc).
    
    refs #1804
    
    Signed-off-by: Matthias Kretz <kretz@kde.org>

--- src/tests/without_float_traps.cc
+++ src/tests/without_float_traps.cc
@@ -39,20 +39,25 @@ static void do_division(float x) {
   }
 }
 
-// this test cannot be merged with the without_float_traps.cc test because a
-// single process can only handle a single SIGFPE. Actually program resumption
-// after the SIGFPE handler is undefined behavior. But we're still in compiler
-// extension land, so the test works.
-TEST(enable_float_traps) {
+// this test cannot be merged with the fpe.cc test because a single process can
+// only handle a single SIGFPE. Actually program resumption after the SIGFPE
+// handler is undefined behavior. But we're still in compiler extension land, so
+// the test works.
+TEST(without_float_traps) {
   std::signal(SIGFPE, &handle_fpe);
 
-  VERIFY(!std::fetestexcept(FE_DIVBYZERO));  // flag not set yet
-  Smash::enable_float_traps(FE_DIVBYZERO);   // now it must trap
-  VERIFY(!std::fetestexcept(FE_DIVBYZERO));  // flag not set yet
+  Smash::enable_float_traps(FE_DIVBYZERO);     // now div by zero must trap
+  Smash::without_float_traps([&] {             // temporarily disable the trap
+    VERIFY(!std::fetestexcept(FE_DIVBYZERO));  // flag not set yet
+    do_division(2.f);  // this sets the flag, but doesn't trap
+    VERIFY(std::fetestexcept(FE_DIVBYZERO));  // flag must be set now
+  });
+  VERIFY(!std::fetestexcept(
+             FE_DIVBYZERO));  // after the lambda the flag must be clear again
 
   bool got_exception = false;
   try {
-    do_division(3.f);  // traps must be enabled now
+    do_division(3.f);  // after the lambda this must trap again
     FAIL() << "may never be reached";
   } catch (fpe_exception &) {  // the signal handler produces an exception via
                                // longjmp
