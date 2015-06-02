412de1c81ca47e8e108e5316fc36b92deb12373d
Dmytro Oliinychenko <oliiny@fias.uni-frankfurt.de>
2015-06-02 16:56:31 +0200

    Lattice class with tests
    Because of templates all the lattice class moved to header file.
    Test checks every function of the lattice.
    
    This refs #1576
    
    Signed-off-by: Dmytro Oliinychenko <oliiny@fias.uni-frankfurt.de>

--- src/include/lattice.h
+++ src/include/lattice.h
@@ -18,6 +18,7 @@
 
 #include "forwarddeclarations.h"
 #include "fourvector.h"
+#include "logging.h"
 
 namespace Smash {
 
@@ -33,9 +34,22 @@ class RectangularLattice {
     * i,j,k comprises volume ((i,i+1)lx/nx; (j,j+1)ly/ny; (k,k+1)lz/nz),
     * i: 0,nx-1; j: 0, ny-1; k: 0, nz-1;
     */
-  RectangularLattice(std::array<float, 3> l,
-                     std::array<size_t, 3> n,
-                     std::array<float, 3> origin, bool periodic);
+  RectangularLattice(const std::array<float, 3> &l,
+                     const std::array<int, 3> &n,
+                     const std::array<float, 3> &origin, bool per)
+  : l_(l),
+    n_(n),
+    csize_{l[0]/n[0], l[1]/n[1], l[2]/n[2]},
+    origin_(origin),
+    periodic_(per) {
+    lattice_.resize(n_[0]*n_[1]*n_[2]);
+    const auto &log = logger<LogArea::Lattice>();
+    log.info("Rectangular lattice created: sizes[fm] = (",
+             l_[0], ",", l_[1], ",", l_[2], "), dims = (",
+             n_[0], ",", n_[1], ",", n_[2], "), origin = (",
+             origin_[0], ",", origin_[1], ",", origin_[2],
+             "), periodic: ", periodic_);
+  }
 
   /// Sets all values on lattice to zeros
   void reset() {
@@ -43,11 +57,11 @@ class RectangularLattice {
   }
 
   /// Checks if 3D index is out of lattice bounds
-  inline bool is_on_lattice(int ix, int iy, int iz) {
-    return periodic_ ||
-           ((ix >= 0 && ix < n_[0]) &&
-            (iy >= 0 && iy < n_[1]) &&
-            (iz >= 0 && iz < n_[2]));
+  inline bool out_of_bounds(int ix, int iy, int iz) {
+    return !periodic_ &&
+           (ix < 0 || ix >= n_[0] ||
+            iy < 0 || iy >= n_[1] ||
+            iz < 0 || iz >= n_[2]);
   }
 
   /// Returns coordinate of cell center given its index
@@ -80,9 +94,8 @@ class RectangularLattice {
   T operator[](std::size_t i) const { return lattice_[i]; }
   std::size_t size() const { return lattice_.size(); }
 
-
   T& node(int ix, int iy, int iz) {
-    return periodic ?
+    return periodic_ ?
            lattice_[positive_modulo(ix, n_[0])   + n_[0] *
                     (positive_modulo(iy, n_[1])  + n_[1] *
                      positive_modulo(iz, n_[2]))] :
@@ -95,7 +108,37 @@ class RectangularLattice {
    */
   void iterate_sublattice(const std::array<int, 3> &lower_bounds,
                           const std::array<int, 3> &upper_bounds,
-                          const std::function<void(T&, int, int, int)> &);
+                          const std::function<void(T&, int, int, int)> &func) {
+    const auto &log = logger<LogArea::Lattice>();
+    log.debug("Iterating sublattice with lower bound index (",
+              lower_bounds[0], ",", lower_bounds[1], ",", lower_bounds[2],
+              "), upper bound index (",
+              upper_bounds[0], ",", upper_bounds[1], ",", upper_bounds[2], ")");
+
+    if (periodic_) {
+      for (int iz = lower_bounds[2]; iz < upper_bounds[2]; iz++) {
+        const int z_offset = positive_modulo(iz, n_[2]) * n_[1];
+        for (int iy = lower_bounds[1]; iy < upper_bounds[1]; iy++) {
+          const int y_offset = n_[0] * (positive_modulo(iy, n_[1]) + z_offset);
+          for (int ix = lower_bounds[0]; ix < upper_bounds[0]; ix++) {
+            const int index = positive_modulo(ix, n_[0]) + y_offset;
+            func(lattice_[index], ix, iy, iz);
+          }
+        }
+      }
+    } else {
+      for (int iz = lower_bounds[2]; iz < upper_bounds[2]; iz++) {
+        const int z_offset = iz * n_[1];
+        for (int iy = lower_bounds[1]; iy < upper_bounds[1]; iy++) {
+          const int y_offset = n_[0] * (iy + z_offset);
+          for (int ix = lower_bounds[0]; ix < upper_bounds[0]; ix++) {
+            func(lattice_[ix + y_offset], ix, iy, iz);
+          }
+        }
+      }
+    }
+
+  };
 
   /**
    * Iterates only nodes, whose cell centers lie not further than r_cut in
@@ -103,7 +146,33 @@ class RectangularLattice {
    * from one particle to the lattice.
    */
   void iterate_in_radius(const ThreeVector& point, const double r_cut,
-                         const std::function<void(T&, int, int, int)> &);
+                         const std::function<void(T&, int, int, int)> &func) {
+    std::array<int, 3> l_bounds, u_bounds;
+
+    // Array holds value at the cell center: r_center = r_0 + (i+0.5)cell_size,
+    // where i is index in any direction. Therefore we want cells with condition
+    // (r-r_cut)*csize - 0.5 < i < (r+r_cut)*csize - 0.5, r = r_center - r_0
+    for (int i = 0; i < 3; i++) {
+      l_bounds[i] = int_ceil((point[i] - origin_[i] - r_cut)/csize_[i] - 0.5);
+      u_bounds[i] = int_ceil((point[i] - origin_[i] + r_cut)/csize_[i] - 0.5);
+    }
+
+    if (!periodic_) {
+      for (int i = 0; i < 3; i++) {
+        if (l_bounds[i] < 0) {
+          l_bounds[i] = 0;
+        }
+        if (u_bounds[i] > n_[i]) {
+          u_bounds[i] = n_[i];
+        }
+        if (l_bounds[i] > n_[i] || u_bounds[i] < 0) {
+          return;
+        }
+      }
+    }
+    iterate_sublattice(l_bounds, u_bounds, func);
+
+  };
 
  protected:
   /// The lattice itself, array containing physical quantities
