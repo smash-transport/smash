c7896bb3ff7c0ed75d294ad144b2f096d5bb5ab3
Vinzent Steinberg <Vinzent.Steinberg@gmail.com>
2015-06-15 16:35:58 +0200

    interpolation: make sure values are sorted

--- src/include/interpolation.h
+++ src/include/interpolation.h
@@ -13,6 +13,7 @@
 #include <vector>
 #include <cassert>
 #include <cstddef>
+#include <algorithm>
 
 template <typename T>
 class InterpolateLinear {
@@ -57,15 +58,50 @@ T InterpolateLinear<T>::operator()(T x) const {
   return slope_ * x + yintercept_;
 }
 
+using Permutation = std::vector<size_t>;
+
+/// Calculate the permutations necessary for sorting a vector.
+template <typename T, typename Cmp>
+Permutation generate_sort_permutation(
+    std::vector<T> const& v, Cmp compare)
+{
+  Permutation p(v.size());
+  std::iota(p.begin(), p.end(), 0);
+  std::sort(p.begin(), p.end(),
+      [&](size_t i, size_t j) {
+        return compare(v[i], v[j]);
+    });
+  return p;
+}
+
+/// Apply a permutation to a vector in place.
+template <typename T>
+void apply_permutation(
+    std::vector<T>& v, const Permutation& p)
+{
+  std::transform(p.begin(), p.end(), v.begin(),
+      [&](size_t i) {
+        return v[i];
+      });
+}
+
 template <typename T>
 InterpolateData<T>::InterpolateData(const std::vector<T>& x,
                                     const std::vector<T>& y) {
   assert(x.size() == y.size());
   const size_t n = x.size();
   x_ = x;
+  std::vector<T> y_sorted = y;
+  const auto p = generate_sort_permutation(x,
+      [&](T const& a, T const& b) {
+        return a < b;
+      });
+  apply_permutation(x_, p);
+  apply_permutation(y_sorted, p);
   f_.reserve(n - 1);
   for (size_t i = 0; i < n - 1; i++) {
-    f_.emplace_back(InterpolateLinear<T>(x[i], y[i], x[i + 1], y[i + 1]));
+    f_.emplace_back(InterpolateLinear<T>(
+        x[i], y_sorted[i], x[i + 1], y_sorted[i + 1]));
   }
 }
 
