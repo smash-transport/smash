b286dfface19674c7c46e0022dbdd9650c607c19
Janus Weil <weil@fias.uni-frankfurt.de>
2015-06-25 17:21:00 +0200

    modifications to ParticleType::create_type_list (refs #1984): * turn the assert into a runtime error, so that it is also triggered in Release builds * update documentation

--- src/particletype.cc
+++ src/particletype.cc
@@ -188,7 +188,9 @@ void ParticleType::create_type_list(const std::string &input) {  // {{{
             [](const ParticleType &l,
                const ParticleType &r) { return l.pdgcode() < r.pdgcode(); });
 
-  assert(nullptr == all_particle_types);
+  if (all_particle_types != nullptr) {
+    throw std::runtime_error("Error: Type list was already built!");
+  }
   all_particle_types = &type_list;  // note that type_list is a function-local
                                     // static and thus will live on until after
                                     // main().
