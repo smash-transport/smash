8ebd781f43fa4244aae61a1e251db315008e6b65
Vinzent Steinberg <Vinzent.Steinberg@gmail.com>
2015-07-20 17:02:29 +0200

    add function to calculate Clebsch-Gordan for three coupled particles

--- src/include/resonances.h
+++ src/include/resonances.h
@@ -59,6 +59,25 @@ inline double isospin_clebsch_gordan(const ParticleType &p_a,
 }
 
 
+/* Calculate isospin Clebsch-Gordan coefficient for three particles p_a, p_b and
+ * p_c coupling to a total isospin (I_tot, I_z) Res.
+ *
+ * Note that the coefficients also depend on the coupled isospin I_ab.
+ *
+ * \fpPrecision Why \c double?
+ */
+inline double isospin_clebsch_gordan(const ParticleType &p_a,
+                                     const ParticleType &p_b,
+                                     const ParticleType &p_c,
+                                     int I_tot, int I_z, int I_ab) {
+    const int I_abz = p_a.isospin3() + p_b.isospin3();
+    return clebsch_gordan (I_ab, p_c.isospin(), I_tot,
+                           I_abz, p_c.isospin3(), I_z)
+         * clebsch_gordan (p_a.isospin(), p_b.isospin(), I_ab,
+                           p_a.isospin3(), p_b.isospin3(), I_abz);
+}
+
+
 /**
  * Spectral function
  * \f$A(m)=\frac{1}{\pi}\frac{m\Gamma(m)}{(m^2-m_0^2)^2+(m\Gamma(m))^2}\f$
