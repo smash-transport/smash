6155b53625762ea9921d16955c2520e94d175576
Jan Staudenmaier <staudenmaier@fias.uni-frankfurt.de>
2015-07-24 20:24:40 +0200

    Double counting fix now actually works
    Never change something after you tested it, without testing again.
    
    this refs #2487

--- src/decayactionsfinderdilepton.cc
+++ src/decayactionsfinderdilepton.cc
@@ -24,7 +24,8 @@ ActionList DecayActionsFinderDilepton::find_actions_in_cell(
   ActionList actions;
 
   for (const auto &p : search_list) {
-    unsigned long n_all_modes = p.type().decay_modes().decay_mode_list().size();
+    unsigned long n_all_modes =
+                        p.type().get_partial_widths(p.effective_mass()).size();
     if (n_all_modes == 0) {
       continue;
     }
