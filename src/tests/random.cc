412583ae06d6e8cef4c59bce496415f171c35756
Janus Weil <weil@fias.uni-frankfurt.de>
2015-08-25 13:36:42 +0200

    random test: modify the 'test_distribution' function (refs #1367) * add argument 'n_test' * move variables 'PRINT', 'sigmabins' and 'allowed' inside * pull factor 'n_test' out of normalization * get rid of variable 'normalize' * add documentation

--- src/tests/random.cc
+++ src/tests/random.cc
@@ -13,37 +13,42 @@
 
 using namespace Smash;
 
-// print the distribution (for plotting)
-constexpr bool PRINT = false;
-
-constexpr int N_TEST = 10000000;
-constexpr int sigmabins = 4;
-// We will check that at least the following ratios of numbers of samples lie
-// within the corresponding sigma environment.
-constexpr double allowed[sigmabins] = {.682 * .99, .954 * .99, .997 * .99, 1.0};
-
+/**
+ * Compare a random distribution to an analytical value.
+ *
+ * The distribution 'get_chi' is sampled 'n_test' times and put into a histogram
+ * with bin size 'dx'. Then it is compared to the analytical function
+ * 'get_analyt', multiplied by the normalization function 'norm'.
+ */
 template <typename Normalization, typename Chi, typename Analytical>
-void test_distribution(double dx, Normalization norm, Chi get_chi,
+void test_distribution(int n_test, double dx, Normalization norm, Chi get_chi,
                        Analytical get_analyt) {
+  // print the distribution (for plotting)
+  constexpr bool PRINT = false;
+
+  // We will check that at least the following ratios of numbers of samples lie
+  // within the corresponding sigma environment.
+  constexpr int sigmabins = 4;
+  constexpr double allowed[sigmabins] = {.682 * .99, .954 * .99, .997 * .99, 1.0};
+
+  // sample distribution and populate histogram
   std::map<int, int> hist{};
-  for (int i = 0; i < N_TEST; i++) {
+  for (int i = 0; i < n_test; i++) {
     const double chi = get_chi();
     ++hist[chi / dx];
   }
+
   int diffbad[sigmabins] = {0};
   int total = 0;
   for (auto b : hist) {
     // the bin
     const double x = dx * b.first;
-    // this comes from the integral of the distribution inside the bins.
-    const double normalize = norm(x);
     // the result that I got:
     const int result = b.second;
     // statistical error is sqrt(N)
     const double stat_err = sqrt(result);
-    // the analytical value (stuff that is independent of x is only
-    // calculated once before the loop)
-    const double analyt = get_analyt(x) * normalize;
+    // the analytical value times normalization times number of samples
+    const double analyt = get_analyt(x) * norm(x) * n_test;
     // if we want to print the distribution, print!
     if (PRINT) {
       printf("%7.3f %7d %7.3f %7.3f\n", x, result, stat_err, analyt);
@@ -69,10 +74,12 @@ void test_distribution(double dx, Normalization norm, Chi get_chi,
   }
 }
 
+constexpr int N_TEST = 1E7;
+
 TEST(exponential) {
   constexpr double dx = 0.1;
-  test_distribution(dx,
-                    [](double) { return (1 - exp(-dx)) * N_TEST; },
+  test_distribution(N_TEST, dx,
+                    [](double) { return (1 - exp(-dx)); },
                     []() { return Random::exponential(1.0); },
                     [](double x) { return exp(-x); });
 }
@@ -80,10 +87,10 @@ TEST(exponential) {
 TEST(x_exponential) {
   constexpr double dx = 0.05;
   test_distribution(
-      dx,
+      N_TEST, dx,
       [](double x) {
-        const double normalize_1 = (1 - exp(-dx)) * N_TEST;
-        const double normalize_2 = (1 + dx) * normalize_1 - dx * N_TEST;
+        const double normalize_1 = (1 - exp(-dx));
+        const double normalize_2 = (1 + dx) * normalize_1 - dx;
         return x * normalize_1 + normalize_2;
       },
       []() { return Random::exponential(1.0) + Random::exponential(1.0); },
@@ -93,14 +100,13 @@ TEST(x_exponential) {
 TEST(xsquared_exponential) {
   constexpr double dx = 0.1;
   test_distribution(
-      dx,
+      N_TEST, dx,
       [](double x) {
-        const double normalize_1 = (1 - exp(-dx)) * N_TEST / 2.0;
+        const double normalize_1 = (1 - exp(-dx)) / 2.0;
         const double normalize_2 = 2.0 * (dx + 1.0);
         const double normalize_3 =
-            N_TEST * (1.0 - exp(-dx) * (dx * (dx / 2.0 + 1.0) + 1.0));
-        return (normalize_1 * (x * x + x * normalize_2) - x * dx * N_TEST +
-                normalize_3);
+            (1.0 - exp(-dx) * (dx * (dx / 2.0 + 1.0) + 1.0));
+        return (normalize_1 * (x * x + x * normalize_2) - x * dx + normalize_3);
       },
       []() {
         return Random::exponential(1.0) + Random::exponential(1.0) +
@@ -111,8 +117,8 @@ TEST(xsquared_exponential) {
 
 TEST(canonical) {
   constexpr double dx = 0.0001;
-  test_distribution(dx,
-                    [](double) { return N_TEST * dx; },
+  test_distribution(N_TEST, dx,
+                    [](double) { return dx; },
                     []() { return Random::canonical(); },
                     [](double) { return 1.0; });
 }
@@ -120,8 +126,8 @@ TEST(canonical) {
 TEST(uniform) {
   constexpr double dx = 0.01;
   auto random_4_6 = Random::make_uniform_distribution(-4.0, 6.0);
-  test_distribution(dx,
-                    [](double) { return N_TEST * dx / 10.0; },
+  test_distribution(N_TEST, dx,
+                    [](double) { return dx / 10.0; },
                     [&]() { return random_4_6(); },
                     [](double) { return 1.0; });
 }
