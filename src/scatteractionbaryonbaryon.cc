37757f6a2c0343e88e04484150cac623bace7863
Janus Weil <weil@fias.uni-frankfurt.de>
2015-09-09 13:34:21 +0200

    implement resonance absorption via Delta R -> N N (refs #1287) * rename ScatterActionBaryonBaryon::nuc_res_to_nuc_nuc to bar_bar_to_nuc_nuc and adjust it accordingly

--- src/scatteractionbaryonbaryon.cc
+++ src/scatteractionbaryonbaryon.cc
@@ -35,57 +35,45 @@ float ScatterActionBaryonBaryon::total_cross_section() const {
 
 CollisionBranchList ScatterActionBaryonBaryon::two_to_two_cross_sections() {
   CollisionBranchList process_list;
-  const ParticleType &type_particle_a = incoming_particles_[0].type();
-  const ParticleType &type_particle_b = incoming_particles_[1].type();
+  const ParticleType &type_a = incoming_particles_[0].type();
+  const ParticleType &type_b = incoming_particles_[1].type();
 
-  if (type_particle_a.pdgcode().iso_multiplet() == 0x1112 ||
-      type_particle_b.pdgcode().iso_multiplet() == 0x1112) {
-    /* Nucleon+Resonance: absorption */
-    process_list = nuc_res_to_nuc_nuc(type_particle_a, type_particle_b);
+  if (type_a.pdgcode().is_nucleon() || type_a.pdgcode().is_Delta() ||
+      type_b.pdgcode().is_nucleon() || type_b.pdgcode().is_Delta()) {
+    /* N R -> N N, Delta R -> N N */
+    process_list = bar_bar_to_nuc_nuc(type_a, type_b);
   }
 
   return process_list;
 }
 
 
-CollisionBranchList ScatterActionBaryonBaryon::nuc_res_to_nuc_nuc(
-                            const ParticleType &type_particle_a,
-                            const ParticleType &type_particle_b) {
-  ParticleTypePtr type_resonance, type_nucleon;
+CollisionBranchList ScatterActionBaryonBaryon::bar_bar_to_nuc_nuc(
+                            const ParticleType &type_a,
+                            const ParticleType &type_b) {
   CollisionBranchList process_list;
-
-  if (type_particle_a.pdgcode().iso_multiplet() == 0x1112) {
-    type_nucleon = &type_particle_a;
-    type_resonance = &type_particle_b;
-  } else if (type_particle_b.pdgcode().iso_multiplet() == 0x1112) {
-    type_nucleon = &type_particle_b;
-    type_resonance = &type_particle_a;
-  } else {
-    throw std::runtime_error("Error: no nucleon found in nuc_res_to_nuc_nuc!");
-  }
 
   const double s = mandelstam_s();
   const double srts = sqrt_s();
   /* CM momentum in final state */
-  double p_cm_final = std::sqrt(s - 4.*type_nucleon->mass_sqr())/2.;
+  double p_cm_final = std::sqrt(s - 4.*nucleon_mass*nucleon_mass)/2.;
 
   /* Loop over all nucleon charge states. */
   for (ParticleTypePtr nuc_a : ParticleType::list_nucleons()) {
     for (ParticleTypePtr nuc_b : ParticleType::list_nucleons()) {
       /* Check for charge conservation. */
-      if (type_resonance->charge() + type_nucleon->charge()
+      if (type_a.charge() + type_b.charge()
           != nuc_a->charge() + nuc_b->charge()) {
         continue;
       }
 
-      int I_z = type_resonance->isospin3() + type_nucleon->isospin3();
+      const int I_z = type_a.isospin3() + type_b.isospin3();
 
       /* Compute total isospin range with given initial and final particles. */
-      int I_max = std::min(type_resonance->isospin() + type_nucleon->isospin(),
-                          nuc_a->isospin() + nuc_b->isospin());
-      int I_min = std::max(
-          std::abs(type_resonance->isospin() - type_nucleon->isospin()),
-          std::abs(nuc_a->isospin() - nuc_b->isospin()));
+      const int I_max = std::min(type_a.isospin() + type_b.isospin(),
+                                 nuc_a->isospin() + nuc_b->isospin());
+      int I_min = std::max(std::abs(type_a.isospin() - type_b.isospin()),
+                           std::abs(nuc_a->isospin() - nuc_b->isospin()));
       I_min = std::max(I_min, std::abs(I_z));
 
       /* Loop over total isospin in allowed range.
@@ -94,7 +82,7 @@ CollisionBranchList ScatterActionBaryonBaryon::nuc_res_to_nuc_nuc(
       for (int I_tot = I_max; I_tot >= I_min; I_tot -= 2) {
         isospin_factor = isospin_factor +
             isospin_clebsch_gordan(*nuc_a, *nuc_b, I_tot, I_z)
-          * isospin_clebsch_gordan(*type_resonance, *type_nucleon, I_tot, I_z);
+          * isospin_clebsch_gordan(type_a, type_b, I_tot, I_z);
       }
 
       /* If Clebsch-Gordan coefficient is zero, don't bother with the rest */
@@ -104,7 +92,7 @@ CollisionBranchList ScatterActionBaryonBaryon::nuc_res_to_nuc_nuc(
 
       /* Calculate matrix element for inverse process. */
       const float matrix_element =
-          nn_to_resonance_matrix_element(srts, *type_resonance, *type_nucleon);
+          nn_to_resonance_matrix_element(srts, type_a, type_b);
       if (matrix_element <= 0.) {
         continue;
       }
@@ -112,21 +100,19 @@ CollisionBranchList ScatterActionBaryonBaryon::nuc_res_to_nuc_nuc(
       /** Cross section for 2->2 resonance absorption, obtained via detailed
        * balance from the inverse reaction.
        * See eqs. (B.6), (B.9) and (181) in \iref{Buss:2011mx}.
-       * There is a symmetry factor 1/2 and a spin factor 2/(2S+1) involved,
-       * which combine to 1/(2S+1). */
+       * There is a symmetry factor 1/2 and a spin factor 4/(2S_a+1)/(2S_b+1)
+       * involved. */
       float xsection = isospin_factor * isospin_factor
-                     * p_cm_final * matrix_element
-                     / ((type_resonance->spin()+1) * s * cm_momentum());
+                     * p_cm_final * matrix_element * 2.
+                     / ((type_a.spin()+1)*(type_b.spin()+1) * s*cm_momentum());
 
       if (xsection > really_small) {
         process_list.push_back(make_unique<CollisionBranch>
                                (*nuc_a, *nuc_b, xsection,
                                 ProcessType::TwoToTwo));
         const auto &log = logger<LogArea::ScatterAction>();
-        log.debug("Found 2->2 absoption process for resonance ",
-                  *type_resonance);
-        log.debug("2->2 with original particles: ",
-                  type_particle_a, type_particle_b);
+        log.debug("2->2 absorption with original particles: ",
+                  type_a, type_b);
       }
     }
   }
