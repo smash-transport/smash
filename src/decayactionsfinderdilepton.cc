d0af95a0fb8ecd32c9ff85bb4f55e78f6c6cfd72
Vinzent Steinberg <Vinzent.Steinberg@gmail.com>
2016-05-20 16:03:37 +0200

    Fix a compiler warning

--- src/decayactionsfinderdilepton.cc
+++ src/decayactionsfinderdilepton.cc
@@ -37,11 +37,14 @@ ActionList DecayActionsFinderDilepton::find_actions_in_cell(
     // if particle can only decay into dileptons or is stable, use shining only
     // in find_final_actions and ignore them here, also unformed
     // resonances cannot decay
+    #pragma GCC diagnostic push
+    #pragma GCC diagnostic ignored "-Wfloat-equal"
     if (dil_modes.size() == n_all_modes || p.type().is_stable()
         || (p.formation_time() > p.position().x0() &&
             p.cross_section_scaling_factor() == 0.0)) {
       continue;
     }
+    #pragma GCC diagnostic pop
 
     for (DecayBranchPtr & mode : dil_modes) {
       float sh_weight = 0.0;
