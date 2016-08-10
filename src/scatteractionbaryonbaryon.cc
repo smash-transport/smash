cee3a48a4c5935fa45337e635022b2cad9b143ff
Dmytro Oliinychenko <oliiny@fias.uni-frankfurt.de>
2016-08-10 13:34:21 +0200

    I_tot_range now returns not vector, but Imax and Imin
    This make performance clearly faster.
    
    Signed-off-by: Dmytro Oliinychenko <oliiny@fias.uni-frankfurt.de>

--- src/scatteractionbaryonbaryon.cc
+++ src/scatteractionbaryonbaryon.cc
@@ -68,7 +68,8 @@ CollisionBranchList ScatterActionBaryonBaryon::bar_bar_to_nuc_nuc(
         continue;
       }
       // loop over total isospin
-      for (const int twoI : I_tot_range(*nuc_a, *nuc_b)) {
+      const std::pair<int, int> I_range = I_tot_range(*nuc_a, *nuc_b);
+      for (int twoI = I_range.first; twoI >= I_range.second; twoI -= 2) {
         const float isospin_factor = isospin_clebsch_gordan_sqr_2to2(type_a,
                                                   type_b, *nuc_a, *nuc_b, twoI);
         /* If Clebsch-Gordan coefficient is zero, don't bother with the rest */
