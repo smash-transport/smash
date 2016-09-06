a4537da567d4fafdbdd7f404a42d4541597ed182
Jean-Bernard Rose <rose@fias.uni-frankfurt.de>
2016-09-06 11:21:43 +0200

    Finished reorganization of NN->** collisions
    New function find_xsection_from_type is now being called for all instances, and properly returns a CollisionBranchList that is then moved to the final 2-to-2 process list.
    -refs #3695

--- src/scatteractionnucleonnucleon.cc
+++ src/scatteractionnucleonnucleon.cc
@@ -115,7 +115,7 @@ CollisionBranchList ScatterActionNucleonNucleon::find_xsection_from_type(
       /* Check for charge conservation. */
       if (type_res_1->charge() + type_res_2->charge() !=
           type_particle_a.charge() + type_particle_b.charge()) {
-        return channel_list;
+        continue;
       }
     
       // loop over total isospin
@@ -165,90 +165,42 @@ CollisionBranchList ScatterActionNucleonNucleon::find_xsection_from_type(
                     type_res_1, ", ", type_res_2);
           log.debug("2->2 with original particles: ",
                     type_particle_a, type_particle_b);
+       //   std::cout << xsection << " ";
         }
       }
     }
   }
+  //std::cout << std::endl;
   return channel_list;
 }
 
 CollisionBranchList ScatterActionNucleonNucleon::two_to_two_inel(
                             const ParticleType &type_particle_a,
                             const ParticleType &type_particle_b) {
-  const auto &log = logger<LogArea::ScatterAction>();
-  CollisionBranchList process_list;
-  const double s = mandelstam_s();
+  CollisionBranchList process_list, channel_list;
   const double sqrts = sqrt_s();
 
   /* First: Find N N → N R channels. */
-  //CollisionBranchList channel_list =
-    find_xsection_from_type(type_particle_a, type_particle_b,
-    ParticleType::list_baryon_resonances(), ParticleType::list_nucleons(),
-    [&sqrts](const ParticleType &type_res_1){return type_res_1.iso_multiplet()->get_integral_NR(sqrts);});
-  //process_list.insert(process_list.end(), ltype.begin(), ltype.end());
+  channel_list = find_xsection_from_type(type_particle_a, type_particle_b,
+      ParticleType::list_baryon_resonances(), ParticleType::list_nucleons(),
+      [&sqrts](const ParticleType &type_res_1){
+          return type_res_1.iso_multiplet()->get_integral_NR(sqrts);
+      });
+  process_list.reserve(process_list.size() + channel_list.size());
+  std::move(channel_list.begin(), channel_list.end(),
+      std::inserter(process_list, process_list.end()));
+  channel_list.clear();
 
   /* Second: Find N N → Δ R channels. */
-  /* Loop over all baryon resonances. */
-  for (ParticleTypePtr type_res_1 : ParticleType::list_baryon_resonances()) {
-    /* Loop over second particle (Δ). */
-    for (ParticleTypePtr type_res_2 : ParticleType::list_Deltas()) {
-      /* Check for charge conservation. */
-      if (type_res_1->charge() + type_res_2->charge() !=
-          type_particle_a.charge() + type_particle_b.charge()) {
-        continue;
-      }
-
-      // loop over total isospin
-      for (const int twoI : I_tot_range(type_particle_a, type_particle_b)) {
-        const float isospin_factor = isospin_clebsch_gordan_sqr_2to2(
-              type_particle_a, type_particle_b, *type_res_1, *type_res_2, twoI);
-        /* If Clebsch-Gordan coefficient is zero, don't bother with the rest. */
-        if (std::abs(isospin_factor) < really_small) {
-          continue;
-        }
-
-        /* Integration limits. */
-        const double lower_limit = type_res_1->minimum_mass();
-        const double upper_limit = sqrts - type_res_2->minimum_mass();
-        /* Check the available energy (requiring it to be a little above the
-         * threshold, because the integration will not work if it's too close). */
-        if (upper_limit - lower_limit < 1E-3) {
-          continue;
-        }
-
-        /* Calculate matrix element. */
-        const float matrix_element = nn_to_resonance_matrix_element(sqrts,
-                                                *type_res_1, *type_res_2, twoI);
-        if (matrix_element <= 0.) {
-          continue;
-        }
-
-        /* Calculate resonance production cross section
-         * using the Breit-Wigner distribution as probability amplitude.
-         * Integrate over the allowed resonance mass range. */
-
-        const double resonance_integral =
-                      type_res_1->iso_multiplet()->get_integral_DR(sqrts);
-
-        /** Cross section for 2->2 process with two resonances in final state.
-         * Based on Eq. (51) in \iref{Weil:2013mya}. */
-        const float spin_factor = (type_res_1->spin() + 1)
-                                * (type_res_2->spin() + 1);
-        const float xsection = isospin_factor * spin_factor * matrix_element
-                             * resonance_integral / (s * cm_momentum());
-
-        if (xsection > really_small) {
-          //process_list.push_back(make_unique<CollisionBranch>
-          //                      (*type_res_1, *type_res_2, xsection,
-          //                        ProcessType::TwoToTwo));
-          log.debug("Found 2->2 creation process with two resonances: ",
-                    *type_res_1, " ", *type_res_2);
-          log.debug("2->2 with original particles: ",
-                    type_particle_a, type_particle_b);
-        }
-      }
-    }
-  }
+  channel_list = find_xsection_from_type(type_particle_a, type_particle_b,
+      ParticleType::list_baryon_resonances(), ParticleType::list_Deltas(),
+      [&sqrts](const ParticleType &type_res_1){
+          return type_res_1.iso_multiplet()->get_integral_DR(sqrts);
+      });
+  process_list.reserve(process_list.size() + channel_list.size());
+  std::move(channel_list.begin(), channel_list.end(),
+      std::inserter(process_list, process_list.end()));
+  channel_list.clear();
 
   return process_list;
 }
