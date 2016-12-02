4cfc9c5febe26fc3bee2d0f67ceb6782d22d16a7
Vinzent Steinberg <Vinzent.Steinberg@gmail.com>
2016-12-02 15:13:40 +0100

    experiment: Fix a check for small deviations
    The check was comparing a difference to a small positive value. This
    check always fails if the difference is negative, even if it is very
    small. This has been fixed by comparing to the absolute difference
    instead.
    
    Hopefully fixes and refs #4230.

--- src/experiment.cc
+++ src/experiment.cc
@@ -1038,7 +1038,7 @@ void Experiment<Modus>::propagate_all_until(float t_end) {
     propagate_all();
     // Reset clock to get rid of small numerical errors from multiple additions
     parameters_.labclock.reset(t_end);
-  } else if (parameters_.labclock.current_time() - t_end > really_small) {
+  } else if (std::abs(parameters_.labclock.current_time() - t_end) > really_small) {
      // current time and t_end may be approximately equal, so the code can get here
      // because of numerics, but if current_time > t_end significantly, then it's a bug
      const auto &log = logger<LogArea::Experiment>();
