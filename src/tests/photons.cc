846bbaf0e82a34bbde13b49ad1e4864874d7a131
Dmytro Oliinychenko <oliiny@fias.uni-frankfurt.de>
2017-01-10 13:36:58 +0100

    Add a test for is_photon_reaction function
    Signed-off-by: Dmytro Oliinychenko <oliiny@fias.uni-frankfurt.de>

--- src/tests/photons.cc
+++ src/tests/photons.cc
@@ -43,3 +43,21 @@ TEST(pi_rho0_pi_gamma) {
   }
   COMPARE_RELATIVE_ERROR(tot_weight, 1.0f, 0.08f);
 }
+
+TEST(is_photon_reaction_function) {
+  const ParticleData pip  {ParticleType::find(0x211)};
+  const ParticleData pim  {ParticleType::find(-0x211)};
+  const ParticleData piz  {ParticleType::find(0x111)};
+  const ParticleData rhop {ParticleType::find(0x213)};
+  const ParticleData rhom {ParticleType::find(-0x213)};
+  const ParticleData rhoz {ParticleType::find(0x113)};
+  const ParticleData eta  {ParticleType::find(0x221)};
+  const ParticleData p    {ParticleType::find(0x2112)};
+
+  const ParticleList l1{pip,pim}, l2{rhop,pim}, l3{p,pim}, l4{pip,eta};
+
+  VERIFY(ScatterActionPhoton::is_photon_reaction(l1));
+  VERIFY(ScatterActionPhoton::is_photon_reaction(l2));
+  VERIFY(!ScatterActionPhoton::is_photon_reaction(l3));
+  VERIFY(ScatterActionPhoton::is_photon_reaction(l4));
+}
