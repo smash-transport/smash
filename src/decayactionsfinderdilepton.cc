71a2edced24441a0682644afcb7af273c3913cf1
Vinzent Steinberg <Vinzent.Steinberg@gmail.com>
2017-06-22 10:46:27 +0200

    Allow floating point comparisons
    Fixes and refs #2310.

--- src/decayactionsfinderdilepton.cc
+++ src/decayactionsfinderdilepton.cc
@@ -36,14 +36,11 @@ void DecayActionsFinderDilepton::shine(
     // if particle can only decay into dileptons or is stable, use shining only
     // in find_final_actions and ignore them here, also unformed
     // resonances cannot decay
-    #pragma GCC diagnostic push
-    #pragma GCC diagnostic ignored "-Wfloat-equal"
     if (dil_modes.size() == n_all_modes || p.type().is_stable()
         || (p.formation_time() > p.position().x0() &&
             p.cross_section_scaling_factor() == 0.0)) {
       continue;
     }
-    #pragma GCC diagnostic pop
 
     for (DecayBranchPtr & mode : dil_modes) {
       // SHINING as described in \iref{Schmidt:2008hm}, chapter 2D
