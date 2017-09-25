b7ac1403227c8d06f31442ed1d95ec1ee303e111
Dmytro Oliinychenko <oliiny@fias.uni-frankfurt.de>
2017-09-25 13:49:42 +0200

    List modus documentation and OSCAR2013 consistency
    Add charge to list modus to be consistent with OSCAR 2013 output.
    
    This refs #5410
    
    Signed-off-by: Dmytro Oliinychenko <oliiny@fias.uni-frankfurt.de>

--- src/listmodus.cc
+++ src/listmodus.cc
@@ -54,16 +54,19 @@ namespace Smash {
  * Starting id for event_id_
  *
  * Example input in particle_lists_in/event{id}
- * \verbatim
-CAR2013 particle_lists t x y z mass p0 px py pz pdg ID
-its: fm fm fm fm GeV GeV GeV GeV GeV none none
-6.42036 1.66473 9.38499 0.138 0.232871 0.116953 -0.115553 0.0903033 111 0
-  \endverbatim
- * It means that one pi^{0} at spatial coordinates (t,x,y,z)=(0.1, 6.42036,
-1.66473, 9.38499) fm
- * and 4-momenta (p0, px. py, pz)=(0.232871, 0.116953, -0.115553, 0.0903033 )
-GeV,
- * with mass=0.138, pdg=111 and id=0 will be initialized.
+ * <div class="fragment">
+ * <div class="line"><span class="preprocessor">#!OSCAR2013 particle_lists
+ * t x y z mass p0 px py pz pdg ID charge</span></div>
+ * <div class="line"><span class="preprocessor">\# Units: fm fm fm fm
+ * GeV GeV GeV GeV GeV none none none</span></div>
+ * <div class="line"><span class="preprocessor">0.1 6.42036 1.66473 9.38499
+ * 0.138 0.232871 0.116953 -0.115553 0.090303 111 0 0</span></div>
+ * </div>
+ * It means that one pi^{0} at spatial coordinates (t,x,y,z) =
+ * (0.1, 6.42036, 1.66473, 9.38499) fm
+ * and 4-momenta (p0, px. py, pz) =
+ * (0.232871, 0.116953, -0.115553, 0.090303) GeV,
+ * with mass = 0.138 GeV, pdg = 111, id = 0 and charge 0 will be initialized.
  */
 
 ListModus::ListModus(Configuration modus_config, const ExperimentParameters &)
@@ -134,8 +137,10 @@ double ListModus::initial_conditions(Particles *particles,
                 << "1. Put the external particle lists in file \n"
                 << "File_Directory/File_Prefix{id} where {id} "
                 << "traversal [Shift_Id, Nevent-1]\n"
-                << "2. Particles info: t x y z mass p0 px py pz pdg ID \n"
-                << "in units of: fm fm fm fm GeV GeV GeV GeV GeV none none \n";
+                << "2. Particles info: t x y z mass p0 px py pz"
+                << " pdg ID charge\n"
+                << "in units of: fm fm fm fm GeV GeV GeV GeV GeV"
+                << "none none none\n";
     throw std::runtime_error("External particle list does not exist!");
   }
 
@@ -148,10 +153,10 @@ double ListModus::initial_conditions(Particles *particles,
   for (const Line &line : line_parser(particle_lists)) {
     std::istringstream lineinput(line.text);
     double t, x, y, z, mass, E, px, py, pz;
-    int id;
+    int id, charge;
     PdgCode pdgcode;
     lineinput >> t >> x >> y >> z >> mass >> E >> px >> py >> pz >> pdgcode >>
-        id;
+        id >> charge;
 
     if (lineinput.fail()) {
       throw LoadFailure(
@@ -161,15 +166,17 @@ double ListModus::initial_conditions(Particles *particles,
                              line));
     }
 
-    log.debug() << "Particle " << pdgcode << " (x,y,z)= "
-                << "( ";
-    log.debug() << x << "," << y << "," << z << ")\n";
+    log.debug("Particle ", pdgcode, " (x,y,z)= (", x, ", ", y, ", ", z, ")");
 
     try {
       ParticleData &particle = particles->create(pdgcode);
+      if (pdgcode.charge() != charge) {
+        log.error() << "Charge of pdg = " << pdgcode << " != " << change;
+        throw std::invalid_argument("Inconsistent input (charge).");
+      }
       particle.set_4momentum(FourVector(E, px, py, pz));
       if (anti_streaming_needed) {
-        /* for hydro output where formation time is different*/
+        /* for hydro output where formation time is different */
         double delta_t = t - start_time_;
         FourVector start_timespace =
             FourVector(t, x, y, z) - delta_t * FourVector(E, px, py, pz) / E;
@@ -177,7 +184,7 @@ double ListModus::initial_conditions(Particles *particles,
         particle.set_formation_time(t);
         particle.set_cross_section_scaling_factor(0.0);
       } else {
-        /* for smash output where formation time is the same*/
+        /* for smash output where formation time is the same */
         particle.set_4position(FourVector(t, x, y, z));
         particle.set_formation_time(t);
         particle.set_cross_section_scaling_factor(1.0);
