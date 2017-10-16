6d3a858cc502d85c3dda232903c90fea01597c84
Jonas Rothermel <rothermel@fias.uni-frankfurt.de>
2017-10-16 17:12:22 +0200

    Continued work on LUT for Photon cross sections

--- src/tabulation3d.cc
+++ src/tabulation3d.cc
@@ -6,3 +6,12 @@
  *    GNU General Public License (GPLv3 or later)
  *
  */
+
+ #include "include/tabulation3d.h"
+
+ template <typename T> 
+ T Tabulation3d<T>::get_value_step(T x, T y, T z)
+ {
+    return 0;
+
+ }
