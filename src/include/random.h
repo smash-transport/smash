0e24b822f042fadc94ed0103d63fa7ca58c33e7f
Anna Schäfer <anna@Annas-MacBook-Pro.local>
2017-12-08 12:17:12 +0100

    See previous commit message
    Forgot to add one file.

--- src/include/random.h
+++ src/include/random.h
@@ -126,7 +126,7 @@ T expo(T A, T x1, T x2) {
   const T a1 = A * x1, a2 = A * x2;
   const T a_min = std::log(std::numeric_limits<T>::min());
 #ifndef NDEBUG
-  assert(A > T(0.) && T(0.) >= x1 && x1 > x2 && a1 > a_min);
+  assert(A > T(0.) && x1 >= x2 && a1 > a_min);
 #endif
   const T r1 = std::exp(a1);
   const T r2 = a2 > a_min ? std::exp(a2) : T(0.);  // prevent underflow
