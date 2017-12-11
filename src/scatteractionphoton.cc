3394a67ce266500275652e8ce80dc698cc2e72bf
Anna Schäfer <anna@Annas-MacBook-Pro.local>
2017-12-11 14:50:55 +0100

    Correct weighting for photons, see #5624.

--- src/scatteractionphoton.cc
+++ src/scatteractionphoton.cc
@@ -56,21 +56,7 @@ void ScatterActionPhoton::generate_final_state() {
   const double pcm_in = cm_momentum();
   const double pcm_out = pCM(sqrts, m3, 0.0);
 
-  assert(t1 < t2);
-  const double stepsize = (t2 - t1) / 100.0;
-  for (double t = t1; t < t2; t += stepsize) {
-    double diff_xsection_max =
-        std::max(diff_cross_section(t, m3), diff_xsection_max);
-  }
-
   double t = Random::uniform(t1, t2);
-  double diff_xsection_max = 0;
-  int iteration_number = 0;
-  do {
-    t = Random::uniform(t1, t2);
-    iteration_number++;
-  } while (diff_cross_section(t, m3) < Random::uniform(0., diff_xsection_max) &&
-           iteration_number < 100);
 
   // TODO(schaefer): this should be moved to kinematics.h and tested
   double costheta =
