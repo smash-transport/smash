7f5a7a92a3262412bf677639ff7daab4e1b06fff
Jonas Rothermel <jonas.rothermel@hotmail.de>
2017-12-14 13:35:50 +0100

    changed bounds for tabulated cross sections. now most of possible values should be tabulated

--- src/include/photoncrosssections.h
+++ src/include/photoncrosssections.h
@@ -98,9 +98,9 @@ class PhotonCrossSection<ComputationMethod::Lookup> {
 
  public:
 
-  const double s0_diff = 0.1, s1_diff = 6.0, t0_diff = -5.0;
+  const double s0_diff = 0.01, s1_diff = 20.0, t0_diff = -18.0;
   const double t1_diff = 5.0, ds_diff = 0.01, dt_diff = 0.01;
-  const double s0_tot = 0.1, s1_tot = 5.0, ds_tot = 0.01;
+  const double s0_tot = 0.01, s1_tot = 20.0, ds_tot = 0.01;
   const double m_rho_0 = 0.775, m_rho_1 = 0.777, dm = 0.01;
 
   double xs_pi_pi_rho0(const double s, const double m_rho);
--- src/scatteractionphoton.cc
+++ src/scatteractionphoton.cc
@@ -304,7 +304,7 @@ double ScatterActionPhoton::mediator_mass() const {
 
 CollisionBranchList ScatterActionPhoton::photon_cross_sections(bool from_check_collision /*=false */) {
   CollisionBranchList process_list;
-  PhotonCrossSection<ComputationMethod::Analytic> xs_object;
+  PhotonCrossSection<ComputationMethod::Lookup> xs_object;
 
   reac = photon_reaction_type(Action::incoming_particles());
 
@@ -380,7 +380,7 @@ double ScatterActionPhoton::diff_cross_section(double t, double t2,
   double diff_xsection = 0.0;
   const double m_rho = mediator_mass();
 
-  PhotonCrossSection<ComputationMethod::Analytic> xs_object;
+  PhotonCrossSection<ComputationMethod::Lookup> xs_object;
 
   switch (reac) {
     case ReactionType::pi_p_pi_m_rho_z:
