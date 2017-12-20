50bbef935bdeefde8a5d50860f4e3acd6a3997bf
Anna Schäfer <anna@Annas-MacBook-Pro.local>
2017-12-20 15:58:45 +0100

    Fix experiment test
    This was done by including all parameters in the generation of the test
    object.

--- src/tests/experiment.cc
+++ src/tests/experiment.cc
@@ -15,13 +15,40 @@ using namespace smash;
 
 TEST(init_particle_types) { Test::create_actual_particletypes(); }
 
-TEST(create_box) { VERIFY(!!Test::experiment("General: {Modus: Box}")); }
+TEST(create_box) { VERIFY(!!Test::experiment(
+                        Configuration("General:\n"
+                         "  Modus: Box\n"
+                         "  End_Time: 100.0\n"
+                         "  Nevents: 1\n"
+                         "Modi: \n"
+                         "  Box:\n"
+                         "    Initial_Condition: \"peaked momenta\"\n"
+                         "    Length: 10.0\n"
+                         "    Temperature: 0.2\n"
+                         "    Start_Time: 0.0\n"
+                         "    Init_Multiplicities:\n"
+                         "      661: 724\n"))); }
+
+
+
 
 TEST(create_collider) {
   VERIFY(!!Test::experiment("General: {Modus: Collider}"));
 }
 
-TEST(create_sphere) { VERIFY(!!Test::experiment("General: {Modus: Sphere}")); }
+TEST(create_sphere) { VERIFY(!!Test::experiment(
+                        Configuration("General:\n"
+                         "  Modus: Sphere\n"
+                         "  End_Time: 100.0\n"
+                         "  Nevents: 1\n"
+                         "Modi: \n"
+                         "  Sphere:\n"
+                         "    Initial_Condition: \"peaked momenta\"\n"
+                         "    Radius: 5.0\n"
+                         "    Temperature: 0.2\n"
+                         "    Start_Time: 0.0\n"
+                         "    Init_Multiplicities:\n"
+                         "      661: 724\n"))); }
 
 TEST_CATCH(create_invalid, ExperimentBase::InvalidModusRequest) {
   Test::experiment("General: {Modus: Invalid}");
