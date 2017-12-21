02f6d8e89046f1770fcf973fa3475b6e689a74f9
Anna Schäfer <anna@Annas-MacBook-Pro.local>
2017-12-21 15:33:05 +0100

    Disable strings for tests
    The strings are for the moment disabled in the tests as they produce
    memory leaks that are probably related to Pythia (not SMASH) and result
    in some of the tests failing. See #5691. The plan for the future is to
    find a way around this problem and in principle enable strings for the
    test setups.

--- src/tests/experiment.cc
+++ src/tests/experiment.cc
@@ -20,6 +20,8 @@ TEST(create_box) { VERIFY(!!Test::experiment(
                          "  Modus: Box\n"
                          "  End_Time: 100.0\n"
                          "  Nevents: 1\n"
+                         "Collision_Term:\n"
+                         "  Strings: False\n"
                          "Modi: \n"
                          "  Box:\n"
                          "    Initial_Condition: \"peaked momenta\"\n"
@@ -27,13 +29,23 @@ TEST(create_box) { VERIFY(!!Test::experiment(
                          "    Temperature: 0.2\n"
                          "    Start_Time: 0.0\n"
                          "    Init_Multiplicities:\n"
-                         "      661: 724\n"))); }
-
-
-
+                         "      661: 724\n")));
+}
 
 TEST(create_collider) {
-  VERIFY(!!Test::experiment("General: {Modus: Collider}"));
+  VERIFY(!!Test::experiment(Configuration("General:\n"
+                         "  Modus: Collider\n"
+                         "  End_Time: 100.0\n"
+                         "  Nevents: 1\n"
+                         "Collision_Term:\n"
+                         "  Strings: False\n"
+                         "Modi: \n"
+                         "  Collider:\n"
+                         "    Projectile: \n"
+                         "      Particles: {2212: 79, 2112: 118}\n"
+                         "    Target: \n"
+                         "      Particles: {2212: 79, 2112: 118}\n"
+                         "    E_Kin: 1.23\n")));
 }
 
 TEST(create_sphere) { VERIFY(!!Test::experiment(
@@ -41,6 +53,8 @@ TEST(create_sphere) { VERIFY(!!Test::experiment(
                          "  Modus: Sphere\n"
                          "  End_Time: 100.0\n"
                          "  Nevents: 1\n"
+                         "Collision_Term:\n"
+                         "  Strings: False\n"
                          "Modi: \n"
                          "  Sphere:\n"
                          "    Initial_Condition: \"thermal momenta\"\n"
@@ -48,7 +62,8 @@ TEST(create_sphere) { VERIFY(!!Test::experiment(
                          "    Sphere_Temperature: 0.2\n"
                          "    Start_Time: 0.0\n"
                          "    Init_Multiplicities:\n"
-                         "      661: 724\n"))); }
+                         "      661: 724\n")));
+}
 
 TEST_CATCH(create_invalid, ExperimentBase::InvalidModusRequest) {
   Test::experiment("General: {Modus: Invalid}");
