e8ccbabac5448e3c5f7652c2712f0a2fe6f3e3c4
Hannah Petersen <petersen@fias.uni-frankfurt.de>
2018-04-05 11:16:33 +0200

    Fixed documentation of nucleus
    - this refs #6081
    
    Signed-off-by: Hannah Petersen <petersen@fias.uni-frankfurt.de>

--- src/include/nucleus.h
+++ src/include/nucleus.h
@@ -20,118 +20,123 @@
 
 namespace smash {
 
-/** Nucleus: a nucleus is a collection of Particles (ParticleData thingys) that
- * are initialized before the beginning of the simulation and all have
- * the same velocity (and spatial proximity).
- *
- * Options added by Nucleus go in the "Modi"→"Nucleus"->"a nucleus" section of
- * the
- * configuration, where "a nucleus" is either projectile or target.
- *
- * The following nucleus directives are understood:
- * -------------
+/**
+ * A nucleus is a collection of particles that are initialized,
+ * before the beginning of the simulation and all have the same velocity.
  */
 class Nucleus {
  public:
+  /// \todo unused or default constructor?
   Nucleus(const std::map<PdgCode, int> &particle_list, int nTest);
+  /**
+   * Constructor for Nucleus, that needs the configuration parameters from
+   * the inputfile and the number of testparticles
+   * \param[in] config contains the parameters from the inputfile on the
+   * numbers of particles with a certain PDG code
+   * \param[in] nTest number of testparticles
+   */
   Nucleus(Configuration &config, int nTest);
 
-  /** returns the mass of the nucleus
-   * needs to be double to allow for calculations at LHC energies
-   * */
+  /**
+   * \return Mass of the nucleus.
+   * It needs to be double to allow for calculations at LHC energies.
+   */
   double mass() const;
 
-  /** Returns a Woods-Saxon distributed position.
+  /**
    * The distribution of return values from this function is according to a
    * spherically symmetric Woods-Saxon distribution suitable for this nucleus.
-   * \f$\frac{dN}{dr} = \frac{r^2}{\exp\left(\frac{r-R}{d}\right) +
-   * 1}\f$ where \f$d\f$ is the diffusiveness_ parameter and \f$R\f$ is
+   * \f$\frac{dN}{dr} = \frac{r^2}{\exp\left(\frac{r-r_0}{d}\right) +
+   * 1}\f$ where \f$d\f$ is the diffusiveness_ parameter and \f$r_0\f$ is
    * nuclear_radius_.
-   **/
+   * \return  Woods-Saxon distributed position.
+   */
   virtual ThreeVector distribute_nucleon() const;
 
-  /** returns the Woods-Saxon distribution directly
-   *
-   * @param x the position at which to evaluate the function
-   * @return un-normalized woods-saxon probability for @param x
-   **/
+  /**
+   * Woods-Saxon distribution
+   * \param[in] x the position at which to evaluate the function
+   * \return un-normalized Woods-saxon probability
+   */
   double woods_saxon(double x);
 
-  /// sets the positions of the nuclei inside nucleus A.
+  /// Sets the positions of the nuclei inside a nucleus.
   void arrange_nucleons();
 
-  /** Sets the deformation parameters of the Woods-Saxon distribution
+  /**
+   * Sets the deformation parameters of the Woods-Saxon distribution
    * according to the current mass number.
-   *
    * Ref. for nuclear radii is \iref{DeJager:1987qc}.
-   * For diffusiveness and saturation density, see [insert reference].
+   * \todo For diffusiveness and saturation density, see [insert reference].
    */
   virtual void set_parameters_automatic();
 
-  /** Sets the parameters of the Woods-Saxon according to
+  /**
+   * Sets the parameters of the Woods-Saxon according to
    * manually added values in the configuration file.
-   *
-   * @param config The configuration for this nucleus (projectile or target).
-   **/
+   * \param config The configuration for this nucleus (projectile or target).
+   */
   virtual void set_parameters_from_config(Configuration &config);
 
   /**
-   * Generates momenta of the Fermi motion of the nucleus constituents.
+   * Generates momenta according to Fermi motion for the nucleons.
    * For neutrons and protons Fermi momenta are calculated as
    * \f$ p_{F} = (3 \pi^2 \rho)^{1/3}\f$, where \f$ rho \f$ is
    * neutron density for neutrons and proton density for protons.
-   * Actual momenta \f$p_x\f$, \f$p_y\f$, \f$p_z\f$ are uniformly distributed
-   * in the sphere with radius \f$p_F\f$.
-   **/
+   * The actual momenta \f$p_x\f$, \f$p_y\f$, \f$p_z\f$ are
+   * uniformly distributed in the sphere with radius \f$p_F\f$.
+   */
   virtual void generate_fermi_momenta();
 
   /**
-   * Boosts the nuclei so that the nucleons have the appropriate
-   * momentum and the nuclei are lorentz-contracted.
-   *
-   * @param beta_scalar magnitude (with sign) of the z
-   * component velocity used for boosting.
-   **/
+   * Boosts the nuclei into the computational frame, such that
+   * the nucleons have the appropriate momentum and the
+   * nuclei are lorentz-contracted. Note that the usual boost cannot be
+   * applied for nuclei, since the particles would end up with different
+   * times and the binding energy needs to be taken into account.
+   * \param[in] beta_scalar velocity in z-direction used for boost.
+   */
   void boost(double beta_scalar);
 
-  /** Adds a particle to the nucleus
-   *
-   * @param pdgcode PDG code of the particle. */
+  /**
+   * Adds a particle to the nucleus
+   * \param pdgcode PDG code of the particle.
+   * \todo unused?
+   */
   void add_particle(int pdgcode);
 
   /**
-   * Adds particles from a map PDG_ID => Number_of_particles_with_that_PDG_ID
+   * Adds particles from a map PDG code => Number_of_particles_with_that_PDG_code
    * to the nucleus.
-   *
-   * If the map is, e.g., [2212: 6, 2112: 7] initializes C-13 (6 protons
+   * E.g., the map [2212: 6, 2112: 7] initializes C-13 (6 protons
    * and 7 neutrons). The particles are only created, no position or
-   * momenta are yet assigned.
-   *
-   * @param particle_list The particles that are added.
-   * @param testparticles Number of test particles to use.
-   *
-   **/
+   * momenta are yet assigned. It is also possible to use any other PDG
+   * code, in addition to nucleons.
+   * \param[out] particle_list The particle slots that are created.
+   * \param[in] testparticles Number of test particles to use.
+   */
   void fill_from_list(const std::map<PdgCode, int> &particle_list,
                       int testparticles);
 
   /**
    * Shifts the nucleus to correct impact parameter and z displacement.
-   *
-   * @param z_offset is the shift in z-direction
-   *
-   * @param x_offset is the shift in x-direction (for impact parameter
-   * setting).
-   *
-   * @param simulation_time set the time of each particle to this value.
-   **/
+   * \param[in] z_offset is the shift in z-direction
+   * \param[in] x_offset is the shift in x-direction
+   * \param[in] simulation_time set the time and formation_time of each
+   * particle to this value.
+   */
   void shift(double z_offset, double x_offset, double simulation_time);
 
-  /** Rotates the nucleus. (Spherical symmetry of nondeformed nuclei
-   * means there is nothing to do.)
-   **/
+  /**
+   * Rotates the nucleus. (Due to spherical symmetry of nondeformed nuclei,
+   * there is nothing to do.)
+   */
   virtual void rotate() {}
 
-  /// copies the particles from this nucleus into the particle list.
+  /**
+   * Copies the particles from this nucleus into the particle list.
+   * \return Particle list with all constituents of a nucleus
+   */
   void copy_particles(Particles *particles);
 
   /// Number of numerical (=test-)particles in the nucleus:
@@ -140,8 +145,8 @@ class Nucleus {
   /// Number of physical particles in the nucleus:
   inline size_t number_of_particles() const {
     size_t nop = particles_.size() / testparticles_;
-    // if size() is not a multiple of testparticles_, this will throw an
-    // error.
+    /* if size() is not a multiple of testparticles_, this will throw an
+     * error. */
     if (nop * testparticles_ != particles_.size()) {
       throw TestparticleConfusion(
           "Number of test particles and test particles"
@@ -150,15 +155,15 @@ class Nucleus {
     return nop;
   }
 
-  /** returns the geometrical center of the nucleus.
-   *
-   * @return \f$\vec r_s = \frac{1}{N} \sum_{i=1}^N \vec r_i\f$ (for a
+  /**
+   * Calculate geometrical center of the nucleus
+   * \return \f$\vec r_s = \frac{1}{N} \sum_{i=1}^N \vec r_i\f$ (for a
    * nucleus with N particles that are at the positions \f$\vec r_i\f$).
    */
   FourVector center() const;
 
-  /** shifts the nucleus so that its center is at (0,0,0)
-   *
+  /**
+   * Shifts the nucleus so that its center is at (0,0,0)
    * \see center()
    */
   void align_center() {
@@ -168,83 +173,88 @@ class Nucleus {
     }
   }
 
-  /// Write the nucleon positions to a text file.
-  //   void print_nucleus(const char * file_name) const;
-
   /// \ingroup exception
   struct TestparticleConfusion : public std::length_error {
     using std::length_error::length_error;
   };
 
  private:
-  /** diffusiveness of Woods-Saxon-distribution in this nucleus in fm
-   * (for diffusiveness_ == 0, we obtain a hard sphere.
-   **/
+  /**
+   * Diffusiveness of Woods-Saxon distribution of this nucleus in fm
+   * (for diffusiveness_ == 0, we obtain a hard sphere).
+   */
   double diffusiveness_ = .545;
   /// Saturation density of this nucleus.
   double saturation_density_ = nuclear_density;
   /// Nuclear radius of this nucleus
   double nuclear_radius_;
-  /** single-proton-radius
-   *
+  /**
+   * Single proton radius in fm
    * \see default_nuclear_radius
-   * */
+   */
   double proton_radius_ = 1.2;
   /// Number of testparticles per physical particle
   size_t testparticles_ = 1;
-  /// particles associated with this nucleus.
+  /// Particles associated with this nucleus.
   std::vector<ParticleData> particles_;
 
  public:
-  /// for iterators over the particle list:
+  /// For iterators over the particle list:
   inline std::vector<ParticleData>::iterator begin() {
     return particles_.begin();
   }
-  /// for iterators over the particle list:
-  inline std::vector<ParticleData>::iterator end() { return particles_.end(); }
-  /// for iterators over the particle list:
+  /// For iterators over the particle list:
+  inline std::vector<ParticleData>::iterator end() {
+    return particles_.end();
+  }
+  /// For iterators over the particle list:
   inline std::vector<ParticleData>::const_iterator cbegin() const {
     return particles_.cbegin();
   }
-  /// for iterators over the particle list:
+  /// For iterators over the particle list:
   inline std::vector<ParticleData>::const_iterator cend() const {
     return particles_.cend();
   }
-  /// sets the diffusiveness of the nucleus
-  ///
-  /// \see diffusiveness_
+  /**
+   * Sets the diffusiveness of the nucleus
+   * \see diffusiveness_
+   */
   inline void set_diffusiveness(double diffuse) { diffusiveness_ = diffuse; }
-  /// gets the diffusiveness of the nucleus
-  ///
-  /// \see diffusiveness_
+  /**
+   * Gets the diffusiveness of the nucleus
+   * \see diffusiveness_
+   */
   inline double get_diffusiveness() const { return diffusiveness_; }
-  /// sets the saturation density of the nucleus
-  ///
-  /// \see saturation_density_
+  /**
+   * Sets the saturation density of the nucleus
+   * \see saturation_density_
+   */
   inline void set_saturation_density(double density) {
     saturation_density_ = density;
   }
-  /// gets the saturation density of the nucleus
-  ///
-  /// \see saturation_density_
+  /**
+   * Gets the saturation density of the nucleus
+   * \see saturation_density_
+   */
   inline double get_saturation_density() const { return saturation_density_; }
-  /** returns a default radius for the nucleus
-   *
+  /**
+   * Returns a default radius for the nucleus
    * Nuclear radius is calculated with the proton radius times the third
    * root of the number of nucleons.
-   **/
+   */
   inline double default_nuclear_radius() {
     return proton_radius_ * std::pow(number_of_particles(), 1. / 3.);
   }
-  /// sets the nuclear radius
-  ///
-  /// \see nuclear_radius
+  /**
+   * Sets the nuclear radius
+   * \see nuclear_radius
+   */
   inline void set_nuclear_radius(double rad) { nuclear_radius_ = rad; }
-  /// gets the nuclear radius
-  ///
-  /// \see nuclear_radius
+  /**
+   * Gets the nuclear radius
+   * \see nuclear_radius
+   */
   inline double get_nuclear_radius() const { return nuclear_radius_; }
-
   /**\ingroup logging
    * Writes the state of the Nucleus object to the output stream.
    */
