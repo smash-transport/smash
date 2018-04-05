3877ae459c4fb4c3b8c017aceb9ed5511520f775
Hannah Petersen <petersen@fias.uni-frankfurt.de>
2018-04-05 11:58:32 +0200

    Fixed documentation of deformed nucleus
    - this refs #6081
    
    Signed-off-by: Hannah Petersen <petersen@fias.uni-frankfurt.de>

--- src/include/deformednucleus.h
+++ src/include/deformednucleus.h
@@ -17,49 +17,45 @@
 
 namespace smash {
 
-/** DeformedNucleus: Child of nucleus for deformed nuclei.
+/**
+ * DeformedNucleus: Child of nucleus for deformed nuclei.
  *
- * To use this modus, choose (ex: for deformed projectile)
- * \code
- * Modi:
- *      Nucleus:
- *            Projectile:
- *                 Deformed: true
- * \endcode
- * in the configuration file.
- *
- * Options added by NucleusModus go in the "Modi"→"Nucleus"->"a nucleus" section
- * of the
- * configuration, where "a nucleus" is either projectile or target.
- * All options from the nucleus will still apply. The deformed nucleus adds new
- * or updated
- * features which are outlined below.
- *
- * The following deformed nucleus directives are understood:
- * -------------
+ * All options from the nucleus will still apply. The deformed nucleus adds
+ * new or updated features which are outlined below.
  */
 
 class DeformedNucleus : public Nucleus {
  public:
+  /// \todo unused or default constructor?
   DeformedNucleus(const std::map<PdgCode, int> &particle_list, int nTest);
+  /**
+   * Constructor for DeformedNucleus, that needs the configuration parameters
+   * from the inputfile and the number of testparticles
+   * \param[in] config contains the parameters from the inputfile on the
+   * numbers of particles with a certain PDG code
+   * \param[in] nTest number of testparticles
+   */
   DeformedNucleus(Configuration &config, int nTest);
 
-  /** Return the deformed Woods-Saxon probability for the given position.
+  /**
+   * Return the deformed Woods-Saxon probability for the given position.
    *
-   * @param r The sample radius
-   * @param cosx The cosine of the sample polar angle
-   * @return The woods-saxon probability
-   **/
+   * \param[in] r The radius at which to sample
+   * \param[in] cosx The cosine of the polar angle at which to sample
+   * \return The Woods-Saxon probability
+   */
   double deformed_woods_saxon(double r, double cosx) const;
 
-  /**  Deformed Woods-Saxon sampling routine.
+  /**
+   * Deformed Woods-Saxon sampling routine.
    *
-   * @return a spatial position from uniformly sampling
+   * \return Spatial position from uniformly sampling
    * the deformed woods-saxon distribution
-   **/
+   */
   ThreeVector distribute_nucleon() const override;
 
-  /** Sets the deformation parameters of the Woods-Saxon distribution
+  /**
+   * Sets the deformation parameters of the Woods-Saxon distribution
    * according to the current mass number.
    *
    * The deformation parameters are taken from \iref{Moller:1993ed}.
@@ -70,43 +66,65 @@ class DeformedNucleus : public Nucleus {
    */
   void set_parameters_automatic() override;
 
-  /** Set parameters for Woods-Saxon by hand using the configuration file.
+  /**
+   * Set parameters for Woods-Saxon by hand using the configuration file.
    * \see Nucleus::set_parameters_from_config
    */
   void set_parameters_from_config(Configuration &config) override;
 
-  /** Rotates the nucleus according to members nucleus_polar_angle_
+  /**
+   * Rotates the nucleus according to members nucleus_polar_angle_
    * and nucleus_azimuthal_angle_ and updates nucleon positions.
    */
   void rotate() override;
 
   /**
    * Does not allow to generate Fermi-momenta for a deformed nucleus.
-   **/
+   */
   void generate_fermi_momenta() override;
 
-  /// Spherical harmonics Y_2_0 and Y_4_0.
+  /**
+   * Spherical harmonics Y_2_0 and Y_4_0.
+   * \param[in] l Angular momentum value (2 and 4 are supported)
+   * \param[in] cosx Cosine of the polar angle
+   * \return Value of the corresponding spherical harmonic
+   */
   double y_l_0(int l, double cosx) const;
 
-  /// Set deformation coefficient for Y_2_0.
+  /**
+   * Set deformation coefficient for Y_2_0.
+   * \param[in] b2 deformation coefficient for l=2
+   */
   inline void set_beta_2(double b2) { beta2_ = b2; }
-  /// Set deformation coefficient for Y_4_0.
+  /**
+   * Set deformation coefficient for Y_4_0.
+   * \param[in] b4 deformation coefficient for l=4
+   */
   inline void set_beta_4(double b4) { beta4_ = b4; }
-  /// Set the nucleus polar angle.
+  /**
+   * Set the nucleus polar angle.
+   * \param[in] theta Polar angle of position inside nucleus
+   */
   inline void set_polar_angle(double theta) {
     nuclear_orientation_.set_theta(theta);
   }
-  /// Set the nucleus azimuthal angle.
+  /**
+   * Set the nucleus azimuthal angle.
+   * \param[in] phi Azimuthal angle of position inside nucleus
+   */
   inline void set_azimuthal_angle(double phi) {
     nuclear_orientation_.set_phi(phi);
   }
 
  private:
-  /// Deformation parameter 2.
+  /// Deformation parameter for angular momentum l=2.
   double beta2_ = 0.0;
-  /// Deformation parameter 4.
+  /// Deformation parameter for angular momentum l=4.
   double beta4_ = 0.0;
-  /// Nucleus orientation (initial profile in xz plane).
+  /**
+   * Nucleus orientation (initial profile in xz plane) in terms of
+   * a pair of angles (theta, phi)
+   */
   Angles nuclear_orientation_;
 };
 
