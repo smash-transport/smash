e1e1a20ac0c820fc1d1c70f6e73f8d211d9e30a5
Vinzent Steinberg <Vinzent.Steinberg@gmail.com>
2018-04-06 12:12:56 +0200

    Improve PDG code constant docs

--- src/include/pdgcode_constants.h
+++ src/include/pdgcode_constants.h
@@ -18,51 +18,85 @@ namespace smash {
  */
 namespace pdg {
 
+/// Invalid particle.
 constexpr int invalid = 0x0;
 
+/// Photon.
 constexpr int photon = 0x22;
 
+/// Proton.
 constexpr int p = 0x2212;
+/// Neutron.
 constexpr int n = 0x2112;
 
+/// N(1535)⁺.
 constexpr int N1535_p = 0x22212;
+/// N(1535)⁰.
 constexpr int N1535_z = 0x22112;
 
+/// Δ⁺⁺.
 constexpr int Delta_pp = 0x2224;
+/// Δ⁺.
 constexpr int Delta_p = 0x2214;
+/// Δ⁰.
 constexpr int Delta_z = 0x2114;
+/// Δ⁻.
 constexpr int Delta_m = 0x1114;
 
+/// Λ.
 constexpr int Lambda = 0x3122;
+/// Σ⁺.
 constexpr int Sigma_p = 0x3222;
+/// Σ⁰.
 constexpr int Sigma_z = 0x3212;
+/// Σ⁻.
 constexpr int Sigma_m = 0x3112;
+/// Ξ⁰.
 constexpr int Xi_z = 0x3322;
+/// Ξ⁻.
 constexpr int Xi_m = 0x3312;
+/// Ω⁻.
 constexpr int Omega_m = 0x3334;
 
+/// π⁺.
 constexpr int pi_p = 0x211;
+/// π⁰.
 constexpr int pi_z = 0x111;
+/// π⁻.
 constexpr int pi_m = -0x211;
 
+/// K⁺.
 constexpr int K_p = 0x321;
+/// K⁰.
 constexpr int K_z = 0x311;
+/// K̄⁰.
 constexpr int Kbar_z = -0x311;
+/// K̄⁻.
 constexpr int K_m = -0x321;
 
+/// η.
 constexpr int eta = 0x221;
+/// ω.
 constexpr int omega = 0x223;
 
+/// ρ⁺.
 constexpr int rho_p = 0x213;
+/// ρ⁰.
 constexpr int rho_z = 0x113;
+/// ρ⁻.
 constexpr int rho_m = -0x213;
 
+/// h₁(1170).
 constexpr int h1 = 0x10223;
 
 }  // namespace pdg
 
 /// Pack two int32_t into an uint64_t.
 /// This is useful for switch statements on pairs.
+///
+/// \param x First integer to be packed.
+/// \param y Second integer to be packed.
+/// \return Combined integer.
 constexpr uint64_t pack(int32_t x, int32_t y) {
   return (static_cast<uint64_t>(static_cast<uint32_t>(x)) << 32) |
          static_cast<uint64_t>(static_cast<uint32_t>(y));
