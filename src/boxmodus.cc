dd05a5930943b48cce2efdeff1381b0593efa8a0
Anna Schäfer <aschaefer@fias.uni-frankfurt.de>
2018-08-09 11:18:52 +0200

    Modify Default Box and Sphere configs according to JB's suggestions.
    Update the user guide with a note about the necessary changes to
    particles.txt and decaymodes.txt when running a box.

--- src/boxmodus.cc
+++ src/boxmodus.cc
@@ -94,6 +94,16 @@ std::ostream &operator<<(std::ostream &out, const BoxModus &m) {
  * Strangeness chemical potential \f$ \mu_S \f$ used in case if
  * Use_Thermal_Multiplicities is true to compute thermal densities \f$ n_i \f$.
  *
+ * \note
+ * The box modus is most useful for infinite matter simulations which
+ * rely on thermal and chemical equilibrium, implying detailed balance. This can
+ * however not be conserved if there are 3-body decays (or higher) performed.
+ * To yield useful results applying a SMASH box simulation, it is
+ * therefore necessary to modify the provided default particles.txt and
+ * decaymodes.txt files by removing all 3-body and higher order decays from
+ * the decaymodes file and all corresponding particles that can no longer be
+ * produced from the particles file.
+ *
  * \n
  * Examples: Configuring a Box Simulation
  * --------------
