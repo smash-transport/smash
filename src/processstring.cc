5dbaf0999f88225e73a43247331607fa11c1a13b
Hannah Elfner <elfner@fias.uni-frankfurt.de>
2018-09-21 14:33:36 +0200

    Pythia info not printed anymore
    - the information on individual processes handled by Pythia
    is now diverted to debug output
    - #7073 contains an idea on how to handle this in the future
    - refs #6790
    
    Signed-off-by: Hannah Elfner <elfner@fias.uni-frankfurt.de>

--- src/processstring.cc
+++ src/processstring.cc
@@ -596,7 +596,7 @@ bool StringProcess::next_NDiffHard() {
     pythia_parton_->settings.parm("Beams:eCM", sqrtsAB_);
 
     pythia_parton_initialized_ = pythia_parton_->init();
-    log.info("Pythia initialized with ", PDGcodes_[0], "+", PDGcodes_[1],
+    log.debug("Pythia initialized with ", PDGcodes_[0], "+", PDGcodes_[1],
              " at CM energy [GeV] ", sqrtsAB_);
     if (!pythia_parton_initialized_) {
       throw std::runtime_error("Pythia failed to initialize.");
