ddff4fb147aa9e8395466f897404ec4cacf19bbe
Jan Staudenmaier <staudenmaier@fias.uni-frankfurt.de>
2018-09-22 20:50:17 +0200

    Updated clang-format-helper for CI
    After testing the script with Travis CI, I did some updates and also 
    polished it a bit more.
    Plus, CONTRIBUTING now mentions it.

--- CONTRIBUTING.md
+++ CONTRIBUTING.md
@@ -381,10 +381,11 @@ following way:
 ### Code formatting with `clang-format`
 
 All code has to be formatted by running `clang-format`. This automatically
-formats the code in SMASH correctly. Use the following command in the SMASH root
-directory:
+formats the code in SMASH correctly. Use the helper script in SMASH's /bin
+directory to format the source code:
 
-    for i in src/*.cc src/include/smash/*.h src/tests/*.cc src/tests/*.h; do clang-format -i $i; done;
+    cd bin
+    ./clang-format-helper -p
 
 If clang-format does changes that don't look good, you can disable it locally
 using comments like this:
--- bin/clang-format-helper.sh
+++ bin/clang-format-helper.sh
@@ -1,7 +1,7 @@
-#!/bin/sh
+#!/bin/bash
 
 fail() {
-  echo "$*" >&2
+  echo "ERROR: $*" >&2
   exit 1
 }
 
@@ -9,33 +9,32 @@ type clang-format >/dev/null 2>&1 || fail "Clang-format not found"
 
 VER_NUM=`echo $(clang-format -version) | sed -ne "s/[^0-9]*\(\([0-9]\.\)\{0,4\}[0-9][^.]\).*/\1/p"`
 if [ "$VER_NUM" != "6.0.0 " ]; then
-    fail "Wrong clang-format version: $VER_NUM (6.0.0 is required)" 
+    fail "Wrong clang-format version: $VER_NUM (6.0.0 is required)"
 fi;
 
-run_cf() {
-    for i in ../src/*.cc ../src/include/smash/*.h ../src/tests/*.cc ../src/tests/*.h;
-    do 
-        clang-format -verbose -i $i; 
-    done;
-}
+FILES_TO_FORMAT="../src/*.cc ../src/include/smash/*.h ../src/tests/*.cc ../src/tests/*.h"
 
-if [ "$1" == "" ] || [ "$1" == "-h" ]; then
-cat <<EOF
-Little helper script that wraps clang-format
-usage: ./clang-format-helper 
-  -p perform automatic formatting (for developers)
-  -t test that automatic formatting has been performed (for CI)
-  -h display this help 
-EOF
-elif [ "$1" == "-p" ]; then
-    echo "Running clang-format ... [perform only from wihtin the /bin directory]"
-    run_cf
-elif [ "$1" == "-t" ]; then
-    echo "Testing that clang-format does no changes to source code ..."
-    run_cf
-    if git diff --exit-code --name-status; then
-        echo "No changes to source code by clang-format. PASS."
+if [ "$1" = "-p" ]; then
+    echo "Running clang-format ... [perform from within the /bin directory]"
+    for i in $FILES_TO_FORMAT;
+    do
+        clang-format --verbose -i $i;
+    done;
+
+elif [ "$1" = "-t" ]; then
+    echo "Testing that clang-format does not change the source code in the working directory ..."
+    if diff <(cat $FILES_TO_FORMAT) <(clang-format $FILES_TO_FORMAT) > /dev/null; then
+        echo "PASS: No changes to source code by clang-format."
     else
         fail "Clang-format was not properly run on latest commit."
     fi
+
+else
+cat <<EOF
+Little helper script that wraps clang-format
+Usage: ./clang-format-helper
+  -p perform automatic formatting (for developers)
+  -t test that automatic formatting has been performed (for CI builds)
+Note: Run from within the /bin directory (uses relative paths).
+EOF
 fi
