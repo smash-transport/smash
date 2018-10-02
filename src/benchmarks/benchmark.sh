6f408e5b40f7e82741236ca02b3ae45874f838d3
Jan Staudenmaier <staudenmaier@fias.uni-frankfurt.de>
2018-10-02 15:54:21 +0200

    Benchmark suite v2: perf
    The benchmark shell script now runs with perf instead of time.
    A markdown file now is outputed containing the measurements,
    which could be easily translated to html if necessary.
    
    perf is not availabe with mac. So the benchmarks are linux-only.
    
    refs #1828

--- src/benchmarks/benchmark.sh
+++ src/benchmarks/benchmark.sh
@@ -12,7 +12,6 @@ configs_dir="${smash_root_dir}/src/benchmarks/configs"
 n_cores=8
 ##############################################
 
-echo
 echo "Building SMASH ..."
 echo
 
@@ -23,41 +22,86 @@ echo
 mkdir -p build || fail "Failed to create $PWD/build directory."
 cd build || fail "Failed to change directory to $PWD/build."
 
-cmake -DCMAKE_BUILD_TYPE=Release $smash_root_dir || fail "Failed to configure the build system."
-make -j${n_cores} || fail "Failed to build SMASH."
+#cmake -DCMAKE_BUILD_perfYPE=Release $smash_root_dir || fail "Failed to configure the build system."
+#make -j${n_cores} || fail "Failed to build SMASH."
 
 echo
-echo "Running Benchmarks:"
+echo "Collecting System Information ..."
+
+
+#TODO: remove tail for master (output is cleaned up there)
+SMASH_VER_NUM=$(./smash -v | grep -E 'SMASH-' | tail -n1)
+SMASH_VER_OUT=$(./smash -v)
+HW_INFO=$(lshw -short 2> /dev/null)
+UNAME_INFO=$(uname -a)
+
+echo
+echo "Running Benchmarks ..."
 echo
 
 benchmark_run() {
   setup=$1
 
-  exec_time=$( { TIMEFORMAT="%R"; time sh -c "./smash -i ${configs_dir}/${setup}/config.yaml -d ${configs_dir}/decaymodes.txt -p ${configs_dir}/particles.txt &> /dev/null"; } 2>&1 )  # Captures time only. SMASH muted completely.
+  PERF_OUT=$(perf stat -B -r 3 ./smash -i ${configs_dir}/${setup}/config.yaml -d ${configs_dir}/${setup}/decaymodes.txt -p ${configs_dir}/${setup}/particles.txt 2>&1 >/dev/null)
 
-  echo "$exec_time"
+  echo "$PERF_OUT"
 }
 
 echo "Started benchmark for collider ..."
-coll_t=$(benchmark_run collider)
+coll_perf=$(benchmark_run collider)
+echo "$coll_perf" | grep -E "time elapsed"
 
 echo "Started benchmark for box ..."
-box_t=$(benchmark_run box)
+box_perf=$(benchmark_run box)
+echo "$box_perf" | grep -E "time elapsed"
 
 echo "Started benchmark for sphere ..."
-sphe_t=$(benchmark_run sphere)
+sphere_perf=$(benchmark_run sphere)
+echo "$sphere_perf" | grep -E "time elapsed"
 
-echo
-echo "Benchmark Result Times ..."
-echo
 
-echo "Collider =    ${coll_t} sec"
-echo "Box      =    ${box_t} sec"
-echo "Sphere   =    ${sphe_t} sec"
+OUTPUT_FILE_N=bm-results-${SMASH_VER_NUM}.md
 
-cat > bm_results.txt <<EOF
-Collider time:    ${coll_t}s
-Box time:         ${coll_b}s
-Sphere time:      ${sphe_t}s
+# \` are espaced backticks
+cat > ../${OUTPUT_FILE_N}<<EOF
+# Benchmark Results for $SMASH_VER_NUM
+
+### \`smash -v\`
+\`\`\`
+$SMASH_VER_OUT}
+\`\`\`
+
+## System information
+
+### \`uname -a\`
+\`\`\`
+$UNAME_INFO
+\`\`\`
+
+### \`lshw -short\`
+
+\`\`\`
+$HW_INFO
+\`\`\`
+
+## Results
+
+Performed with \`perf\` and the \`stat -B\` options.
+
+### Collider Run
+\`\`\`
+$coll_perf
+\`\`\`
+
+### Box Run
+\`\`\`
+$box_perf
+\`\`\`
+
+### Sphere Run
+\`\`\`
+$sphere_perf
+\`\`\`
 EOF
-echo "\nResults are also written to bm_results.txt in /build."
+echo
+echo "Benchmark results are written to $OUTPUT_FILE_N"
