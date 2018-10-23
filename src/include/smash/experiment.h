4b091c69fb9567a92fbe53256fe0064c65594c6e
Mohs <mohs@fias.uni-frankfurt.de>
2018-10-23 11:30:42 +0200

    Add end of comment
    The comment was not properly escaped.
    
    This refs #5688

--- src/include/smash/experiment.h
+++ src/include/smash/experiment.h
@@ -1449,7 +1449,7 @@ void Experiment<Modus>::run_time_evolution() {
           }
         });
 
-    /* \todo (optimizations) Adapt timestep size here
+    /* \todo (optimizations) Adapt timestep size here */
 
     /* (2) Propagation from action to action until the end of timestep */
     run_time_evolution_timestepless(actions);
