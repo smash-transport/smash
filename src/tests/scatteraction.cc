ec1cc454d454737745e3e18636869aecf0a21675
Jean-Bernard Rose <rose@fias.uni-frankfurt.de>
2019-01-30 17:09:59 +0100

    Implemented test that cross-sections are symmetric
    i.e. that colliding A with B is the same as B with A

--- src/tests/scatteraction.cc
+++ src/tests/scatteraction.cc
@@ -12,6 +12,7 @@
 #include "setup.h"
 
 #include "../include/smash/angles.h"
+#include "../include/smash/random.h"
 #include "../include/smash/scatteraction.h"
 #include "Pythia8/Pythia.h"
 
@@ -170,6 +171,69 @@ TEST(outgoing_valid) {
   VERIFY(outgoing_particles[0].id() > p2_copy.id());
   // verify that particle is placed in the middle between the incoming ones
   COMPARE(outgoing_particles[0].position(), middle);
+}
+
+TEST(cross_sections_symmetric) {
+  // create a list of all particles
+  const auto &all_types = ParticleType::list_all();
+  int ntypes = all_types.size();
+  for (int i=0; i<42; i++) {
+    // create a random pair of particles
+    ParticleData p1{ParticleType::find(
+                        all_types[random::uniform_int(0, ntypes)].pdgcode())};
+    ParticleData p2{ParticleType::find(
+                        all_types[random::uniform_int(0, ntypes)].pdgcode())};
+    // set position
+    constexpr double p_x = 3.0;
+    p1.set_4position(pos_a);
+    p2.set_4position(pos_b);
+    // set momenta
+    p1.set_4momentum(p1.pole_mass(), p_x, 0., 0.);
+    p2.set_4momentum(p2.pole_mass(), -p_x, 0., 0.);
+
+    // put in particles object
+    Particles particles;
+    particles.insert(p1);
+    particles.insert(p2);
+
+    // construct actions
+    ScatterActionPtr act12, act21;
+    act12 = make_unique<ScatterAction>(p1, p2, 0.2, false, 1.0);
+    act21 = make_unique<ScatterAction>(p2, p1, 0.2, false, 1.0);
+    std::unique_ptr<StringProcess> string_process_interface =
+        make_unique<StringProcess>(1.0, 1.0, 0.5, 0.001, 1.0, 2.5, 0.217, 0.081,
+                                   0.7, 0.7, 0.25, 0.68, 0.98, 0.25, 1.0, true,
+                                   1. / 3.);
+    act12->set_string_interface(string_process_interface.get());
+    act21->set_string_interface(string_process_interface.get());
+    VERIFY(act12 != nullptr);
+    VERIFY(act21 != nullptr);
+
+    // add processes
+    constexpr double elastic_parameter = -10.; // no added elastic x-sections
+    constexpr bool two_to_one = true;
+    const ReactionsBitSet included_2to2 = ReactionsBitSet().flip();
+    constexpr double low_snn_cut = 0.;
+    constexpr bool strings_switch = true;
+    constexpr bool use_AQM = true;
+    constexpr bool strings_with_probability = true;
+    const NNbarTreatment nnbar_treatment = NNbarTreatment::Strings;
+    act12->add_all_scatterings(elastic_parameter, two_to_one, included_2to2,
+                               low_snn_cut, strings_switch, use_AQM,
+                               strings_with_probability, nnbar_treatment);
+    act21->add_all_scatterings(elastic_parameter, two_to_one, included_2to2,
+                               low_snn_cut, strings_switch, use_AQM,
+                               strings_with_probability, nnbar_treatment);
+
+    VERIFY(act12->cross_section() >= 0.);
+    VERIFY(act21->cross_section() >= 0.);
+
+    // check symmetry of the cross-section, i.e. xsec_AB == xsec_BA
+    FUZZY_COMPARE(act12->cross_section(), act21->cross_section())
+        << "Colliding " << p1.pdgcode() << " with " << p2.pdgcode()
+        << " does not yield the same cross-section as " << p2.pdgcode()
+        << " with " << p1.pdgcode();
+  }
 }
 
 TEST(pythia_running) {
