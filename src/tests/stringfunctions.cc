1f0e48419beb69e538fcea7308b831b00900ed9c
LeonGeiger <52000288+LeonGeiger@users.noreply.github.com>
2019-07-23 13:19:41 +0200

    new tests for trim, remove_substr and split (#270)
    * Add tests for trim, remove_substr and split
    
    Added the above tests to conclude the coverage of the _stringfunctions.cc_ module.
    
    * Add vector, sstream, correct namespaces
    
    added the _vector_ and _sstream_ library which are used in the new tests.
    Also fixed the missing namespaces for _trim_ , _remove_substr_ and _split_ .
    
    * Apply clang-format
    
    Used the clang-format script in /bin/ to reformat the code and avoid further failing of the checks due to format reasons.
    
    * Fix correct number of spaces before comment
    
    * Add missing copyright information

--- src/tests/stringfunctions.cc
+++ src/tests/stringfunctions.cc
@@ -1,9 +1,21 @@
+/*
+ *
+ *    Copyright (c) 2014-2019
+ *      SMASH Team
+ *
+ *    GNU General Public License (GPLv3 or later)
+ *
+ */
+
 #include "unittest.h"  // This include has to be first
 
 #include "../include/smash/stringfunctions.h"
 
-using namespace smash::utf8;
+#include <sstream>
+#include <vector>
 
+namespace smash {
+namespace utf8 {
 TEST(sequence_length) {
   COMPARE(sequence_length("xπ"), 1);
   COMPARE(sequence_length("πx"), 2);
@@ -20,3 +32,28 @@ TEST(fill) {
   COMPARE(fill_right("xπ", 5, '#'), "xπ###");
   COMPARE(fill_both("xπ", 5, '#'), "#xπ##");
 }
+}  // namespace utf8
+
+TEST(trim_and_remove) {
+  COMPARE(trim("  xπ# \n \r \t "), "xπ#");
+  COMPARE(trim("  xπ# \n  xπ "), "xπ# \n  xπ");
+
+  std::string s = "xπ#\n xπ90009_pion_\n";
+  remove_substr(s, "xπ");
+  COMPARE(s, "#\n 90009_pion_\n");
+  remove_substr(s, "\n");
+  COMPARE(s, "# 90009_pion_");
+  remove_substr(s, " 90009_pion_");
+  COMPARE(s, "#");
+}
+
+TEST(split) {
+  std::string s = "xπ/.90928/_is_pion_/true";
+  std::vector<std::string> storage;
+  storage = split(s, '/');
+  COMPARE(storage[0], "xπ");
+  COMPARE(storage[1], ".90928");
+  COMPARE(storage[2], "_is_pion_");
+}
+
+}  // namespace smash
