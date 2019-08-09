764aebc577c10ea6f539c77392b675cf9af041ea
Dmytro Oliinychenko <doliinychenko@lbl.gov>
2019-08-09 14:41:29 -0700

    Satisfy clang-format
    Signed-off-by: Dmytro Oliinychenko <doliinychenko@lbl.gov>

--- src/tests/hadgas_eos2.cc
+++ src/tests/hadgas_eos2.cc
@@ -30,8 +30,8 @@ TEST(partial_density) {
   const double T = 0.15, mub = 0.0, mus = 0.0;
   constexpr bool account_for_spectral_function = true;
   const double dpip = HadronGasEos::partial_density(pip, T, mub, mus);
-  const double drhop = HadronGasEos::partial_density(rhop, T, mub, mus,
-                                                account_for_spectral_function);
+  const double drhop = HadronGasEos::partial_density(
+      rhop, T, mub, mus, account_for_spectral_function);
   COMPARE_ABSOLUTE_ERROR(dpip, 0.0372145, 1.e-6);
   COMPARE(rhop.mass(), 0.776);
   COMPARE(rhop.width_at_pole(), 0.149);
