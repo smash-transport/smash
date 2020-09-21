a337a74c0fdf838614017299fbd34dd2de27b135
Agnieszka Sorensen <awergieluk@gmail.com>
2020-09-21 00:29:23 -0400

    Rewrite Juttner distribution.
    Signed-off-by: Agnieszka Sorensen <awergieluk@gmail.com>

--- src/distributions.cc
+++ src/distributions.cc
@@ -68,29 +68,23 @@ double density_integrand_2M_IC(const double energy, const double momentum_sqr,
          std::exp(-energy / temperature) * momentum_sqr;
 }
 
-/*
- * ****************************************************************************
- *
- * This is the same function as written here before;
- * however, the distribution as defined here is easier to handle numerically
- * than the one originally implemented  in SMASH. This is because
- * exp(-x)/[ 1 + exp(-x) ] is numerically more stable than 1/[ exp(x) + 1 ] .
- *
- * ****************************************************************************
- */
+/**
+  * Juttner distribution is a unified way of introducing quantum thermal
+  * distributions, in which the "statistics" variable controls the type of the
+  * distribution used:
+  * statistics = 0: Boltzmann distribution
+  * statistics = -1: Bose distribution
+  * statistics = +1: Fermi distribution
+  */
 double juttner_distribution_func(double momentum_radial, double mass,
                                  double temperature,
                                  double effective_chemical_potential,
                                  double statistics) {
-  return (std::exp(
-             -(std::sqrt(momentum_radial * momentum_radial + mass * mass) -
-               effective_chemical_potential) /
-             temperature)) /
-         (1 +
-          statistics * std::exp(-(std::sqrt(momentum_radial * momentum_radial +
-                                            mass * mass) -
-                                  effective_chemical_potential) /
-                                temperature));
+  const double Boltzmann_factor =
+    std::exp(-(std::sqrt(momentum_radial * momentum_radial + mass * mass) -
+	       effective_chemical_potential) / temperature);
+  // exp(-x) / [1 + exp(-x)] is numerically more stable than 1 / [exp(x) + 1]
+  return Boltzmann_factor / (1 + statistics * Boltzmann_factor);
 }
 
 double sample_momenta_non_eq_mass(const double temperature, const double mass) {
