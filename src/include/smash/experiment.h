3d6071a85edc12d59aa185281f754b22e048d28e
Jan Staudenmaier <staudenmaier@fias.uni-frankfurt.de>
2020-11-10 11:43:25 +0100

    Fix divison by zero when no interaction occured
    The calculation of the precentage of interaction that was rejected
    produced a divison by zero error, if the total number of
    interactions is zero.
    
    The number of interaction is now made sure to be larger than 0,
    if not the precentage is set to 0.

--- src/include/smash/experiment.h
+++ src/include/smash/experiment.h
@@ -2161,9 +2161,10 @@ void Experiment<Modus>::final_output(const int evt_num) {
             << total_hypersurface_crossing_actions_;
       }
     } else {
-      const double precent_discarded =
-          static_cast<double>(discarded_interactions_total_) * 100. /
-          interactions_total_;
+      const double precent_discarded = interactions_total_ > 0 ?
+          static_cast<double>(discarded_interactions_total_) * 100.0 /
+          interactions_total_ :
+          0.0;
       std::stringstream msg_discarded;
       msg_discarded
           << "Discarded interaction number: " << discarded_interactions_total_
