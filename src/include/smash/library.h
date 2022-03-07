b06421e093dda8849d131277c0bf172662517877
Jan Staudenmaier <staudenmaier@fias.uni-frankfurt.de>
2022-03-07 17:16:55 +0100

    Seperate library function implementation into cc file

--- src/include/smash/library.h
+++ src/include/smash/library.h
@@ -1,4 +1,3 @@
-
 /*
  *
  *    Copyright (c) 2022-
@@ -8,17 +7,9 @@
  *
  */
 
-#include "smash/setup_particles_decaymodes.h"
-#include "smash/decaymodes.h"
-#include "smash/filelock.h"
-#include "smash/random.h"
-#include "smash/sha256.h"
-#include "smash/config.h"
-#include "smash/experiment.h"
-#include "smash/stringfunctions.h"
+#include <boost/filesystem.hpp>
 
-// TODO(stdnmr) check what includes are really necessary
-
+#include "configuration.h"
 
 #ifndef SRC_INCLUDE_SMASH_LIBRARY_H_
 #define SRC_INCLUDE_SMASH_LIBRARY_H_
@@ -27,7 +18,6 @@ namespace smash {
 
 // Free functions to interface with smash
 // TODO(stdnmr) Put example program here
-// TODO(stdnmr) Place implementation into .cc
 
 /// Set up configuration and logging from input files and extra config
 Configuration setup_config_and_logging(const bf::path &config_file,
@@ -42,79 +32,6 @@ Configuration setup_config_and_logging(const bf::path &config_file,
  */
 void initalize(Configuration &configuration, std::string version,
                bf::path tabulations_path);
-
-////////////////////////////////////////////////////////////////////////////////
-
-Configuration setup_config_and_logging(const bf::path &config_file,
-                           const bf::path &particles_file,
-                           const bf::path &decaymodes_file,
-                           const std::vector<std::string> &extra_config){
-  // Read in config file
-  Configuration configuration(config_file.parent_path(),
-                              config_file.filename());
-
-  // Merge config passed via command line
-  for (const auto &config : extra_config) {
-    configuration.merge_yaml(config);
-  }
-
-  // Set up logging
-  set_default_loglevel(
-      configuration.take({"Logging", "default"}, einhard::ALL));
-  create_all_loggers(configuration["Logging"]);
-
-  logg[LMain].trace(SMASH_SOURCE_LOCATION, " load ParticleType and DecayModes");
-
-  auto particles_and_decays =
-      load_particles_and_decaymodes(particles_file, decaymodes_file);
-  /* For particles and decaymodes: external file is superior to config.
-   * Hovever, warn in case of conflict.
-   */
-  if (configuration.has_value({"particles"}) && !particles_file.empty()) {
-    logg[LMain].warn(
-        "Ambiguity: particles from external file ", particles_file,
-        " requested, but there is also particle list in the config."
-        " Using particles from ",
-        particles_file);
-  }
-  if (!configuration.has_value({"particles"}) || !particles_file.empty()) {
-    configuration["particles"] = particles_and_decays.first;
-  }
-
-  if (configuration.has_value({"decaymodes"}) && !decaymodes_file.empty()) {
-    logg[LMain].warn(
-        "Ambiguity: decaymodes from external file ", decaymodes_file,
-        " requested, but there is also decaymodes list in the config."
-        " Using decaymodes from",
-        decaymodes_file);
-  }
-  if (!configuration.has_value({"decaymodes"}) || !decaymodes_file.empty()) {
-    configuration["decaymodes"] = particles_and_decays.second;
-  }
-
-  return configuration;
-}
-
-void initalize(Configuration &configuration, std::string version,
-               bf::path tabulations_path) {
-  logg[LMain].trace(SMASH_SOURCE_LOCATION,
-                    " create ParticleType and DecayModes");
-  ParticleType::create_type_list(configuration.take({"particles"}));
-  DecayModes::load_decaymodes(configuration.take({"decaymodes"}));
-  ParticleType::check_consistency();
-
-  logg[LMain].info("Tabulating cross section integrals...");
-  // Calculate a hash of the SMASH version, the particles and decaymodes.
-  const std::string particle_string = configuration["particles"].to_string();
-  const std::string decay_string = configuration["decaymodes"].to_string();
-  sha256::Context hash_context;
-  hash_context.update(version);
-  hash_context.update(particle_string);
-  hash_context.update(decay_string);
-  const auto hash = hash_context.finalize();
-  logg[LMain].info() << "Config hash: " << sha256::hash_to_string(hash);
-  IsoParticleType::tabulate_integrals(hash, tabulations_path);
-}
 
 
 } // namespace smash
---
+++ src/library.cc
@@ -0,0 +1,92 @@
+/*
+ *
+ *    Copyright (c) 2022-
+ *      SMASH Team
+ *
+ *    GNU General Public License (GPLv3 or later)
+ *
+ */
+
+#include "smash/library.h"
+#include "smash/setup_particles_decaymodes.h"
+#include "smash/decaymodes.h"
+#include "smash/inputfunctions.h"
+#include "smash/isoparticletype.h"
+#include "smash/logging.h"
+
+namespace smash {
+static constexpr int LMain = LogArea::Main::id;
+
+
+Configuration setup_config_and_logging(const bf::path &config_file,
+                           const bf::path &particles_file,
+                           const bf::path &decaymodes_file,
+                           const std::vector<std::string> &extra_config){
+  // Read in config file
+  Configuration configuration(config_file.parent_path(),
+                              config_file.filename());
+
+  // Merge config passed via command line
+  for (const auto &config : extra_config) {
+    configuration.merge_yaml(config);
+  }
+
+  // Set up logging
+  set_default_loglevel(
+      configuration.take({"Logging", "default"}, einhard::ALL));
+  create_all_loggers(configuration["Logging"]);
+
+  logg[LMain].trace(SMASH_SOURCE_LOCATION, " load ParticleType and DecayModes");
+
+  auto particles_and_decays =
+      load_particles_and_decaymodes(particles_file, decaymodes_file);
+  /* For particles and decaymodes: external file is superior to config.
+   * Hovever, warn in case of conflict.
+   */
+  if (configuration.has_value({"particles"}) && !particles_file.empty()) {
+    logg[LMain].warn(
+        "Ambiguity: particles from external file ", particles_file,
+        " requested, but there is also particle list in the config."
+        " Using particles from ",
+        particles_file);
+  }
+  if (!configuration.has_value({"particles"}) || !particles_file.empty()) {
+    configuration["particles"] = particles_and_decays.first;
+  }
+
+  if (configuration.has_value({"decaymodes"}) && !decaymodes_file.empty()) {
+    logg[LMain].warn(
+        "Ambiguity: decaymodes from external file ", decaymodes_file,
+        " requested, but there is also decaymodes list in the config."
+        " Using decaymodes from",
+        decaymodes_file);
+  }
+  if (!configuration.has_value({"decaymodes"}) || !decaymodes_file.empty()) {
+    configuration["decaymodes"] = particles_and_decays.second;
+  }
+
+  return configuration;
+}
+
+void initalize(Configuration &configuration, std::string version,
+               bf::path tabulations_path) {
+  logg[LMain].trace(SMASH_SOURCE_LOCATION,
+                    " create ParticleType and DecayModes");
+  ParticleType::create_type_list(configuration.take({"particles"}));
+  DecayModes::load_decaymodes(configuration.take({"decaymodes"}));
+  ParticleType::check_consistency();
+
+  logg[LMain].info("Tabulating cross section integrals...");
+  // Calculate a hash of the SMASH version, the particles and decaymodes.
+  const std::string particle_string = configuration["particles"].to_string();
+  const std::string decay_string = configuration["decaymodes"].to_string();
+  sha256::Context hash_context;
+  hash_context.update(version);
+  hash_context.update(particle_string);
+  hash_context.update(decay_string);
+  const auto hash = hash_context.finalize();
+  logg[LMain].info() << "Config hash: " << sha256::hash_to_string(hash);
+  IsoParticleType::tabulate_integrals(hash, tabulations_path);
+}
+
+}  // namespace smash
