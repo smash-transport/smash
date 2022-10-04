a3558f9cee89ea1c64f586a4bb68e656bb25613d
ren <hirayama@fias.uni-frankfurt.de>
2022-10-05 00:13:43 +0200

    Initial commit. Create basic FluidizationAction without including in Experiment run

---
+++ src/fluidizationaction.cc
@@ -0,0 +1,121 @@
+/*
+ *
+ *    Copyright (c) 2019-2020,2022
+ *      SMASH Team
+ *
+ *    GNU General Public License (GPLv3 or later)
+ *
+ */
+
+#include "smash/fluidizationaction.h"
+
+#include "smash/logging.h"
+#include "smash/quantumnumbers.h"
+
+namespace smash {
+static constexpr int LFluidization = LogArea::HyperSurfaceCrossing::id;
+
+void HypersurfacecrossingAction::generate_final_state() {
+  logg[LFluidization].debug("Process: Fluidization. ");
+
+  ParticleList empty_list;
+
+  // check that there is only 1 incoming particle
+  assert(incoming_particles_.size() == 1);
+
+  // Return empty list because we want to remove the incoming particle
+  outgoing_particles_ = empty_list;
+}
+
+void FluidizationAction::check_conservation(
+    const uint32_t id_process) const {
+  QuantumNumbers before(incoming_particles_);
+  QuantumNumbers after(outgoing_particles_);
+  if (before == after) {
+    // Conservation laws should not be conserved since particles are removed
+    // from the evolution
+    throw std::runtime_error(
+        "Conservation laws conserved during fluidization action. "
+        "Particle was not properly removed in process: " +
+        std::to_string(id_process));
+  }
+
+  if (outgoing_particles_.size() != 0) {
+    throw std::runtime_error(
+        "Particle was not removed successfully in fluidization action.");
+  }
+}
+
+ActionList FluidizationActionFinder::find_actions_with_neighbors(
+      const ParticleList &search_list, const ParticleList &neighbors_list, const double dt,
+      const std::vector<FourVector> &) const {
+  std::vector<ActionPtr> actions;
+
+  build_lattice(neighbors_list);
+
+  for (const ParticleData &p : search_list) {
+    double t0 = p.position().x0();
+    double t_end = t0 + dt;  // Time at the end of timestep
+
+    // Particles should not be removed before the nuclei collide, and after some 
+    // time max_time_ there won't be any fluidization
+    if (t_end < min_time_ || t0 > max_time_) {
+      break;
+    }
+
+    bool fluidize = above_threshold(p, neighbors_list);
+
+    /*
+       This is from HypersurfacecrossingAction, not sure if will be needed.
+
+       If rapidity or transverse momentum cut is to be employed; check if
+       particles are within the relevant region
+       Implementation explanation: The default for both cuts is 0.0, as a cut at
+       0 implies that not a single particle contributes to the initial
+       conditions. If the user specifies a value of 0.0 in the config, SMASH
+       crashes with a corresponding error message. The same applies to negtive
+       values.
+    */
+    bool is_within_y_cut = true;
+    // Check whether particle is in desired rapidity range
+    if (rap_cut_ > 0.0) {
+      const double rapidity =
+          0.5 * std::log((p.momentum().x0() + p.momentum().x3()) /
+                         (p.momentum().x0() - p.momentum().x3()));
+      if (std::fabs(rapidity) > rap_cut_) {
+        is_within_y_cut = false;
+      }
+    }
+    bool is_within_pT_cut = true;
+    // Check whether particle is in desired pT range
+    if (pT_cut_ > 0.0) {
+      const double transverse_momentum =
+          std::sqrt(p.momentum().x1() * p.momentum().x1() +
+                    p.momentum().x2() * p.momentum().x2());
+      if (transverse_momentum > pT_cut_) {
+        is_within_pT_cut = false;
+      }
+    }
+
+    if (fluidize && is_within_y_cut && is_within_pT_cut) {
+      // for now the action should be performed at the start of the time step
+      ActionPtr action = std::make_unique<FluidizationAction>(p, 0);
+      actions.emplace_back(std::move(action));
+    }
+  }  // search_list loop
+  return actions;
+}
+
+bool above_threshold(ParticleData &pdata){
+  EnergyMomentumTensor Tmunu;
+  bool fluidize = false;
+  // value_at returns false if pdata is out of bounds, this is desirable here
+  if(lat_.value_at(pdata.position().threevec(), Tmunu)){
+      if (Tmunu.boosted(Tmunu.landau_frame_4velocity())[0] >= energy_density_threshold_)
+	     fluidize = true; 
+  }
+  return fluidize;
+}
+
+
+}  // namespace smash
---
+++ src/include/smash/fluidizationaction.h
@@ -0,0 +1,119 @@
+/*
+ *
+ *    Copyright (c) 2019-2020,2022
+ *      SMASH Team
+ *
+ *    GNU General Public License (GPLv3 or later)
+ *
+ */
+
+#ifndef SRC_INCLUDE_SMASH_HYPERSURFACECROSSINGACTION_H_
+#define SRC_INCLUDE_SMASH_HYPERSURFACECROSSINGACTION_H_
+
+#include <vector>
+
+#include "action.h"
+#include "actionfinderfactory.h"
+
+namespace smash {
+
+/**
+ * \ingroup action
+ */
+class FluidizationAction : public Action {
+ public:
+  /**
+   * Construct action of fluidization.
+   * \param[in] in_part Data of incoming particle.
+   * \param[in] out_part Data of particles which surpass the energy density threshold
+   */
+  FluidizationAction(const ParticleData &in_part,
+                             const ParticleData &out_part)
+      : Action(in_part, out_part, time_until,
+               ProcessType::HyperSurfaceCrossing) {}
+  double get_total_weight() const override { return 0.0; };
+  double get_partial_weight() const override { return 0.0; };
+  void format_debug_output(std::ostream &out) const override {
+    out << "Fluidization of " << incoming_particles_;
+  }
+
+  /**
+   * Generate the final state of particles to be fluidized and removes from the evolution.
+   */
+  void generate_final_state() override;
+
+  void check_conservation(const uint32_t id_process) const override;
+};
+
+/**
+ * \ingroup action
+ */
+class FluidizationActionsFinder : public ActionFinderInterface {
+ public:
+  /**
+   * Construct finder for fluidization action.
+   */
+  explicit FluidizationFinder(const double min_energy=0.5, const double min_time=0, const double max_time=100) : energy_density_threshold_{min_energy}, min_time_{min_time}, max_time_{max_time} {
+    std::array<double, 3> l{20., 20., 20.};
+    std::array<int, 3> n{10, 10, 10};
+    std::array<double, 3> origin{-10., -10., -10.};
+    bool periodic = false;
+    lat_ = make_unique<RectangularLattice<EnergyMomentumTensor>>(l, n, origin, periodic, LatticeUpdate::EveryTimestep);
+  };
+
+  /// Ignore the cell search for fluidization
+  ActionList find_actions_in_cell(
+      const ParticleList &, double, const double,
+      const std::vector<FourVector> &) const override {
+	  return {};
+  }
+
+  /**
+   * Find particles to fluidize positions due to neighboring particles.
+   * \param[in] search_list List of candidate particles to fluidize
+   * \param[in] neighbors_list List of neighboring particles that contribute 
+   * \param[in] dt duration of the current time step [fm]
+   * \param[in] beam_momentum Unimportant?
+   */
+  ActionList find_actions_with_neighbors(
+      const ParticleList &search_list, const ParticleList &neighbors_list, const double dt,
+      const std::vector<FourVector> &beam_momentum) const override;
+
+  /// Ignore the surrounding searches for hypersurface crossing
+  ActionList find_actions_with_surrounding_particles(
+      const ParticleList &, const Particles &, double,
+      const std::vector<FourVector> &) const override {
+    return {};
+  }
+
+  /// No final actions after fluidizing 
+  ActionList find_final_actions(const Particles &, bool) const override {
+    return {};
+  }
+
+ private:
+  /// Parameters for smearing particles in the lattice
+  const DensityParameters dens_par_;
+  /// Lattice where the energy density will be computed
+  std::unique_ptr<RectangularLattice<EnergyMomentumTensor>> lat_;
+  /// Minimum energy density surrounding the particle to fluidize it
+  const double energy_density_threshold_;
+  /// Minimum time (in lab frame) in fm to allow fluidization
+  const double min_time_;
+  /// Maximum time (in lab frame) in fm to allow fluidization
+  const double max_time_;
+  /**
+   * Determine fluidization
+   * \param[in] pdata particle to be checked for fluidization
+   * \return whether energy density around pdata is high enough
+   */
+  bool above_threshold(ParticleData &pdata) const;
+  /// Build energy momentum tensor
+  inline void build_lattice(const ParticleList &neighbors_list){
+    update_lattice(lat_.get(), LatticeUpdate::EveryTimestep, DensityType::None, dens_par_, neighbors_list, false);
+  }
+};
+
+}  // namespace smash
+
+#endif  // SRC_INCLUDE_SMASH_HYPERSURFACECROSSINGACTION_H_
