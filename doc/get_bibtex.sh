1f5d3e59f930eae406823914ffeed113de2e778b
Alessandro Sciarra <asciarra@fias.uni-frankfurt.de>
2022-11-01 17:47:37 +0100

    Rewrite and fix doc/get_bibtex.sh script
    The script was written in an sh-bash mixed style and was cleaned. It
    now fetches entries using the INSPIRE rest API instead of an old domain
    which is not valid anymore.

--- doc/get_bibtex.sh
+++ doc/get_bibtex.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 #
 # This script produces a bibtex file 'inspire.bib' including all papers
 # referenced in the Smash source code via the doxygen command \iref{}.
@@ -6,25 +6,44 @@
 # It first extracts all bibtex keys from the source and then queries Inspire
 # for each of them, in order to get the full bibtex entry.
 
-# make the script callable from anywhere (by changing to script directory)
-cd `dirname \`which "$0"\``
+# Set up a trap on exit to clean up temporary files
+trap 'rm -f ${tmp_bib_file}; echo' EXIT
 
-# make sure no earlier bib file is present
-rm -f inspire.bib
+# Make the script callable from anywhere (by changing to script directory)
+cd "$(dirname "${BASH_SOURCE[0]}")"; echo ''
 
-# extract all bibtex keys from the source files
-for line in `grep -hor '\\iref{[A-Z,a-z,0-9,:]*}' ../src | sort -u | sed 's/\\iref{//g; s/}/ /g'`; do
-  echo "fetching $line ..."
-  # search for bibtex key on Inspire, extract bib entry and append it to bibtex file
-  wget -q -O - "http://old.inspirehep.net/search?p=$line&of=hx" | \
-      sed -n "/<pre>/,/<\/pre>/p" | sed '$ d; 1d' | sed 's/&gt;/>/g'> tmp.bib
-  nl=`cat tmp.bib | wc -l`
-  if [ $nl -eq 0 ]; then
-    echo "not found!"
+# A couple global variable
+readonly tmp_bib_file='tmp.bib'
+readonly bib_file='inspire.bib'
+
+# Extract keys cited in documentation
+readonly cited_keys=( $(grep -hor '\\iref{[A-Za-z0-9:-]*}' ../src | sort -u | sed 's/\\iref{\([^}]*\)}/\1/g') )
+readonly number_of_keys=${#cited_keys[@]}
+
+# Iterate over all keys and extract all bibtex entries
+fetched_keys=0
+failed_to_fetch=0
+unfetched_keys=()
+for key in "${cited_keys[@]}"; do
+  printf "\n\e[1A\e[K Fetching key %${#number_of_keys}d/%d [%d error(s) occurrred] -> key = %s"\
+         $((fetched_keys+failed_to_fetch+1)) ${number_of_keys} ${failed_to_fetch} "${key}"
+  # Search for bibtex key on Inspire, obtain it using the API
+  #  -> https://github.com/inspirehep/rest-api-doc#internal-identifiers
+  bibtex_entry=$(wget -q -O - "https://inspirehep.net/api/literature?q=${key}&format=bibtex")
+  if [[ "${bibtex_entry}" != '' ]]; then
+    echo "${bibtex_entry}" >> "${tmp_bib_file}"
+    ((fetched_keys++))
   else
-    cat tmp.bib >> inspire.bib
+    unfetched_keys+=( "${key}")
+    ((failed_to_fetch++))
   fi
 done
 
-# clean up
-rm tmp.bib
+# Print report and move temporary in place if successful
+printf '\n\e[1A\e[K Successfully fetched %d/%d keys\n' ${fetched_keys} ${#cited_keys[@]}
+if [[ ${failed_to_fetch} -eq 0 ]]; then
+  mv "${tmp_bib_file}" "${bib_file}"
+else
+  printf '\n Unable to fetch following keys:\n'
+  printf '  - %s\n' "${unfetched_keys[@]}"
+fi
