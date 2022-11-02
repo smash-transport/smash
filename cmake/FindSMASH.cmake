1e3a07741a63ee7e1c1a5c4d8e2dd4e12f71173b
Alessandro Sciarra <asciarra@fias.uni-frankfurt.de>
2022-11-02 15:58:44 +0100

    Add optional environment variable to improve FindSMASH module
    The SMASH_BUILD_DIR environment variable can now be used to tell CMake
    where to look for some files. If the environment variable is not set,
    it is assumed to be "${SMASH_DIR}/build" (as it was before). A check on
    SMASH_DIR to make the script fail in a more user-friendly way if it is
    not defined has been added.

--- cmake/FindSMASH.cmake
+++ cmake/FindSMASH.cmake
@@ -17,8 +17,14 @@
 # SMASH_INCLUDE_DIR    directory of smash includes
 # SMASH_FOUND          true is smash found
 #
-# The environment variable SMASH_DIR must be set properly to succeed, e. g.:
-# export SMASH_DIR=~/Work/SMASH/smash
+# The environment variable SMASH_DIR must be set properly
+# to succeed, e.g.:
+#   export SMASH_DIR=~/Work/SMASH/smash
+#
+# Optionally, a SMASH_BUILD_DIR environment variable can
+# be defined to specify the path to the folder where SMASH
+# has been built. If this is not set, it is assumed that
+# SMASH has been built in "${SMASH_DIR}/build".
 #=======================================================
 # cmake-format: on
 
@@ -33,38 +39,48 @@ else()
     add_definitions("-DLITTLE_ENDIAN_ARCHITECTURE")
 endif()
 
-message(STATUS "Looking for SMASH ...")
+message(STATUS "Looking for SMASH")
+
+if(NOT DEFINED ENV{SMASH_DIR})
+    message(FATAL_ERROR " \n" " Environment variable SMASH_DIR containing the path to the SMASH\n"
+                        " codebase found undefined but needed by FindSMASH.cmake module.\n")
+endif()
+
+if(NOT DEFINED ENV{SMASH_BUILD_DIR})
+    set(SMASH_BUILD_DIR "$ENV{SMASH_DIR}/build")
+else()
+    set(SMASH_BUILD_DIR "$ENV{SMASH_BUILD_DIR}")
+endif()
 
 find_package(GSL 2.0 REQUIRED)
 find_package(Eigen3 REQUIRED)
 find_package(Pythia 8.307 EXACT REQUIRED)
 
-set(SMASH_LIBRARIES ${GSL_LIBRARY} ${GSL_CBLAS_LIBRARY} ${Pythia_LIBRARIES} ${CMAKE_DL_LIBS})
-
 set(SMASH_INCLUDE_DIR
     $ENV{SMASH_DIR}/3rdparty/Cuba-4.2.1
     $ENV{SMASH_DIR}/3rdparty/einhard
     $ENV{SMASH_DIR}/3rdparty/yaml-cpp-0.7.0/include
-    $ENV{SMASH_DIR}/build/src/include
     $ENV{SMASH_DIR}/src/include
+    ${SMASH_BUILD_DIR}/src/include # For the decaymodes and particles header files
     ${GSL_INCLUDE_DIR}
     ${EIGEN3_INCLUDE_DIR}
     ${Pythia_INCLUDE_DIRS})
+message(VERBOSE "SMASH includes found in ${SMASH_INCLUDE_DIR}")
 
-message(STATUS "SMASH includes found in ${SMASH_INCLUDE_DIR}")
-find_library(SMASH_LIBRARY NAMES smash PATHS $ENV{SMASH_DIR}/build/src)
-find_library(EINHARD_LIBRARY NAMES einhard PATHS $ENV{SMASH_DIR}/build/3rdparty/einhard)
-find_library(CPPYAML_LIBRARY NAMES yaml-cpp PATHS $ENV{SMASH_DIR}/build/3rdparty/yaml-cpp-0.7.0)
-find_library(INTEGRATION_LIBRARY NAMES cuhre
-             PATHS $ENV{SMASH_DIR}/build/3rdparty/Cuba-4.2.1/src/cuhre)
+find_library(SMASH_LIBRARY NAMES smash PATHS ${SMASH_BUILD_DIR}/src)
+find_library(EINHARD_LIBRARY NAMES einhard PATHS ${SMASH_BUILD_DIR}/3rdparty/einhard)
+find_library(CPPYAML_LIBRARY NAMES yaml-cpp PATHS ${SMASH_BUILD_DIR}/3rdparty/yaml-cpp-0.7.0)
+find_library(INTEGRATION_LIBRARY NAMES cuhre PATHS ${SMASH_BUILD_DIR}/3rdparty/Cuba-4.2.1/src/cuhre)
 set(SMASH_LIBRARIES
-    ${SMASH_LIBRARIES}
+    ${GSL_LIBRARY}
+    ${GSL_CBLAS_LIBRARY}
+    ${Pythia_LIBRARIES}
+    ${CMAKE_DL_LIBS}
     ${EINHARD_LIBRARY}
     ${CPPYAML_LIBRARY}
     ${INTEGRATION_LIBRARY}
     ${SMASH_LIBRARY})
-
-# message(STATUS "SMASH libraries: ${SMASH_LIBRARIES}")
+message(VERBOSE "SMASH libraries: ${SMASH_LIBRARIES}")
 
 set(SMASH_FOUND FALSE)
 if(SMASH_INCLUDE_DIR
@@ -74,5 +90,4 @@ if(SMASH_INCLUDE_DIR
    AND INTEGRATION_LIBRARY)
     set(SMASH_FOUND TRUE)
 endif()
-
 message(STATUS "SMASH found: ${SMASH_FOUND}")
