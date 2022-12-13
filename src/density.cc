65f79472247108c96142ca6d35eef4092c4084ed
Jan Hammelmann <jan.hammelmann@live.de>
2022-12-13 17:31:20 +0100

    Fix position where the normalization is applied

--- src/density.cc
+++ src/density.cc
@@ -125,11 +125,6 @@ current_eckart_impl(const ThreeVector &r, const T &plist,
     }
   }
 
-  if (smearing) {
-    jmu_pos *= par.norm_factor_sf();
-    jmu_neg *= par.norm_factor_sf();
-  }
-
   // Eckart density (rest frame density)
   const double rho_eck = (jmu_pos.abs() - jmu_neg.abs()) * par.norm_factor_sf();
 
@@ -162,6 +157,10 @@ current_eckart_impl(const ThreeVector &r, const T &plist,
     for (int i = 1; i < 4; i++) {
       grad_j0[i - 1] += djmu_dxnu[i].x0() * par.norm_factor_sf();
     }
+  }
+  if (smearing) {
+    jmu_pos *= par.norm_factor_sf();
+    jmu_neg *= par.norm_factor_sf();
   }
   return std::make_tuple(rho_eck, jmu_pos + jmu_neg, grad_j0, curl_vecj,
                          djmu_dt, djmu_dx, djmu_dy, djmu_dz);
