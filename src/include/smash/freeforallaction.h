c54caaaa710286d328c0b0c07202d6b53c7197cb
Hendrik Roch <42201737+Hendrik1704@users.noreply.github.com>
2023-03-24 16:28:30 +0100

    Clarify the documentation of the FreeforallAction
    Co-authored-by: Alessandro Sciarra <asciarra@fias.uni-frankfurt.de>

--- src/include/smash/freeforallaction.h
+++ src/include/smash/freeforallaction.h
@@ -25,15 +25,20 @@ class FreeforallAction : public Action {
  public:
   /**
    * The FreeforallAction is able to add particles by providing an empty
-   * particle list for in_part and a list with particles which are supposed to
-   * be added to the evolution as out_part.
-   * If particles should be removed a filled particle list with the
-   * corresponding particles should be provided as in_part and an empty list as
-   * out_part.
+   * particle list for \c in_part and a list with particles which are supposed to
+   * be added to the evolution as \c out_part .
+   * If particles should be removed a filled particle list with the corresponding
+   * particles should be provided as \c in_part and an empty list as \c out_part .
+   *
    * \param[in] in_part List of incoming particles
    * \param[in] out_part List of outgoing particles
    * \param[in] absolute_labframe_time Absolute time at which the action is
    * supposed to take place
+   *
+   * \note This action takes advantage of how the parent class works in order to
+   * fake 0 &rarr; 1 and 1 &rarr; 0 processes. If particles need to be both added
+   * and removed one needs to create and perform this action twice. Said
+   * differently, the provided lists cannot be non-empty at the same time.
    */
   FreeforallAction(const ParticleList &in_part, const ParticleList &out_part,
                    double absolute_labframe_time)
