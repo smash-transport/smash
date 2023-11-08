213d1c94277f3def03b061fc3175871f5d6c1aff
Alessandro Sciarra <asciarra@fias.uni-frankfurt.de>
2023-11-08 17:49:35 +0100

    Add check on Experiment constructor path parameter
    The output path passed to the Experiment class constructor was not
    validated as e.g. done in the ExperimentBase::create function. If such a
    constructor was used (for instance in an external software) and a not
    existing path given (for example expecting that SMASH would have created
    the output folder), a segmentation fault would have occurred trying to
    create output files. Now the output path is validated and an error is
    given, making explicit the assumption that such a path must be valid,
    i.e. all directories in it must already exist.

--- src/include/smash/experiment.h
+++ src/include/smash/experiment.h
@@ -1347,6 +1347,11 @@ Experiment<Modus>::Experiment(Configuration &config,
   if (output_path == "") {
     throw std::invalid_argument(
         "Invalid empty output path provided to Experiment constructor.");
+  } else if (!std::filesystem::exists(output_path)) {
+    logg[LExperiment].fatal(
+        "Output path \"" + output_path.string() +
+        "\" used to create an Experiment object does not exist.");
+    throw NonExistingOutputPathRequest("Attempt to use not existing path.");
   }
   if (output_conf.is_empty()) {
     logg[LExperiment].warn() << "No \"Output\" section found in the input "
