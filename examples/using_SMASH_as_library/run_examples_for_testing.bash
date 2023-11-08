8276fa701b5aab393c65b71ea6238c78f9096204
Alessandro Sciarra <asciarra@fias.uni-frankfurt.de>
2023-11-08 17:46:06 +0100

    Remove unnecessary argument to test SMASH as library

--- examples/using_SMASH_as_library/run_examples_for_testing.bash
+++ examples/using_SMASH_as_library/run_examples_for_testing.bash
@@ -1,22 +1,21 @@
 #!/usr/bin/env bash
 
-if [[ $# -ne 2 ]] && [[ $# -ne 3 ]]; then
-  echo "Script requires 2 or 3 arguments. Usage: $0 SMASH_DIR EIGEN_DIR [PYTHIA_DIR_VAR]"
-  echo "PYTHIA_DIR_VAR needs to be given if Pythia is not installed system wide."
+if [[ $# -ne 1 ]] && [[ $# -ne 2 ]]; then
+  echo "Script requires 2 or 3 arguments. Usage: $0 SMASH_DIR [PYTHIA_CONFIG_PATH]"
+  echo "PYTHIA_CONFIG_PATH needs to be given if Pythia is not installed system wide."
   exit 1
 fi
 
 SMASH_DIR_VAR=$1
-EIGEN_DIR_VAR=$2
-PYTHIA_DIR_VAR=$3
+PYTHIA_CONFIG_PATH=$2
 
-if [[ "${PYTHIA_DIR_VAR}" = '' ]]; then
+if [[ "${PYTHIA_CONFIG_PATH}" = '' ]]; then
     PYTHIA_OPTION_FOR_CMAKE=''
 else
-    PYTHIA_OPTION_FOR_CMAKE="-DPythia_CONFIG_EXECUTABLE=${PYTHIA_DIR_VAR}"
+    PYTHIA_OPTION_FOR_CMAKE="-DPythia_CONFIG_EXECUTABLE=${PYTHIA_CONFIG_PATH}"
 fi
 
-export SMASH_DIR=$SMASH_DIR_VAR
+export SMASH_DIR="${SMASH_DIR_VAR}"
 EXAMPLE_DIR=${SMASH_DIR_VAR}/examples/using_SMASH_as_library
 
 fail_and_rm_build() {
@@ -25,10 +24,10 @@ fail_and_rm_build() {
   exit 1
 }
 
-cd $EXAMPLE_DIR
+cd "${EXAMPLE_DIR}"
 mkdir -p build && cd build
 # Assume the default compiler is fine or that `CC` and `CXX` environment variables are set
-cmake .. -DCMAKE_INSTALL_PREFIX=$EIGEN_DIR_VAR $PYTHIA_OPTION_FOR_CMAKE || fail_and_rm_build "Failed to setup cmake for SMASH library examples"
+cmake "${PYTHIA_OPTION_FOR_CMAKE}" .. || fail_and_rm_build "Failed to setup cmake for SMASH library examples"
 make -j$(nproc) || fail_and_rm_build "Failed to build SMASH library examples"
 
 ./example || fail_and_rm_build "Failed to run SMASH library examples"
