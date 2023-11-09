aedcf60238eb083c11edbb5ac1fc4347b7956b5d
Alessandro Sciarra <asciarra@fias.uni-frankfurt.de>
2023-11-09 09:14:50 +0100

    Add further check on output path argument of Experiment ctor

--- src/include/smash/experiment.h
+++ src/include/smash/experiment.h
@@ -1352,6 +1352,11 @@ Experiment<Modus>::Experiment(Configuration &config,
         "Output path \"" + output_path.string() +
         "\" used to create an Experiment object does not exist.");
     throw NonExistingOutputPathRequest("Attempt to use not existing path.");
+  } else if (!std::filesystem::is_directory(output_path)) {
+    logg[LExperiment].fatal("Output path \"" + output_path.string() +
+                            "\" used to create an Experiment object "
+                            "exists, but it is not a directory.");
+    throw std::logic_error("Attempt to use invalid existing path.");
   }
   if (output_conf.is_empty()) {
     logg[LExperiment].warn() << "No \"Output\" section found in the input "
