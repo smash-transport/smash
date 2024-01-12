da42d32e632662734e0a8148f6b8b468b714d053
Alessandro Sciarra <asciarra@fias.uni-frankfurt.de>
2024-01-12 10:38:29 +0100

    Add graphical visualization of benchmarks comparison

--- bin/benchmarks/compare_benchmarks.bash
+++ bin/benchmarks/compare_benchmarks.bash
@@ -24,6 +24,7 @@ function main()
     if [[ $# -eq 2 ]]; then
         Compare_Two_Benchmarks "$1" "$2"
     fi
+    Make_Benchmark_Plot "$@"
 }
 
 function Validate_command_line_options()
@@ -80,6 +81,72 @@ function Compare_Two_Benchmarks()
     done
     Print_list_of_benchmarks "dismissed after ${version_old}" "${old_dismissed_benchmarks[@]}"
     Print_list_of_benchmarks "newly introduced ${version_new}" "${new_introduced_benchmarks[@]}"
+}
+
+# Here we use the python plotting script in the same folder for which data
+# has to be prepared in a very precise form -> Do it in constructing the
+# python dictionary as bash string and then inject it in the python code
+#
+# NOTE: Since it is in general not guaranteed that all benchmark files contain
+#       the same SMASH runs, here first all existing runs are extracted in
+#       a list and then we go through such a list to put data in the correct
+#       format (cf. with the python plotting script).
+#
+# NOTE: The data are extracted into global associative arrays and here it is
+#       needed to iterate over them, which is not a native feature of bash.
+#       Hence we create a reference to each array in a loop and use it as needed.
+function Make_Benchmark_Plot()
+{
+    local data_dictionary x_ticks number_of_arrays list_of_benchmarks index name
+    number_of_arrays=$#
+    list_of_benchmarks=()
+    for ((index=1; index<=number_of_arrays; index++)); do
+        for name in BM_timings_${index}{,_errors}; do
+            declare -n array_ref="${name}"
+            list_of_benchmarks+=( "${!array_ref[@]}" )
+        done
+    done
+    # Remove duplicates from the list and then iterate through it
+    readarray -d $'\0' -t list_of_benchmarks < <(printf "%s\0" "${list_of_benchmarks[@]}" | sort -uz)
+    data_dictionary=''
+    for ((index=1; index<=number_of_arrays; index++)); do
+        data_dictionary+="  \"$(Extract_version_from_filename "${!index}")\": ["
+        for name in "${list_of_benchmarks[@]}"; do
+            declare -n \
+                value_array_ref="BM_timings_${index}"\
+                errors_array_ref="BM_timings_${index}_errors"
+            if [[ ${data_dictionary: -1} != '[' ]]; then
+                data_dictionary+=', '
+            fi
+            if [[ ${value_array_ref["${name}"]} != '' ]]; then
+                data_dictionary+="(${value_array_ref["${name}"]},${errors_array_ref["${name}"]})"
+            else
+                data_dictionary+="(0,0)"
+            fi
+        done
+        data_dictionary+=$'],\n'
+    done
+    data_dictionary='data = {'$'\n'"${data_dictionary%,$'\n'}"$'\n''}'
+    printf -v x_ticks '"%s", ' "${list_of_benchmarks[@]}"
+    x_ticks="[${x_ticks%, }]"
+    # Shorten names for x-ticks
+    x_ticks="${x_ticks// Run/}"             # remove ' Run'
+    x_ticks="${x_ticks//without/w\/o}"      # 'without' -> 'w/o'
+    x_ticks="${x_ticks//with/w.}"           # 'with' -> 'w.'
+    x_ticks="${x_ticks//Testparticles/TP}"  # 'Testparticles' -> 'TP'
+    echo "$x_ticks"
+    echo "${data_dictionary}"
+    python3 - <<EOF
+from multi_bar_histogram_plot import make_multi_bar_histogram_plot
+
+${data_dictionary}
+
+make_multi_bar_histogram_plot(data, total_width=.75, single_width=.95,
+                              xticks=${x_ticks},
+                              ylabel="Run time {s}",
+                              ymargin=0.2,
+                              annotate=True)
+EOF
 }
 
 #========================== Utility functions ==================================
