77c158d3c645ba055205181186561f78ac232bc7
gabriele-inghirami <inghirami@fias.uni-frankfurt.de>
2024-11-15 00:27:34 +0100

    Replace exec_program with execute_process
    The exec_program() command is deprecated, one should use execute_process() instead

--- cmake/FindPythia.cmake
+++ cmake/FindPythia.cmake
@@ -50,7 +50,9 @@ if(NOT ("${Pythia_CONFIG_EXECUTABLE}" STREQUAL ""))
         message(FATAL_ERROR "pythia8-config executable not found, please check \"-DPythia_CONFIG_EXECUTABLE\" or use \"-DPYTHIA_ROOT_DIR\""
         )
     else()
-        exec_program(${Pythia_CONFIG_EXECUTABLE} ARGS "--prefix" OUTPUT_VARIABLE PYTHIA_ROOT_DIR)
+        execute_process(COMMAND ${Pythia_CONFIG_EXECUTABLE} --prefix
+                        OUTPUT_VARIABLE PYTHIA_ROOT_DIR_WITH_TRAILING_NEWLINE)
+        string(STRIP ${PYTHIA_ROOT_DIR_WITH_TRAILING_NEWLINE} PYTHIA_ROOT_DIR)
     endif()
 endif()
 
