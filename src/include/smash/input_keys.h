smash-transport/smash

commit c258ff5fb0598cf32320cb4d3f81a7fa3d553c65

Remove old interface for Configuration::take with default
This required a huge set of changes as expected. Most of them were
simply replacing the key labels with the appropriate InputKeys
member. However, some keys (Start_Time in box and sphere modi as well as
Fractional_Photons) were taken with a default, although not having one
and being stated as required in the documentation. This has been fixed
making them really mandatory.

Moreover, the take operations in the ListModus class constructor have
been refactored as a naive replacement was not really possible. Also the
orientation parsing logic in the Nucleus class has been simplified in
code, but left unchanged.

Note, that some tests are now failing as there is a mismatch in how the
taken keys are in the sub-configurations w.r.t. their labels. Now keys
are (almost) always taken with all labels, but different objects get
sub-configurations without the top-level labels. This will be fixed
later using the new method to extract a complete sub-configuration as
much as possible.

src/scatteractionsfinder.cc
@@ -34,10 +34,10 @@ static constexpr int LFindScatter = LogArea::FindScatter::id;
 ScatterActionsFinder::ScatterActionsFinder(
     Configuration& config, const ExperimentParameters& parameters)
     : finder_parameters_(create_finder_parameters(config, parameters)),
-      isotropic_(config.take({"Collision_Term", "Isotropic"}, false)),
+      isotropic_(config.take(InputKeys::collTerm_isotropic)),
       box_length_(parameters.box_length),
-      string_formation_time_(config.take(
-          {"Collision_Term", "String_Parameters", "Formation_Time"}, 1.)) {
+      string_formation_time_(
+          config.take(InputKeys::collTerm_stringParam_formationTime)) {
   if (is_constant_elastic_isotropic()) {
     logg[LFindScatter].info(
         "Constant elastic isotropic cross-section mode:", " using ",
@@ -102,39 +102,36 @@ ScatterActionsFinder::ScatterActionsFinder(
   }
 
   if (finder_parameters_.strings_switch) {
-    auto subconfig = config.extract_sub_configuration(
-        {"Collision_Term", "String_Parameters"}, Configuration::GetEmpty::Yes);
     string_process_interface_ = std::make_unique<StringProcess>(
-        subconfig.take({"String_Tension"}, 1.0), string_formation_time_,
-        subconfig.take({"Gluon_Beta"}, 0.5),
-        subconfig.take({"Gluon_Pmin"}, 0.001),
-        subconfig.take({"Quark_Alpha"}, 2.0),
-        subconfig.take({"Quark_Beta"}, 7.0),
-        subconfig.take({"Strange_Supp"}, 0.16),
-        subconfig.take({"Diquark_Supp"}, 0.036),
-        subconfig.take({"Sigma_Perp"}, 0.42),
-        subconfig.take({"StringZ_A_Leading"}, 0.2),
-        subconfig.take({"StringZ_B_Leading"}, 2.0),
-        subconfig.take({"StringZ_A"}, 2.0), subconfig.take({"StringZ_B"}, 0.55),
-        subconfig.take({"String_Sigma_T"}, 0.5),
-        subconfig.take({"Form_Time_Factor"}, 1.0),
-        subconfig.take({"Mass_Dependent_Formation_Times"}, false),
-        subconfig.take({"Prob_proton_to_d_uu"}, 1. / 3.),
-        subconfig.take({"Separate_Fragment_Baryon"}, true),
-        subconfig.take({"Popcorn_Rate"}, 0.15),
-        subconfig.take({"Use_Monash_Tune"},
-                       parameters.use_monash_tune_default.value()));
+        config.take(InputKeys::collTerm_stringParam_stringTension),
+        string_formation_time_,
+        config.take(InputKeys::collTerm_stringParam_gluonBeta),
+        config.take(InputKeys::collTerm_stringParam_gluonPMin),
+        config.take(InputKeys::collTerm_stringParam_quarkAlpha),
+        config.take(InputKeys::collTerm_stringParam_quarkBeta),
+        config.take(InputKeys::collTerm_stringParam_strangeSuppression),
+        config.take(InputKeys::collTerm_stringParam_diquarkSuppression),
+        config.take(InputKeys::collTerm_stringParam_sigmaPerp),
+        config.take(InputKeys::collTerm_stringParam_stringZALeading),
+        config.take(InputKeys::collTerm_stringParam_stringZBLeading),
+        config.take(InputKeys::collTerm_stringParam_stringZA),
+        config.take(InputKeys::collTerm_stringParam_stringZB),
+        config.take(InputKeys::collTerm_stringParam_stringSigmaT),
+        config.take(InputKeys::collTerm_stringParam_formTimeFactor),
+        config.take(InputKeys::collTerm_stringParam_mDependentFormationTimes),
+        config.take(InputKeys::collTerm_stringParam_probabilityPToDUU),
+        config.take(InputKeys::collTerm_stringParam_separateFragmentBaryon),
+        config.take(InputKeys::collTerm_stringParam_popcornRate),
+        config.take(InputKeys::collTerm_stringParam_useMonashTune));
   }
 }
 
 ScatterActionsFinderParameters create_finder_parameters(
     Configuration& config, const ExperimentParameters& parameters) {
   std::pair<double, double> sqrts_range_Npi =
-      config.take({"Collision_Term", "String_Transition", "Sqrts_Range_Npi"},
-                  InputKeys::collTerm_stringTrans_rangeNpi.default_value());
+      config.take(InputKeys::collTerm_stringTrans_rangeNpi);
   std::pair<double, double> sqrts_range_NN =
-      config.take({"Collision_Term", "String_Transition", "Sqrts_Range_NN"},
-                  InputKeys::collTerm_stringTrans_rangeNN.default_value());
+      config.take(InputKeys::collTerm_stringTrans_rangeNN);
   if (sqrts_range_Npi.first < nucleon_mass + pion_mass) {
     sqrts_range_Npi.first = nucleon_mass + pion_mass;
     if (sqrts_range_Npi.second < sqrts_range_Npi.first)
@@ -153,9 +150,7 @@ ScatterActionsFinderParameters create_finder_parameters(
         "threshold. New range is [",
         sqrts_range_NN.first, ',', sqrts_range_NN.second, "] GeV.");
   }
-  auto xs_strategy =
-      config.take({"Collision_Term", "Total_Cross_Section_Strategy"},
-                  InputKeys::collTerm_totXsStrategy.default_value());
+  auto xs_strategy = config.take(InputKeys::collTerm_totXsStrategy);
   if (xs_strategy == TotalCrossSectionStrategy::BottomUp) {
     logg[LFindScatter].info(
         "Evaluating total cross sections from partial processes.");
@@ -173,40 +168,31 @@ ScatterActionsFinderParameters create_finder_parameters(
         "Evaluating total cross sections from parametrizations only for "
         "measured processes.");
   }
-  return {
-      config.take({"Collision_Term", "Elastic_Cross_Section"}, -1.),
-      parameters.low_snn_cut,
-      parameters.scale_xs,
-      config.take({"Collision_Term", "Additional_Elastic_Cross_Section"}, 0.0),
-      parameters.maximum_cross_section,
-      parameters.coll_crit,
-      parameters.nnbar_treatment,
-      parameters.included_2to2,
-      parameters.included_multi,
-      parameters.testparticles,
-      parameters.two_to_one,
-      config.take({"Modi", "Collider", "Collisions_Within_Nucleus"}, false),
-      parameters.strings_switch,
-      config.take({"Collision_Term", "Use_AQM"}, true),
-      config.take({"Collision_Term", "Strings_with_Probability"}, true),
-      config.take({"Collision_Term", "Only_Warn_For_High_Probability"}, false),
-      StringTransitionParameters{
-          sqrts_range_Npi, sqrts_range_NN,
-          config.take({"Collision_Term", "String_Transition", "Sqrts_Lower"},
-                      InputKeys::collTerm_stringTrans_lower.default_value()),
-          config.take(
-              {"Collision_Term", "String_Transition", "Sqrts_Range_Width"},
-              InputKeys::collTerm_stringTrans_range_width.default_value()),
-          config.take(
-              {"Collision_Term", "String_Transition", "PiPi_Offset"},
-              InputKeys::collTerm_stringTrans_pipiOffset.default_value()),
-          config.take(
-              {"Collision_Term", "String_Transition", "KN_Offset"},
-              InputKeys::collTerm_stringTrans_KNOffset.default_value())},
-      xs_strategy,
-      config.take({"Collision_Term", "Pseudoresonance"},
-                  InputKeys::collTerm_pseudoresonance.default_value()),
-      parameters.spin_interactions};
+  return {config.take(InputKeys::collTerm_elasticCrossSection),
+          parameters.low_snn_cut,
+          parameters.scale_xs,
+          config.take(InputKeys::collTerm_additionalElasticCrossSection),
+          parameters.maximum_cross_section,
+          parameters.coll_crit,
+          parameters.nnbar_treatment,
+          parameters.included_2to2,
+          parameters.included_multi,
+          parameters.testparticles,
+          parameters.two_to_one,
+          config.take(InputKeys::modi_collider_collisionWithinNucleus),
+          parameters.strings_switch,
+          config.take(InputKeys::collTerm_useAQM),
+          config.take(InputKeys::collTerm_stringsWithProbability),
+          config.take(InputKeys::collTerm_onlyWarnForHighProbability),
+          StringTransitionParameters{
+              sqrts_range_Npi, sqrts_range_NN,
+              config.take(InputKeys::collTerm_stringTrans_lower),
+              config.take(InputKeys::collTerm_stringTrans_range_width),
+              config.take(InputKeys::collTerm_stringTrans_pipiOffset),
+              config.take(InputKeys::collTerm_stringTrans_KNOffset)},
+          xs_strategy,
+          config.take(InputKeys::collTerm_pseudoresonance),
+          parameters.spin_interactions};
 }
 
 ActionPtr ScatterActionsFinder::check_collision_two_part(

src/boxmodus.cc
@@ -19,6 +19,7 @@
 #include "smash/constants.h"
 #include "smash/cxx17compat.h"
 #include "smash/experimentparameters.h"
+#include "smash/input_keys.h"
 #include "smash/logging.h"
 #include "smash/quantumsampling.h"
 #include "smash/random.h"
@@ -67,16 +68,16 @@ BoxModus::BoxModus(Configuration modus_config,
     : initial_condition_(modus_config.take({"Box", "Initial_Condition"})),
       length_(modus_config.take({"Box", "Length"})),
       equilibration_time_(
-          modus_config.take({"Box", "Equilibration_Time"}, -1.)),
+          modus_config.take(InputKeys::modi_box_equilibrationTime)),
       temperature_(modus_config.take({"Box", "Temperature"})),
-      start_time_(modus_config.take({"Box", "Start_Time"}, 0.)),
+      start_time_(modus_config.take({"Box", "Start_Time"})),
       use_thermal_(
-          modus_config.take({"Box", "Use_Thermal_Multiplicities"}, false)),
-      mub_(modus_config.take({"Box", "Baryon_Chemical_Potential"}, 0.)),
-      mus_(modus_config.take({"Box", "Strange_Chemical_Potential"}, 0.)),
-      muq_(modus_config.take({"Box", "Charge_Chemical_Potential"}, 0.)),
+          modus_config.take(InputKeys::modi_box_useThermalMultiplicities)),
+      mub_(modus_config.take(InputKeys::modi_box_baryonChemicalPotential)),
+      mus_(modus_config.take(InputKeys::modi_box_strangeChemicalPotential)),
+      muq_(modus_config.take(InputKeys::modi_box_chargeChemicalPotential)),
       account_for_resonance_widths_(
-          modus_config.take({"Box", "Account_Resonance_Widths"}, true)),
+          modus_config.take(InputKeys::modi_box_accountResonanceWidths)),
       init_multipl_(use_thermal_
                         ? std::map<PdgCode, int>()
                         : modus_config.take({"Box", "Init_Multiplicities"})
@@ -92,7 +93,7 @@ BoxModus::BoxModus(Configuration modus_config,
                          modus_config.take({"Box", "Jet", "Jet_PDG"}))
                    : std::nullopt),
 
-      jet_mom_(modus_config.take({"Box", "Jet", "Jet_Momentum"}, 20.)) {
+      jet_mom_(modus_config.take(InputKeys::modi_box_jet_jetMomentum)) {
   if (parameters.res_lifetime_factor < 0.) {
     throw std::invalid_argument(
         "Resonance lifetime modifier cannot be negative!");

src/collidermodus.cc
@@ -91,7 +91,7 @@ ColliderModus::ColliderModus(Configuration modus_config,
 
   // Get the Fermi-Motion input (off, on, frozen)
   if (modus_cfg.has_value({"Fermi_Motion"})) {
-    fermi_motion_ = modus_cfg.take({"Fermi_Motion"}, FermiMotion::Off);
+    fermi_motion_ = modus_cfg.take(InputKeys::modi_collider_fermiMotion);
   }
 
   // Get the total nucleus-nucleus collision energy. Since there is
@@ -269,7 +269,7 @@ ColliderModus::ColliderModus(Configuration modus_config,
   /// \todo include a check that only one method of specifying impact is used
   // whether the direction of separation should be ramdomly smapled
   random_reaction_plane_ =
-      modus_cfg.take({"Impact", "Random_Reaction_Plane"}, false);
+      modus_cfg.take(InputKeys::modi_collider_impact_randomReactionPlane);
   // Look for user-defined initial separation between nuclei.
   if (modus_cfg.has_value({"Initial_Distance"})) {
     initial_z_displacement_ = modus_cfg.take({"Initial_Distance"});
@@ -278,8 +278,18 @@ ColliderModus::ColliderModus(Configuration modus_config,
     initial_z_displacement_ /= 2.0;
   }
 
-  FluidizationType IC_type = modus_cfg.take({"Initial_Conditions", "Type"},
-                                            FluidizationType::ConstantTau);
+  if (fermi_motion_ == FermiMotion::On) {
+    logg[LCollider].info() << "Fermi motion is ON.";
+  } else if (fermi_motion_ == FermiMotion::Frozen) {
+    logg[LCollider].info() << "FROZEN Fermi motion is on.";
+  } else if (fermi_motion_ == FermiMotion::Off) {
+    logg[LCollider].info() << "Fermi motion is OFF.";
+  }
+
+  FluidizationType IC_type = FluidizationType::ConstantTau;
+  if (modus_cfg.has_value(InputKeys::modi_collider_initialConditions_type)) {
+    IC_type = modus_cfg.take(InputKeys::modi_collider_initialConditions_type);
+  }
 
   if (IC_type == FluidizationType::ConstantTau) {
     if (modus_cfg.has_value({"Initial_Conditions", "Proper_Time"})) {

src/experiment.cc
@@ -132,14 +132,14 @@ ExperimentPtr ExperimentBase::create(Configuration &config,
 ExperimentParameters create_experiment_parameters(Configuration &config) {
   logg[LExperiment].trace() << SMASH_SOURCE_LOCATION;
 
-  const int ntest = config.take({"General", "Testparticles"}, 1);
+  const int ntest = config.take(InputKeys::gen_testparticles);
   if (ntest <= 0) {
     throw std::invalid_argument("Testparticle number should be positive!");
   }
 
   // sets whether to consider only participants in thermodynamic outputs or not
   const bool only_participants =
-      config.take({"Output", "Thermodynamics", "Only_Participants"}, false);
+      config.take(InputKeys::output_thermodynamics_onlyParticipants);
 
   if (only_participants && config.has_value({"Potentials"})) {
     throw std::invalid_argument(
@@ -163,7 +163,7 @@ ExperimentParameters create_experiment_parameters(Configuration &config) {
   /* If this Delta_Time option is absent (this can be for timestepless mode)
    * just assign 1.0 fm, reasonable value will be set at event initialization
    */
-  const double dt = config.take({"General", "Delta_Time"}, 1.);
+  const double dt = config.take(InputKeys::gen_deltaTime);
   if (dt <= 0.) {
     throw std::invalid_argument("Delta_Time cannot be zero or negative.");
   }
@@ -193,7 +193,8 @@ ExperimentParameters create_experiment_parameters(Configuration &config) {
     output_times.push_back(t_end + 1.);
     output_clock = std::make_unique<CustomClock>(output_times);
   } else {
-    const double output_dt = config.take({"Output", "Output_Interval"}, t_end);
+    const double output_dt =
+        config.take(InputKeys::output_outputInterval, t_end);
     if (output_dt <= 0.) {
       throw std::invalid_argument(
           "Output_Interval cannot be zero or negative.");
@@ -259,7 +260,7 @@ ExperimentParameters create_experiment_parameters(Configuration &config) {
   /* Elastic collisions between the nucleons with the square root s
    * below low_snn_cut are excluded. */
   const double low_snn_cut =
-      config.take({"Collision_Term", "Elastic_NN_Cutoff_Sqrts"}, 1.98);
+      config.take(InputKeys::collTerm_elasticNNCutoffSqrts);
   const auto proton = ParticleType::try_find(pdg::p);
   const auto pion = ParticleType::try_find(pdg::pi_z);
   if (proton && pion &&
@@ -268,12 +269,10 @@ ExperimentParameters create_experiment_parameters(Configuration &config) {
                            " of the process: NN to NNpi");
   }
   const bool potential_affect_threshold =
-      config.take({"Lattice", "Potentials_Affect_Thresholds"}, false);
-  const double scale_xs =
-      config.take({"Collision_Term", "Cross_Section_Scaling"}, 1.0);
+      config.take(InputKeys::lattice_potentialsAffectThreshold);
+  const double scale_xs = config.take(InputKeys::collTerm_crossSectionScaling);
 
-  const auto criterion = config.take({"Collision_Term", "Collision_Criterion"},
-                                     CollisionCriterion::Covariant);
+  const auto criterion = config.take(InputKeys::collTerm_collisionCriterion);
 
   if (config.has_value({"Collision_Term", "Fixed_Min_Cell_Length"}) &&
       criterion != CollisionCriterion::Stochastic) {
@@ -301,50 +300,41 @@ ExperimentParameters create_experiment_parameters(Configuration &config) {
   const double maximum_cross_section_default =
       ParticleType::exists("d'") ? 2000.0 : 200.0;
 
-  double maximum_cross_section =
-      config.take({"Collision_Term", "Maximum_Cross_Section"},
-                  maximum_cross_section_default);
+  double maximum_cross_section = config.take(
+      InputKeys::collTerm_maximumCrossSection, maximum_cross_section_default);
   maximum_cross_section *= scale_xs;
-  return {
-      std::make_unique<UniformClock>(0.0, dt, t_end),
-      std::move(output_clock),
-      config.take({"General", "Ensembles"}, 1),
-      ntest,
-      config.take({"General", "Derivatives_Mode"},
-                  DerivativesMode::CovariantGaussian),
-      config.has_value({"Potentials", "VDF"})
-          ? RestFrameDensityDerivativesMode::On
-          : RestFrameDensityDerivativesMode::Off,
-      config.take({"General", "Field_Derivatives_Mode"},
-                  FieldDerivativesMode::ChainRule),
-      config.take({"General", "Smearing_Mode"},
-                  SmearingMode::CovariantGaussian),
-      config.take({"General", "Gaussian_Sigma"}, 1.),
-      config.take({"General", "Gauss_Cutoff_In_Sigma"}, 4.),
-      config.take({"General", "Discrete_Weight"}, 1. / 3.0),
-      config.take({"General", "Triangular_Range"}, 2.0),
-      criterion,
-      config.take({"Collision_Term", "Two_to_One"}, true),
-      config.take({"Collision_Term", "Included_2to2"}, ReactionsBitSet().set()),
-      config.take({"Collision_Term", "Multi_Particle_Reactions"},
-                  MultiParticleReactionsBitSet().reset()),
-      config.take({"Collision_Term", "Strings"}, modus_chooser != "Box"),
-      config.take({"Collision_Term", "Resonance_Lifetime_Modifier"}, 1.),
-      config.take({"Collision_Term", "NNbar_Treatment"},
-                  NNbarTreatment::Strings),
-      low_snn_cut,
-      potential_affect_threshold,
-      box_length,
-      maximum_cross_section,
-      config.take({"Collision_Term", "Fixed_Min_Cell_Length"}, 2.5),
-      scale_xs,
-      only_participants,
-      config.take({"Collision_Term", "Include_Weak_And_EM_Decays_At_The_End"},
-                  false),
-      config.take({"Collision_Term", "Decay_Initial_Particles"},
-                  InputKeys::collTerm_decayInitial.default_value()),
-      config.take({"Collision_Term", "Spin_Interactions"}, false),
-      std::nullopt};
+  return {std::make_unique<UniformClock>(0.0, dt, t_end),
+          std::move(output_clock),
+          config.take(InputKeys::gen_ensembles),
+          ntest,
+          config.take(InputKeys::gen_derivativesMode),
+          config.has_value({"Potentials", "VDF"})
+              ? RestFrameDensityDerivativesMode::On
+              : RestFrameDensityDerivativesMode::Off,
+          config.take(InputKeys::gen_fieldDerivativesMode),
+          config.take(InputKeys::gen_smearingMode),
+          config.take(InputKeys::gen_smearingGaussianSigma),
+          config.take(InputKeys::gen_smearingGaussCutoffInSigma),
+          config.take(InputKeys::gen_smearingDiscreteWeight),
+          config.take(InputKeys::gen_smearingTriangularRange),
+          criterion,
+          config.take(InputKeys::collTerm_twoToOne),
+          config.take(InputKeys::collTerm_includedTwoToTwo),
+          config.take(InputKeys::collTerm_multiParticleReactions),
+          config.take(InputKeys::collTerm_strings, modus_chooser != "Box"),
+          config.take(InputKeys::collTerm_resonanceLifetimeModifier),
+          config.take(InputKeys::collTerm_nnbarTreatment),
+          low_snn_cut,
+          potential_affect_threshold,
+          box_length,
+          maximum_cross_section,
+          config.take(InputKeys::collTerm_fixedMinCellLength),
+          scale_xs,
+          only_participants,
+          config.take(InputKeys::collTerm_includeDecaysAtTheEnd),
+          config.take(InputKeys::collTerm_decayInitial),
+          config.take(InputKeys::collTerm_spinInteractions),
+          std::nullopt};
 }
 
 std::string format_measurements(const std::vector<Particles> &ensembles,

src/include/smash/configuration.h
@@ -1218,15 +1218,6 @@ class Configuration {
     return default_value;
   }
 
-  /// \see take
-  template <typename T>
-  T take(std::initializer_list<const char *> keys, T default_value) {
-    if (has_value(keys)) {
-      return take(keys);
-    }
-    return default_value;
-  }
-
   /**
    * Additional interface for SMASH to read configuration values without
    * removing them.

commit a6fff6268cc0940e30ead1eeb4f8c611562e1dd8

Fix wrong default value of Monash tune key
Although set to false, which is the correct behavioural default for most
SMASH runs, the InputKey object's default value should have been
DefaultValue::Dependent as it is now. That is the way to signal in the
code that there is no a fixed default value and that this depends on
other aspects. Being this wrong, taking the key as introduced in commit
c258ff5  was resulting in not using the Monash tune even at high
energies, unless explicitly specified in the configuration (rarely the
case). Now that the default is marked as dependent, the key must be
taken explicitly specifying the default value and this is done as it was
before commit c258ff5.

src/include/smash/input_keys.h
@@ -3000,7 +3000,9 @@ struct InputKeys {
    * \see_key{key_CT_SP_use_monash_tune_}
    */
   inline static const Key<bool> collTerm_stringParam_useMonashTune{
-      InputSections::c_stringParameters + "Use_Monash_Tune", false, {"3.0"}};
+      InputSections::c_stringParameters + "Use_Monash_Tune",
+      DefaultType::Dependent,
+      {"3.0"}};
 
   /*!\Userguide
    * \page doxypage_input_conf_ct_dileptons

src/scatteractionsfinder.cc
@@ -122,7 +122,8 @@ ScatterActionsFinder::ScatterActionsFinder(
         config.take(InputKeys::collTerm_stringParam_probabilityPToDUU),
         config.take(InputKeys::collTerm_stringParam_separateFragmentBaryon),
         config.take(InputKeys::collTerm_stringParam_popcornRate),
-        config.take(InputKeys::collTerm_stringParam_useMonashTune));
+        config.take(InputKeys::collTerm_stringParam_useMonashTune,
+                    parameters.use_monash_tune_default.value()));
   }
 }
 