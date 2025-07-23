4f6afebac674ca9c849811c0d88b4e88f9d60ae9
Nils Saß <nsass@itp.uni-frankfurt.de>
2025-07-23 17:29:15 +0200

    correct bug in listmodus test

--- src/tests/listmodus.cc
+++ src/tests/listmodus.cc
@@ -567,23 +567,60 @@ TEST(try_create_particle_func) {
 // Test that try_create_particle correctly stores the provided spin vector
 // when spin interactions are enabled.
 TEST(try_create_particle_with_spin_func) {
-  ListModus list_modus = create_list_modus_for_test();
-  list_modus.spin_interaction_type_ = SpinInteractionType::On;
+  // Create a ListModus with spin interactions enabled
+  ExperimentParameters parameters_with_spin{
+      std::make_unique<UniformClock>(0., 0.1, 300.0),
+      std::make_unique<UniformClock>(0., 1., 300.0),
+      1,  // ensembles
+      1,  // testparticles
+      DerivativesMode::CovariantGaussian,
+      RestFrameDensityDerivativesMode::Off,
+      FieldDerivativesMode::ChainRule,
+      SmearingMode::CovariantGaussian,
+      1.0,
+      4.0,
+      0.333333,
+      2.0,
+      CollisionCriterion::Geometric,
+      true,
+      Test::all_reactions_included(),
+      Test::no_multiparticle_reactions(),
+      false,  // strings
+      1.0,
+      NNbarTreatment::NoAnnihilation,
+      0.,
+      false,
+      -1.0,
+      200.0,
+      2.5,
+      1.0,
+      false,
+      false,
+      true,
+      SpinInteractionType::On,
+      std::nullopt};
 
+  Configuration config{R"(
+    Modi:
+      List:
+        File_Directory: ToBeSet
+        File_Prefix: event
+    )"};
+  config.set_value(InputKeys::modi_list_fileDirectory, testoutputpath.string());
+  
+  ListModus list_modus = ListModus(std::move(config), parameters_with_spin);
   Particles particles;
   ParticleData smashon = Test::smashon_random();
-
+  FourVector r = smashon.position(), p = smashon.momentum();
+  PdgCode pdg = smashon.pdgcode();
   FourVector spin_vec(0.1, 0.2, 0.3, 0.4);
-
-  list_modus.try_create_particle(particles, pdg,
-                                 r.x0(), r.x1(), r.x2(), r.x3(),
-                                 Test::smashon_mass,
-                                 p.x0(), p.x1(), p.x2(), p.x3(),
-                                 spin_vec[0], spin_vec[1], spin_vec[2], spin_vec[3]);
+  list_modus.try_create_particle(particles, pdg, r.x0(), r.x1(), r.x2(), r.x3(),
+                                 Test::smashon_mass, p.x0(), p.x1(), p.x2(),
+                                 p.x3(), spin_vec[0], spin_vec[1], spin_vec[2],
+                                 spin_vec[3]);
 
   ParticleList plist = particles.copy_to_vector();
   ParticleData created = plist.back();
-
   compare_fourvector(created.spin_vector(), spin_vec);
 }
 
