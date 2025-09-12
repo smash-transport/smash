c3baacb30d0e17ac8eff6bfd47615a32f45db0e4
Alessandro Sciarra <sciarra@itp.uni-frankfurt.de>
2025-09-13 00:19:18 +0200

    Add InputKeys test to check if all enum keys have to_string
    This is meant to support the developer in the future to ensure that all
    keys with an enum type have a conversion to string to be able to be set
    in the Configuration object.

--- src/include/smash/traits.h
+++ src/include/smash/traits.h
@@ -11,6 +11,7 @@
 #define SRC_INCLUDE_SMASH_TRAITS_H_
 
 #include <sstream>
+#include <string>
 #include <type_traits>
 
 namespace smash {
@@ -43,6 +44,36 @@ struct is_writable_to_stream<
 template <typename S, typename T>
 inline constexpr bool is_writable_to_stream_v =
     is_writable_to_stream<S, T>::value;
+
+/**
+ * Type trait to infer if there is an <tt>std::string to_string(T)</tt> overload
+ * for a given type <tt>T</tt>. This is the general case in which the overload
+ * is missing.
+ *
+ * \tparam T The type for which the overload has to be checked.
+ * \tparam Enable Type set to \c void as utility to implement the type trait.
+ */
+template <typename T, typename Enable = void>
+struct has_to_string : std::false_type {};
+
+/**
+ * Trait specialization for the case when the overload is present. The test is
+ * done in two parts. The second template parameter is used to test if there
+ * exists a \c to_string(T) overload, while inheriting from \c is_same will
+ * provide a \c value boolean static constant member set to \c true if the
+ * return value is an \c std::string and to \c false otherwise.
+ *
+ * \tparam T The type for which the overload has to be checked.
+ */
+template <typename T>
+struct has_to_string<T, std::void_t<decltype(to_string(std::declval<T>()))>>
+    : std::is_same<decltype(to_string(std::declval<T>())), std::string> {};
+
+/**
+ * Helper alias which is always defined next to a type trait.
+ */
+template <typename T>
+inline constexpr bool has_to_string_v = has_to_string<T>::value;
 
 }  // namespace smash
 
