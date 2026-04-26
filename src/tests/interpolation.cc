77da10dfa6baf9a689f5b3c05d9f1cfaed31e115
Robin Sattler <sattler@itp.uni-frankfurt.de>
2026-04-26 13:38:05 +0200

    Add failing tests for out-of-bound values in interpolations

--- src/tests/interpolation.cc
+++ src/tests/interpolation.cc
@@ -1,6 +1,6 @@
 /*
  *
- *    Copyright (c) 2015-2018,2020,2022
+ *    Copyright (c) 2015-2018,2020,2022,2026
  *      SMASH Team
  *
  *    GNU General Public License (GPLv3 or later)
@@ -36,30 +36,6 @@ TEST(permutation) {
   COMPARE(permuted_y, correctly_permuted_y);
 }
 
-TEST(interpolate_data_linear) {
-  std::vector<double> x = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-  std::vector<double> y = {1, 2, 0, 0, 0, 0, 0, 8, 9};
-  InterpolateDataLinear<double> f(x, y);
-  x.resize(0);
-  y.resize(0);
-  COMPARE(f(1.5), 1.5);
-  COMPARE(f(5), 0.0);
-  COMPARE(f(0), 0.0);
-  COMPARE(f(10), 10.0);
-}
-
-TEST(interpolate_data_linear_unsorted) {
-  std::vector<double> x = {5, 6, 7, 9, 3, 4, 1, 2, 8};
-  std::vector<double> y = {0, 0, 0, 9, 0, 0, 1, 2, 8};
-  InterpolateDataLinear<double> f(x, y);
-  x.resize(0);
-  y.resize(0);
-  COMPARE(f(1.5), 1.5);
-  COMPARE(f(5), 0.0);
-  COMPARE(f(0), 0.0);
-  COMPARE(f(10), 10.0);
-}
-
 TEST(find_index) {
   const std::vector<double> data = {0.0, 0.2, 0.4, 0.6, 0.8, 1.0};
   COMPARE(find_index(data, -1.0), 0ul);
@@ -70,24 +46,80 @@ TEST(find_index) {
   COMPARE(find_index(data, 10.0), 5ul);
 }
 
+TEST(interpolate_data_linear) {
+  std::vector<double> x = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+  std::vector<double> y = {1, 2, 0, 0, 0, 0, 0, 8, 9};
+  const InterpolateDataLinear<double> f(x, y);
+  x.clear();
+  y.clear();
+  COMPARE(f(1.5), 1.5);
+  COMPARE(f(5), 0.0);
+}
+
+TEST_CATCH(interpolate_data_linear_value_out_of_lower_bound,
+           std::out_of_range) {
+  std::vector<double> x = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+  std::vector<double> y = {1, 2, 0, 0, 0, 0, 0, 8, 9};
+  const InterpolateDataLinear<double> f(x, y);
+  x.clear();
+  y.clear();
+  f(0);
+}
+
+TEST_CATCH(interpolate_data_linear_value_out_of_upper_bound,
+           std::out_of_range) {
+  std::vector<double> x = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+  std::vector<double> y = {1, 2, 0, 0, 0, 0, 0, 8, 9};
+  const InterpolateDataLinear<double> f(x, y);
+  x.clear();
+  y.clear();
+  f(10);
+}
+
+TEST(interpolate_data_linear_unsorted) {
+  std::vector<double> x = {5, 6, 7, 9, 3, 4, 1, 2, 8};
+  std::vector<double> y = {0, 0, 0, 9, 0, 0, 1, 2, 8};
+  const InterpolateDataLinear<double> f(x, y);
+  x.clear();
+  y.clear();
+  COMPARE(f(1.5), 1.5);
+  COMPARE(f(5), 0.0);
+}
+
 TEST(interpolate_data_spline) {
   std::vector<double> x = {1, 2, 3, 4, 5, 6, 7, 8, 9};
   std::vector<double> y = x;
   const InterpolateDataSpline f(x, y);
-  x.resize(0);
-  y.resize(0);
+  x.clear();
+  y.clear();
   COMPARE(f(1.5), 1.5);
-  COMPARE(f(0), 1.0);
-  COMPARE(f(10), 9.0);
+}
+
+TEST_CATCH(interpolate_data_spline_value_out_of_lower_bound,
+           std::out_of_range) {
+  std::vector<double> x = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+  std::vector<double> y = x;
+  const InterpolateDataSpline f(x, y);
+  x.clear();
+  y.clear();
+  f(0);
+}
+
+TEST_CATCH(interpolate_data_spline_value_out_of_upper_bound,
+           std::out_of_range) {
+  std::vector<double> x = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+  std::vector<double> y = x;
+  const InterpolateDataSpline f(x, y);
+  x.clear();
+  y.clear();
+  f(10);
 }
 
 TEST(interpolate_data_spline_unsorted) {
   std::vector<double> x = {7, 5, 4, 8, 6, 2, 1, 3, 9};
   std::vector<double> y = x;
   const InterpolateDataSpline f(x, y);
-  x.resize(0);
-  y.resize(0);
+  x.clear();
+  y.clear();
   COMPARE(f(1.5), 1.5);
-  COMPARE(f(0), 1.0);
-  COMPARE(f(10), 9.0);
 }
