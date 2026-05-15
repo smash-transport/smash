20c3208e1b607f835ae37c837232c6ccbf75463f
Robin Sattler <sattler@itp.uni-frankfurt.de>
2026-05-15 22:34:40 +0200

    Remove pointer usage from 2D interpolation tests

--- src/tests/interpolation2D.cc
+++ src/tests/interpolation2D.cc
@@ -17,9 +17,6 @@
 
 using namespace smash;
 
-std::unique_ptr<InterpolateData2DSpline> interp = nullptr;
-const double accuracy = 1e-6;
-
 static auto set_up_x_y_z_values() {
   const std::vector<double> x = {1, 2, 3, 4, 5};
   const std::vector<double> y = {1, 4, 8, 12};
@@ -35,7 +32,7 @@ TEST_CATCH(fail_N_points, std::invalid_argument) {
 
   /* Try creating 2D interpolation with too few points, which is expected
    * to raise an exception */
-  interp = std::make_unique<InterpolateData2DSpline>(x, y, z);
+  InterpolateData2DSpline(x, y, z);
 }
 
 TEST_CATCH(fail_dimensions, std::invalid_argument) {
@@ -45,116 +42,116 @@ TEST_CATCH(fail_dimensions, std::invalid_argument) {
 
   /* Try creating 2D interpolation with not-fitting dimensions, which is
    * expected to raise an exception */
-  interp = std::make_unique<InterpolateData2DSpline>(x, y, z);
+  InterpolateData2DSpline(x, y, z);
 }
 
 TEST_CATCH(x_vector_not_sorted, std::invalid_argument) {
   auto [x, y, z] = set_up_x_y_z_values();
   std::swap(x[0], x[1]);
 
-  interp = std::make_unique<InterpolateData2DSpline>(x, y, z);
+  InterpolateData2DSpline(x, y, z);
 }
 
 TEST_CATCH(y_vector_not_sorted, std::invalid_argument) {
   auto [x, y, z] = set_up_x_y_z_values();
   std::swap(y[0], y[1]);
 
-  interp = std::make_unique<InterpolateData2DSpline>(x, y, z);
+  InterpolateData2DSpline(x, y, z);
 }
 
 TEST(interpolate_bicubic) {
+  const double accuracy = 1e-6;
   const auto [x, y, z] = set_up_x_y_z_values();
-  interp = std::make_unique<InterpolateData2DSpline>(x, y, z);
+  const InterpolateData2DSpline f = InterpolateData2DSpline(x, y, z);
 
   // check exact values at the nodes
-  FUZZY_COMPARE((*interp)(2, 4), 3.0);
-  FUZZY_COMPARE((*interp)(4, 1), 5.0);
-  FUZZY_COMPARE((*interp)(5, 8), 6.0);
+  FUZZY_COMPARE(f(2, 4), 3.0);
+  FUZZY_COMPARE(f(4, 1), 5.0);
+  FUZZY_COMPARE(f(5, 8), 6.0);
 
   // check interpolation in x direction
-  COMPARE_RELATIVE_ERROR((*interp)(1.3, 1), 2.170375, accuracy);
-  COMPARE_RELATIVE_ERROR((*interp)(2.5, 4), 4.977678, accuracy);
-  COMPARE_RELATIVE_ERROR((*interp)(4.8, 12), 3.849142, accuracy);
+  COMPARE_RELATIVE_ERROR(f(1.3, 1), 2.170375, accuracy);
+  COMPARE_RELATIVE_ERROR(f(2.5, 4), 4.977678, accuracy);
+  COMPARE_RELATIVE_ERROR(f(4.8, 12), 3.849142, accuracy);
 
   // check interpolation in y direction
-  COMPARE_RELATIVE_ERROR((*interp)(2, 6), 4.043269, accuracy);
-  COMPARE_RELATIVE_ERROR((*interp)(3, 8.1), 3.929960, accuracy);
-  COMPARE_RELATIVE_ERROR((*interp)(5, 9.5), 5.530273, accuracy);
+  COMPARE_RELATIVE_ERROR(f(2, 6), 4.043269, accuracy);
+  COMPARE_RELATIVE_ERROR(f(3, 8.1), 3.929960, accuracy);
+  COMPARE_RELATIVE_ERROR(f(5, 9.5), 5.530273, accuracy);
 
   // check interpolation in both directions
-  COMPARE_RELATIVE_ERROR((*interp)(1.5, 5.3), 4.246599, accuracy);
-  COMPARE_RELATIVE_ERROR((*interp)(2.95, 9.1), 3.825197, accuracy);
-  COMPARE_RELATIVE_ERROR((*interp)(4.33, 11.4), 7.024691, accuracy);
+  COMPARE_RELATIVE_ERROR(f(1.5, 5.3), 4.246599, accuracy);
+  COMPARE_RELATIVE_ERROR(f(2.95, 9.1), 3.825197, accuracy);
+  COMPARE_RELATIVE_ERROR(f(4.33, 11.4), 7.024691, accuracy);
 }
 
 TEST_CATCH(x_value_out_of_lower_bound, std::out_of_range) {
   const auto [x, y, z] = set_up_x_y_z_values();
-  interp = std::make_unique<InterpolateData2DSpline>(x, y, z,
-                                                     ExtrapolationType::None);
-  (*interp)(0.5, 4);
+  const InterpolateData2DSpline f =
+      InterpolateData2DSpline(x, y, z, ExtrapolationType::None);
+  f(0.5, 4);
 }
 
 TEST_CATCH(x_value_out_of_upper_bound, std::out_of_range) {
   const auto [x, y, z] = set_up_x_y_z_values();
-  interp = std::make_unique<InterpolateData2DSpline>(x, y, z,
-                                                     ExtrapolationType::None);
-  (*interp)(5.5, 4);
+  const InterpolateData2DSpline f =
+      InterpolateData2DSpline(x, y, z, ExtrapolationType::None);
+  f(5.5, 4);
 }
 
 TEST_CATCH(y_value_out_of_lower_bound, std::out_of_range) {
   const auto [x, y, z] = set_up_x_y_z_values();
-  interp = std::make_unique<InterpolateData2DSpline>(x, y, z,
-                                                     ExtrapolationType::None);
-  (*interp)(2, 0.8);
+  const InterpolateData2DSpline f =
+      InterpolateData2DSpline(x, y, z, ExtrapolationType::None);
+  f(2, 0.8);
 }
 
 TEST_CATCH(y_value_out_of_upper_bound, std::out_of_range) {
   const auto [x, y, z] = set_up_x_y_z_values();
-  interp = std::make_unique<InterpolateData2DSpline>(x, y, z,
-                                                     ExtrapolationType::None);
-  (*interp)(2, 13);
+  const InterpolateData2DSpline f =
+      InterpolateData2DSpline(x, y, z, ExtrapolationType::None);
+  f(2, 13);
 }
 
 TEST(extrapolate_zero) {
   // check constant extrapolation if x or y values are out of bounds
   const auto [x, y, z] = set_up_x_y_z_values();
-  interp = std::make_unique<InterpolateData2DSpline>(x, y, z,
-                                                     ExtrapolationType::Zero);
+  const InterpolateData2DSpline f =
+      InterpolateData2DSpline(x, y, z, ExtrapolationType::Zero);
 
   // x out of bounds
-  FUZZY_COMPARE((*interp)(0.5, 4), 0.);
-  FUZZY_COMPARE((*interp)(8, 4), 0.);
+  FUZZY_COMPARE(f(0.5, 4), 0.);
+  FUZZY_COMPARE(f(8, 4), 0.);
 
   // y out of bounds
-  FUZZY_COMPARE((*interp)(2, 0.8), 0.);
-  FUZZY_COMPARE((*interp)(5, 16), 0.);
+  FUZZY_COMPARE(f(2, 0.8), 0.);
+  FUZZY_COMPARE(f(5, 16), 0.);
 
   // x and y out of bounds
-  FUZZY_COMPARE((*interp)(0.5, 0.8), 0.);
-  FUZZY_COMPARE((*interp)(7, 16), 0.);
+  FUZZY_COMPARE(f(0.5, 0.8), 0.);
+  FUZZY_COMPARE(f(7, 16), 0.);
 }
 
 TEST(extrapolate_constant) {
   // check constant extrapolation if x or y values are out of bounds
   const auto [x, y, z] = set_up_x_y_z_values();
-  interp = std::make_unique<InterpolateData2DSpline>(
-      x, y, z, ExtrapolationType::Constant);
+  const InterpolateData2DSpline f =
+      InterpolateData2DSpline(x, y, z, ExtrapolationType::Constant);
 
   // x out of bounds
-  FUZZY_COMPARE((*interp)(0.5, 4), (*interp)(1, 4));
-  FUZZY_COMPARE((*interp)(8, 4), (*interp)(5, 4));
+  FUZZY_COMPARE(f(0.5, 4), f(1, 4));
+  FUZZY_COMPARE(f(8, 4), f(5, 4));
 
   // y out of bounds
-  FUZZY_COMPARE((*interp)(2, 0.8), (*interp)(2, 1));
-  FUZZY_COMPARE((*interp)(5, 16), (*interp)(5, 12));
+  FUZZY_COMPARE(f(2, 0.8), f(2, 1));
+  FUZZY_COMPARE(f(5, 16), f(5, 12));
 
   // x and y out of bounds
-  FUZZY_COMPARE((*interp)(0.5, 0.8), (*interp)(1, 1));
-  FUZZY_COMPARE((*interp)(7, 16), (*interp)(5, 12));
+  FUZZY_COMPARE(f(0.5, 0.8), f(1, 1));
+  FUZZY_COMPARE(f(7, 16), f(5, 12));
 }
 
 TEST_CATCH(invalid_extrapolation_type, std::invalid_argument) {
   const auto [x, y, z] = set_up_x_y_z_values();
-  interp = std::make_unique<InterpolateData2DSpline>(x, y, z,
-                                                     ExtrapolationType::Linear);
+  InterpolateData2DSpline(x, y, z, ExtrapolationType::Linear);
 }
